4e4dad85604e8addf10780481695143a9f331dd8
Logan Peterson <lapeterson01@gmail.com>
2018-01-29 17:19:45 -0500

    add default city button

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
+//search for city in 'search' box
 $('.search').on('click', function() {
-  fetchCurrent($('#input-search').val());
-  fetchForecast($('#input-search').val());
+  if ($('#input-search').val() != '') {
+    fetchCurrent($('#input-search').val());
+    fetchForecast($('#input-search').val());
+  }
 
+  //clear text from 'search' box
   $('#input-search').val('');
 });
 
+//allow user to click 'search' button upon clicking 'Enter'
 $('#input-search').keyup(function(event) {
   if (event.keyCode == 13) {
     $('.search').click();
--- utils.js
+++ utils.js
@@ -1,25 +1,72 @@
+var setDefaultCity = function() {
+  $('#default-city').empty();
+  var defaultCity = 'Default City: <b>' + localStorage.getItem('default') + '</b>';
+  $('#default-city').append(defaultCity);
+}
+
+//posts current weather information for inputted city
 var postCurrent = function (data) {
   $('#weather-info').empty();
   $('#weather-info-img').empty();
 
-  var weatherInfo = '<div class="temp">' + data.main.temp + '°</div><div class=city>' + data.name + '</div><div>' + data.weather[0].main + '</div>';
+  let temp = Math.round(data.main.temp);
+
+  var weatherInfo = '<div class="temp">' + temp + '°</div><div class=city>' + data.name + '</div><div>' + data.weather[0].main + '</div>';
   $('#weather-info').append(weatherInfo);
 
-  var weatherInfoImg = '<img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png" alt="" height="70" width="70">';
-  $('#weather-info-img').append(weatherInfoImg);
+  var defaultButtonSet = function(data) {
+    if (data.name != localStorage.getItem('default')) {
+      $('#weather-info-img').empty();
+      var weatherInfoImg = '<div><img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png" alt="" height="70" width="70"></div><div><button type="button" class="btn btn-link default">Set as Default</button></div>';
+      $('#weather-info-img').append(weatherInfoImg);
+    } else {
+      $('#weather-info-img').empty();
+      var weatherInfoImg = '<div><img src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png" alt="" height="70" width="70"></div><div>Default</div><div><button type="button" class="btn btn-link undo">Undo</button></div>';
+      $('#weather-info-img').append(weatherInfoImg);
+    }
+  };
+
+  defaultButtonSet(data);
+
+  //set current city as default city
+  $(document).on('click', 'button.default', function() {
+    setDefault(data);
+    defaultButtonSet(data);
+    setDefaultCity();
+  });
+
+  //undo current city as default city
+  $(document).on('click', 'button.undo', function() {
+    localStorage.clear();
+    defaultButtonSet(data);
+
+    $('#default-city').empty();
+  });
 };
 
+//posts 5-day forecast for inputted city
 var postForecast = function (data) {
   $('.daily').remove();
 
   for (let i = 0; i <= 32; i+=8) {
     let date = data.list[i].dt_txt.substr(0, data.list[i].dt_txt.length - 9)
     let day = moment(date).format('dddd');
-    var daily = '<div class="daily"><div>' + data.list[i].weather[0].main + '</div><div class="temp">' + data.list[i].main.temp + '°</div><div><img src="http://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png" alt=""></div><div>' + day + '</div></div>';
+    let temp = Math.round(data.list[i].main.temp);
+    var daily = '<div class="daily"><div>' + data.list[i].weather[0].main + '</div><div class="temp">' + temp + '°</div><div><img src="http://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png" alt=""></div><div>' + day + '</div></div>';
     $('#daily-' + i).append(daily);
   };
 };
 
+//posts error message for inputted city that does not exist
+var postError = function(query, errorMsg) {
+  $('#weather-info').empty();
+  $('#weather-info-img').empty();
+  $('.daily').remove();
+
+  $('#weather-info').append('<div>' + query + ' ' + errorMsg + '</div>');
+}
+
+//retrieves current weather information
 var fetchCurrent = function (query) {
   $.ajax({
     method: "GET",
@@ -29,11 +76,12 @@ var fetchCurrent = function (query) {
       postCurrent(data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
-      console.log(textStatus);
+      postError(query, errorThrown);
     }
   });
 };
 
+//retrieves weather forecast information
 var fetchForecast = function (query) {
   $.ajax({
     method: "GET",
@@ -43,7 +91,27 @@ var fetchForecast = function (query) {
       postForecast(data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
-      console.log(textStatus);
+      postError(query, errorThrown);
     }
   });
 };
+
+//sets current city as default city
+var setDefault = function (data) {
+
+  if (data && localStorage.getItem('default') != data.name) {
+    localStorage.setItem('default', data.name);
+  };
+}
+
+var getDefault = function() {
+  if (localStorage.getItem('default')) {
+    fetchCurrent(localStorage.getItem('default'));
+    fetchForecast(localStorage.getItem('default'));
+  }
+};
+
+getDefault();
+if (localStorage.getItem('default')) {
+  setDefaultCity();
+}
