64898734f551a8cefcfa81beff18f869a4f8181c
Matthew Keller <matthewkeller@Rachels-MacBook-Air.local>
2018-05-06 16:00:57 -0400

    first user story

---
+++ main.js
@@ -0,0 +1,48 @@
+const currentWeather = []
+
+var renderWeather = function () {
+  $('.current-weather').empty();
+    let currentTemperature = (Number(currentWeather[0].main.temp)*9/5-459.67).toFixed(0);
+    let currentCity = currentWeather[0].name;
+    let currentDescription = currentWeather[0].weather[0].main;
+    let weatherIcon = "http://openweathermap.org/img/w/"+currentWeather[0].weather[0].icon+".png"
+
+    var source = $('#current-weather-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template({
+      temp:currentTemperature,
+      city:currentCity,
+      description:currentDescription,
+      imageURL:weatherIcon,
+    })
+    $(newHTML).appendTo($('.current-weather'));
+
+  // $('#loading_icon').addClass("hide");
+};
+
+var fetch = function (query) {
+  $('#loading_icon').removeClass("hide");
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/weather?APPID=617793780ad9ce0ef4274bedce3819d2&q="+query,
+    dataType: "json",
+    success: function(data) {
+      storeWeather(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      $('#loading_icon').addClass("hide");
+      console.log(textStatus);
+    }
+  });
+};
+
+var storeWeather = function (data) {
+  currentWeather[0] = data
+  renderWeather();
+};
+
+$('.search').on('click', function () {
+  var search = $('#search-query').val();
+
+  fetch(search);
+});
