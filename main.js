80d15ab73d5585d06352932604f22136e67ea6dc
davieu <davisumana@gmail.com>
2018-05-06 15:12:08 -0400

    initial commit 1st story complete

---
+++ main.js
@@ -0,0 +1,48 @@
+var weather = [
+];
+
+var renderWeather = function () {
+  $('.weather').empty();
+    // var iconsss = weather[0].weather[0].icon;
+    // var iconIt = 'http:\/\/openweathermap.org\/img\/w\/' + icon +'.png';
+    var source = $('#weather-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template({
+      temperature:Math.round(weather[0].main.temp * 1.8 - 459.67) + '\xB0',
+      city:weather[0].name  + ', ' + weather[0].sys.country,
+      condition:weather[0].weather[0].description,
+      icon: 'http:\/\/openweathermap.org\/img\/w\/' + weather[0].weather[0].icon + '.png'
+    })
+    $(newHTML).appendTo($('.weather'));
+
+  }
+
+
+var addWeather = function (data) {
+  console.log(data);
+  weather = [];
+  weather.push(data);
+  renderWeather();
+
+};
+
+
+var fetch = function (query) {
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + query+"&APPID=c3e4742d4285be4db83f16fdce0c8f7b",
+    dataType: "json",
+    success: function(data) {
+      addWeather(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+};
+
+  $('.search').on('click', function (){
+    var search = $('#search-query').val();
+
+    fetch(search);
+  });
