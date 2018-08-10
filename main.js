e768036e643156a675573187c3d6c17bcfa481bf
Carrington Perry <carringtonperry@Carringtons-MacBook-Pro.local>
2018-08-10 10:35:22 -0400

    finished adding all files and links

---
+++ main.js
@@ -0,0 +1,21 @@
+//Model is both the data and the logic you will use
+// only stor data in model.
+// only up date when events(client) changes it.
+
+
+
+
+
+
+//View What the user see rendered to the screen
+//only update view in repsonse to changes in model
+// Can be considered the UI
+
+
+
+
+
+
+
+//Controller Handles Events. Takes in info from UI
+// and processes it to change the Model. 
