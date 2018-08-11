8be1f37735fd2d81c90f12fba60bdb6b3d53f7b8
Tanya Cox <tanya54.tb@gmail.com>
2018-08-11 17:47:58 -0400

    added bootstrap

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
-//create click handler for search button
-$('#search-button').click(function() {
-  console.log('button clicked!');
-  //get user input from search query
+$(document).ready(function() {
 
-  //display results from user input
+  //create click handler for search button
+  $('#search-button').click(function() {
+    console.log('button clicked!');
 
-  //get results from weather api
+    //get user input from search query
+    var userInputCity = $('#search-query').val();
+    console.log(userInputCity);
+    //display results from user input
+    $('#city').html(userInputCity);
+    //get results from weather api
 
-  //display current forecast from weather api
-  
-  //display 5-day forcst with results weather api
+    //display current forecast from weather api
 
+    //display 5-day forcst with results weather api
+
+  })
 })
+
+var fetch = function (query) {
+
+};
+
+var addBooks = function (data) {
+
+};
+
+
+var fetch = function (query) {
+  $.ajax({
+    method: "GET",
+    url: "https://www.googleapis.com/books/v1/volumes?q=hunger+games",
+    dataType: "json",
+    success: function(data) {
+      console.log(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+};
