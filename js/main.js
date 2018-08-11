c47252869d4b6d83ee8eb4735a3947c6d91a61f2
shodan993 <paulnstanley@gmail.com>
2018-08-11 12:30:29 -0400

    adding forecast functionality - part 2 complete

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@
 // moment().format();
 
 var weatherArray = [];
+var forecastArray = [];
 
 //functions
 var fetch = function (search) {
@@ -17,36 +18,49 @@ var fetch = function (search) {
   var cityName = search;
   var country = ",us"
   var apiKey = "&APPID=94e4fb6ff9320109825dcbc988e23b69";
-  //concatened URLs
   var units = "&units=imperial"
+  //concatenated URLs
   var currentQuery = baseUrl + current + cityName + country + apiKey + units;
   var forecastQuery = baseUrl + forecast + cityName + country + apiKey + units;
-  // var forecastQuery = baseUrl + forecast + cityID + apiKey;
-  console.log("concatenated URL for current weather request: " + currentQuery); //test the concat
-  console.log("concatenated URL for forecast weather request: " + forecastQuery); //test the concat
+  //log the URLs for testing/viewing data
+  console.log("concatenated URL for current weather request: " + currentQuery);
+  console.log("concatenated URL for forecast weather request: " + forecastQuery);
+  //make the ajax calls separately, push to arrays, push to html
+  ajaxFunc(currentQuery, weatherArray, addWeather);
+  ajaxFunc(forecastQuery, forecastArray, addForecast);
+};
 
+//ajax func:
+var ajaxFunc = function (url, arr, func) {
   $.ajax({
     method: "GET",
-    url: currentQuery,
+    url: url,
     dataType: "json",
     success: function(data) {
-      addWeather(data);
+      arr.push(data);
+      func(data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
     }
-  });
+});
 };
 
 var addWeather = function (data) {
   $('.weather-main').empty();
-    // for (var i=0; i<10; i++) {
-      // console.log(data.items[i]);
     weatherArray.push(data);
-    console.log("here's the forecast array after data push: " + weatherArray[0]);
-    console.log("here's the forecast array's 0.main.temp: " + weatherArray[0].main.temp);
-  // }
+    console.log("here's the weather array after data push: " + weatherArray[0]);
+    console.log("here's the weather array's 0.main.temp: " + weatherArray[0].main.temp);
   findState(data);
+};
+
+var addForecast = function (data) {
+  $('.forecast-main').empty();
+    // for (var i=0; i<10; i++) {
+    forecastArray.push(data);
+    console.log("here's the forecast array after data push: " + forecastArray[0]);
+    console.log("here's the forecast array's first timestamp: " + forecastArray[0].list[0].dt);
+  // }
 };
 
 var findState = function (search) {
@@ -66,26 +80,56 @@ var findState = function (search) {
 };
 
 var renderWeather = function (cityState) {
-  // for (var i=0; i<renderWeather; i++){
-    var temperature = weatherArray[0].main.temp;
-    var tempRounded = temperature.toFixed(1);
-    var source = $('#weather-handlebars').html();
-    var weatherMainDisplayTemplate = Handlebars.compile(source);
-    var newHTML = weatherMainDisplayTemplate(
-      {
-      temp: (tempRounded + "° F"),
-      location: cityState,
-      description: weatherArray[0].weather[0].main,
-      image: ("http://openweathermap.org/img/w/" + weatherArray[0].weather[0].icon + ".png")
-      }
-    );
-    $('.weather-main').append(newHTML);
-  // }
+  console.log("renderWeather has been invoked");
+  var temperature = weatherArray[0].main.temp;
+  var tempRounded = temperature.toFixed(1);
+  var source = $('#weather-handlebars').html();
+  var weatherMainDisplayTemplate = Handlebars.compile(source);
+  var newHTML = weatherMainDisplayTemplate(
+    {
+    temp: (tempRounded + "° F"),
+    location: cityState,
+    description: weatherArray[0].weather[0].main,
+    image: ("http://openweathermap.org/img/w/" + weatherArray[0].weather[0].icon + ".png")
+    }
+  );
+  $('.weather-main').append(newHTML);
+  renderForecast();
 };
 
-//click handlers
+var renderForecast = function () {
+  console.log("renderForecast has been invoked");
+  var forecasts = forecastArray[0].list;
+  var numberForecasts = forecasts.length;
+  console.log("forecastArray length check: " + numberForecasts);
+  for (var i=7; i<numberForecasts; i+=8){
+    var temperature = forecastArray[0].list[i].main.temp;
+    var tempRounded = temperature.toFixed(0);
+    console.log("temp for day at index " + i + ": " + tempRounded);
+    var timestamp = forecastArray[0].list[i].dt;
+    console.log("unix timestamp for this index: " + timestamp);
+    var timestampConverted = moment.unix(timestamp).format('MMMM Do YYYY, h:mm:ss a');
+    console.log("full converted timestamp for this index: " + timestampConverted);
+    var dayConverted = moment.unix(timestamp).format('dddd');
+    console.log("day of week for index " + i + ": " + dayConverted);
+    var source = $('#forecast-handlebars').html();
+    var weatherForecastTemplate = Handlebars.compile(source);
+    var newHTML = weatherForecastTemplate(
+      {
+      description: forecastArray[0].list[i].weather[0].main,
+      image: ("http://openweathermap.org/img/w/" + forecastArray[0].list[i].weather[0].icon + ".png"),
+      temp: (tempRounded + "° F"),
+      day: dayConverted
+      }
+    );
+    $('.forecast-main').append(newHTML);
+  }
+};
+
+//click handler
 $('#location-submit').on('click', function () {
   weatherArray.length = 0;  //empties the array
+  forecastArray.length = 0;
   console.log("clicked the submit button")
   var search = $('#location-input').val();
   console.log("search input was: " + search);
