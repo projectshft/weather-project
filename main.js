d9d6874a2720e594cb9e71db5e2110e6293b67fa
Tanya Cox <tanya54.tb@gmail.com>
2018-08-11 15:11:03 -0400

    add bootstrap, update html, add form, create click event handler for search button

---
+++ main.js
@@ -0,0 +1,14 @@
+//create click handler for search button
+$('#search-button').click(function() {
+  console.log('button clicked!');
+  //get user input from search query
+
+  //display results from user input
+
+  //get results from weather api
+
+  //display current forecast from weather api
+  
+  //display 5-day forcst with results weather api
+
+})
