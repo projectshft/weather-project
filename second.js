4c2d2bde76ced9dd2aec57738df603bf83812b0b
AaronStearns <stearnsconcepts@gmail.com>
2018-08-12 16:19:40 -0400

    adding weatherFormatter function that extracts and formats day of week, hour/minute of day, temp, and weather description

--- second.js
+++ second.js
@@ -43,10 +43,32 @@ var weatherGetter = function (urlbase, query, array) {
 var forecastWeather = [];
 
 
-//////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 // "Sunday Aug 12th"
-moment().format('dddd MMM Do');
-//////////////////////////////////////////////////////////////////////////////////
+// moment().format('dddd MMM Do');
+
+let dataArray = [];
+// extracts and formats day of week, hour/minute of day, temp, and weather description
+var weatherFormatter = function (data) {
+for (let i = 0; i < data[0].list.length; i++) {
+  let structuredData = {
+        [i]: {
+            // formats the dates down to days ex: 'Sunday', 'Monday'
+            dayOfWeek: moment(data[0].list[i].dt_txt).format('dddd').toString(),
+            // formats the hour and minute ex: '18:00'
+            timeOfDay: moment(data[0].list[i].dt_txt).format('hh:mm').toString(),
+            // gets all un-rounded temperature values
+            temp: data[0].list[i].main.temp,
+            // gets all weather descriptions ex: 'Sunny', 'Cloudy'
+            weather: data[0].list[0].weather[0].main
+        }
+    };
+  // pushes to dataArray in global scope
+  dataArray.push(structuredData);
+  }
+  // return dataArray
+}
+////////////////////////////////////////////////////////////////////////////////
 
 
 
