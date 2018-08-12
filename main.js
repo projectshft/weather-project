681e51552c5996f90ba2b08ab8ecc650d6f5e7c6
zulmycortez <zulmycortez@gmail.com>
2018-08-12 18:38:28 -0400

    Achieved retrieval of api data through search button and accurate html

---
+++ main.js
@@ -0,0 +1,55 @@
+var search = $('#search-query').val();
+
+var fetch = function(search) {
+  $.ajax({
+    method: "GET",
+    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + search + ",US&units=imperial&APPID=f4f569817c0687a151c5c1af2f1ddfd2",
+    dataType: "json",
+    success: function(data) {
+      addWeather(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+
+  });
+};
+
+$('.search').on('click', function() {
+
+  var search = $('#search-query').val();
+
+  fetch(search);
+});
+
+var addWeather = function(data) {
+  weatherArray = [];
+
+  var city = data.city.name;
+  var weathertype = data.list[0].weather[0].main;
+  var temp = data.list[0].main.temp;
+
+  var uniqueWeather = {
+    city: city,
+    weather: weathertype,
+    temp: temp
+  };
+
+  weatherArray.push(uniqueWeather);
+  renderWeather();
+};
+
+var renderWeather = function() {
+  $('.weather').empty();
+
+  for (var i = 0; i < weatherArray.length; i++) {
+    var weather = weatherArray[i];
+    var index = weatherArray.indexOf(weather);
+
+    var source = $('#weather-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template({"City": weatherArray[i].city, "temperature": weatherArray[i].temp, "weather": weatherArray[i].weather});
+
+    $('.weather').append(newHTML);
+  };
+}
---
+++ utils.js
@@ -0,0 +1,84 @@
+const Collection = (config) => {
+  const models = []
+
+  const init = () => {
+    if (config) {
+      models.push(config)
+    }
+  }
+
+  let changeCallback = null
+
+  const add = (item) => {
+    if (!_.includes(models, item) || _.isEmpty(models)) {
+      models.push(item);
+
+      if (changeCallback) {
+        changeCallback()
+      }
+    }
+  }
+
+  const change = (func) => changeCallback = func;
+
+  init();
+
+  return {
+    add,
+    models,
+    change
+  }
+};
+
+const Model = (config) => {
+  const attributes = {}
+
+  let changeCallback = null
+
+  const init = () => Object.assign(attributes, config);
+
+  const set = (prop, value) => {
+    const tempObj = Object.assign({}, attributes)
+
+    tempObj[prop] = value
+
+    if (!_.isEqual(attributes, tempObj)) {
+      attributes[prop] = value
+
+      if (changeCallback) {
+        changeCallback()
+      }
+    }
+  };
+
+  const get = (prop) => attributes[prop];
+  const change = (func) => changeCallback = func;
+
+  const getAttributes = function() {
+  return attributes;
+
+  };
+
+  init()
+
+  return {
+    set,
+    get,
+    change,
+    getAttributes
+
+  }
+
+};
+
+const View = (model, template) => {
+const render = function () {
+  var attrs = model.getAttributes();
+
+  return template(attrs);
+}
+
+return {
+  render
+}
+};
