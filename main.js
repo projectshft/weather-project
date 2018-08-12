087ca8fef3c10db27e1acf663f119e5222bd0aa2
Tanya Cox <tanya54.tb@gmail.com>
2018-08-12 18:54:57 -0400

    add 5-day forecast api

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ $(document).ready(function() {
     $('#city').html(userInputCity);
     //get results from weather api
 
-
     $.ajax({
       method: "GET",
       url: "http://api.openweathermap.org/data/2.5/weather",
@@ -29,8 +28,8 @@ $(document).ready(function() {
         console.log(currTemp);
         var currCondition = data.weather[0].main;
         console.log(currCondition);
-        var weatherStr = "<div>" + currTemp + "</br>" + currCondition + "</div>";
-        $('#weekly-forecast').html(weatherStr);
+        var weatherStr = "<div>" + currTemp + "&#176" + "</br>" + currCondition + "</div>";
+        $('#currentWeather').html(weatherStr);
 
 
       },
@@ -43,28 +42,55 @@ $(document).ready(function() {
 
 
     //display 5-day forcst with results weather api
-    /*
-        $.ajax({
-          method: "GET",
-          url: "http://api.openweathermap.org/data/2.5/weather",
-          data: {
-            q: userInputCity,
-            APPID: "25d5868ca4cded8099e8b289946b3b89",
-            units: "imperial"
-          },
-          dataType: "json",
-          success: function(data) {
-            //console.log(data);
-            //$('#currentWeather').html(data.main.temp);//display current forecast from weather api
 
-          },
-          error: function(jqXHR, textStatus, errorThrown) {
-            console.log(textStatus, errorThrown);
-            alert("Error! PLease try again later.")
-          }
+    $.ajax({
+      method: "GET",
+      url: "http://api.openweathermap.org/data/2.5/forecast",
+      data: {
+        q: userInputCity,
+        APPID: "02a9b0ceaf821b88e1a2be12620fef05",
+        units: "imperial"
+      },
+      dataType: "json",
+      success: function(data) {
+        console.log(data);
 
-        });
-        */
+        // note:
+        for (var i = 0; i < 40; i+=8) {
+
+          var temp = data.list[i].main.temp;
+          var cond =data.list[i].weather[0].description;
+          console.log(temp + "-" + cond);
+
+
+          var line2 = temp + "</br>";
+          var line3 = cond + "</br>";
+          // add new div with date and weather
+          $('#weekly-forecast').append(line2 + line3);
+
+
+
+
+/*
+          var listItem, itemStatus, eventType;
+          listItem = 'item: ' + e.target.textContent + '<br/>';
+          itemStatus = 'Status: ' + e.data.status + '<br /> ';
+          eventType= 'Event: ' + e.type;
+          $('#notes').html(listItem + itemStatus + eventType);
+*/
+
+
+      }
+    },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus, errorThrown);
+        alert("Error! PLease try again later.")
+      }
+
+
+
+    });
+
 
     // https://api.openweathermap.org/data/2.5/weather ?id=524901 & APPID=1111111111
 
