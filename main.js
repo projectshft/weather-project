d90e20e6b2d3448ee84c587a780af65fd3b3844f
Charles Ferringer <charlesferringer@yahoo.com>
2018-08-12 22:30:46 -0400

    added local storage functionality but didn't get it to work yet

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
+//Enable user to save the searched for city as default so it loads when the page refreshes
+var STORAGE_ID = 'location';
+var saveToLocalStorage = function () {
+  if ($('#search-query').val() !== "" && $('#search-query').val() !== null)
+  {
+    localStorage.setItem(STORAGE_ID, JSON.stringify($('#search-query').val()));
+  }
+  else {
+    alert("You must enter a valid U.S. city.")
+  }
+};
+var getFromLocalStorage = function () {
+  return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+};
+
+$('#set-as-default').on('click', function () {
+  saveToLocalStorage();
+});
+
+//Create a map with a default location of Durham that changes to the location of the city entered by the user
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 35.9940, lng: -78.8986},
-    zoom: 8
+    center: {lat: 35.9050, lng: -78.8615},
+    zoom: 9
     });
   };
 
-//Fetch the weather information from the api and return in JSON format
+//Fetch the weather information from the api and return it in JSON format
 var fetchWeather = function (query) {
   var citySearch = $('#search-query').val();
   var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + citySearch + ',us&units=imperial&appid=015bc22e332b00d0c46a9ee1a9d27e75';
@@ -23,7 +43,7 @@ var fetchWeather = function (query) {
   });
 };
 
-//Fetch the 5 day forecast from the api and return in JSON format
+//Fetch the 5 day forecast from the api and return it in JSON format
 var fetchForecast = function (query) {
   var citySearch = $('#search-query').val();
   var url = 'http://api.openweathermap.org/data/2.5/forecast?q=' + citySearch + ',us&units=imperial&appid=015bc22e332b00d0c46a9ee1a9d27e75';
@@ -39,13 +59,16 @@ var fetchForecast = function (query) {
     }
   });
 };
-//create empty arrays to store data in for our model
+
+//create empty arrays to store weather data in for our model
 var weathers;
 var forecasts;
 
 //Create a function that takes weather data from the api and pushes it into the weathers array
 var addWeather = function (data) {
   weathers = [];
+
+  //Reformat this
   for (var i = 0; i < 1; i++) {
     var weatherData = data;
 
@@ -58,6 +81,9 @@ var addWeather = function (data) {
     };
 
     var city = function () {
+      // if (localStorage.location !== null && localStorage.location !== "") {
+      //   return localStorage.location;
+      // }
       if (weatherData.name) {
         return weatherData.name;
       } else {
@@ -76,8 +102,19 @@ var addWeather = function (data) {
     };
 
     var icon = function () {
-      var iconURL = 'http://openweathermap.org/img/w/' + weatherData.weather[0].icon +'.png';
-      if (weatherData.weather[0].icon) {
+      var iconData = weatherData.weather[0].icon;
+      var iconURL = 'http://openweathermap.org/img/w/' + iconData +'.png';
+
+      //Change CSS styling based on the icon returned
+
+      // var styles = function () {
+      //   var background =  document.body.style.backgroundColor;
+      //   if (iconData = '03d') {
+      //     background = #FFFF00;
+      //   }
+      // };
+
+      if (iconData) {
         return iconURL;
       } else {
         return null;
@@ -97,9 +134,10 @@ var addWeather = function (data) {
   renderWeather();
 };
 
-//create a function that takes the 5 day forecast data from the api and pushes it into the forecasts array
+//Create a function that takes the 5 day forecast data from the api and pushes it into the forecasts array.
 var addForecast = function (data) {
   forecasts = [];
+  //Find a better way to do this. Return the highest temperature or the temperature at a certain point every day.
   for (var i = 0; i < 40; i+=8) {
     var forecastData = data;
 
@@ -138,6 +176,7 @@ var addForecast = function (data) {
       }
     };
 
+    //Format the timestamp returned by the API and turn it into the day of the week.
     var day = function () {
 
       var d = new Date(forecastData.list[i].dt_txt);
@@ -172,7 +211,7 @@ var addForecast = function (data) {
   renderForecast();
 };
 
-//Create a function to render the weather onto the page
+//Create a function to render the weather onto the page. Empty the weather array and store the data from the search in it.
 var renderWeather = function () {
   $('.weathers').empty();
   var source = $('#weather-template').html();
@@ -189,7 +228,7 @@ var renderWeather = function () {
   }
 };
 
-//Create a function to render the forecast onto the page
+//Create a function to render the forecast onto the page. Empty the forecast array and store the data from the search in it.
 var renderForecast = function () {
   $('.forecasts').empty();
   var source = $('#forecast-template').html();
@@ -208,9 +247,14 @@ var renderForecast = function () {
 };
 
 //Listen for clicks and perform a search based on what is entered in the input box
-$('.search').on('click', function () {
+$('#search').on('click', function () {
   var search = $('#search-query').val();
-
+  if (search == "" || search == null) {
+    alert("Please enter a valid U.S. city");
+  } else {
   fetchWeather(search);
   fetchForecast(search);
+  }
 });
+
+console.log(localStorage.location);
