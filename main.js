cb6f743593403e5cf77e83aa3768329964f01347
Tien Linda Vu <lindavu@Tiens-MacBook-Pro.local>
2018-08-13 17:47:50 -0400

    re-structured javascript - the parsed data is ugly but the render works

--- main.js
+++ main.js
@@ -1,76 +1,44 @@
 //API KEY:  APPID {1b7b6777bc5e9df73114043701e7a3d1}
 //server: api.openweathermap.org
+// "http://api.openweathermap.org/data/2.5/weather?q=durham,us&APPID=1b7b6777bc5e9df73114043701e7a3d1"
 // first half of URL: "http://api.openweathermap.org/data/2.5/weather?q="
 // second half of URL with API KEY: "&APPID=1b7b6777bc5e9df73114043701e7a3d1"
-
-//initialize & declare global variables as needed
-
-
+ //initialize & declare global variables as needed
 var searchQuery;
 
-//use JQUERY to bind search button with click
+ //use JQUERY to bind search button with click
 $('.search').on('click', function () {
   searchQuery = $("#search-query").val();
   fetch(searchQuery);
 });
-
-//create render function to display data
-var renderData = function() {
+ //create render function to display data
+var renderData = function(data) {
   $('.current-weather').empty();
   var source = document.getElementById("weather-template").innerHTML;
   var template = Handlebars.compile(source);
-  var newHTML = template(weather[0]);
+  var newHTML = template(data);
+
   $('.current-weather').append(newHTML);
 };
 
-// loop through big data object
-var addWeather = function (data) {
-  weather = [];
+//create function to grab desired data
+var parseData = function(data) {
+  var temperature = data.main.temp;
+  var conditions = data.weather[0].description;
+  var image = data.weather[0].icon;
+  var city = data.name;
 
-  for(var i = 0; i < data.list.length; i++) {
-    var weatherData = data.list[i];
-
-    var temperature = function() {
-      if(weatherData.main.temp) {
-        return weatherData.main.temp;
-      } else {
-        return null;
-    }
+  return {
+    temperature: temperature,
+    conditions: conditions,
+    city: city,
+    image: image,
   };
-  var conditions = function() {
-    if(weatherData.weather[0].main) {
-      return weatherData.weather[0].main;
-    } else {
-      return null;
-    }
-  };
-  var image = function() {
-    if(weatherData.weather[0].icon) {
-      return weatherData.weather[0].icon;
-    } else {
-      return null;
-    }
-  };
-  var locale = function() {
-    if(data.city.name) {
-      return data.city.name;
-    } else {
-      return null;
-    }
-  };
-  var currentWeather = {
-    temperature: temperature(),
-    conditions: conditions(),
-    image: image(),
-    locale: locale(),
-  };
-  weather.push(currentWeather);
-}
-renderData();
 };
 
-//the values that the user inputs should signal to model and pull correct data from the api
+ //the values that the user inputs should signal to model and pull correct data from the api
 //this data should render onto the webpage
+//var fetch = function (clickSearch) {
 var fetch = function (query) {
   $.ajax({
     method: "GET",
@@ -78,9 +46,9 @@ var fetch = function (query) {
     url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + ",us&APPID=1b7b6777bc5e9df73114043701e7a3d1",
     dataType: "json",
     success: function(data) {
-      console.log(data);
-      addWeather(data);
+      var weatherData = parseData(data);
+        renderData(weatherData);
     },
-    error: console.log('Sorry, something went wrong.')
+    error: () => console.log('Sorry, something went wrong.')
   });
 };
