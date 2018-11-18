720dacdb93dafd39618321cde2dfff59691361db
Rhys Lutsky <rhyslutsky@gmail.com>
2018-11-18 12:40:41 -0500

    add data to part 2

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ $('.btn-search').on('click', function (e) {
     e.preventDefault();
     var search = $('#searchField').val();
     fetch(search);
+    fetch2(search);
 });
 
 
@@ -12,12 +13,12 @@ var fetch = function (query) {
     //compile template in JS using Handlebars
     var source = $('#currentWeather-template').html();
     var template = Handlebars.compile(source);
-   
+
     $.ajax({
         method: 'GET',
         url: "http://api.openweathermap.org/data/2.5/weather?q=" + query + ",us&units=imperial&APPID=2207b3e862065d895f387f6ef45e43ad",
         dataType: "json",
-        
+
         success: function (data) {
             //fill template with data
             var html = template(data);
@@ -29,4 +30,23 @@ var fetch = function (query) {
     });
 }
 
+var fetch2 = function (query) {
+    //compile template in JS using Handlebars
+    var source2 = $('#forecast-template').html();
+    var template2 = Handlebars.compile(source2);
 
+    $.ajax({
+        method: 'GET',
+        url: "http://api.openweathermap.org/data/2.5/forecast?q=" + query + ",us&units=imperial&APPID=2207b3e862065d895f387f6ef45e43ad",
+        dataType: "json",
+
+        success: function (data) {
+            //fill template with data
+            var html2 = template2(data);
+            $('#weather-forecast').append(html2)
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
+        }
+    });
+}
