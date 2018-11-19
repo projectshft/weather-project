d4a9c53b7ea334cd4b0993136f5135b17b1ba5b4
Rhys Lutsky <rhyslutsky@gmail.com>
2018-11-18 23:17:03 -0500

    access date text from DOM

--- script.js
+++ script.js
@@ -22,31 +22,89 @@ var fetch = function (query) {
         success: function (data) {
             //fill template with data
             var html = template(data);
-            $('#weather-data').html(html)
+            $('#weather-data').html(html);
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
         }
     });
-}
+};
+
 
 var fetch2 = function (query) {
     //compile template in JS using Handlebars
     var source2 = $('#forecast-template').html();
     var template2 = Handlebars.compile(source2);
 
+    //5 day forecast data
     $.ajax({
         method: 'GET',
         url: "http://api.openweathermap.org/data/2.5/forecast?q=" + query + ",us&units=imperial&APPID=2207b3e862065d895f387f6ef45e43ad",
         dataType: "json",
 
         success: function (data) {
+            //change given date/time format to just day of week
+            console.log(data);
+
             //fill template with data
             var html2 = template2(data);
-            $('#weather-forecast').append(html2)
+            $('#weather-forecast').html(html2)
+
+            //jQuery select DOM element
+            var dates = $("p.day")
+            console.log(dates);
+
+            //jQuery - access NodeList items
+            for (var i = 0; i < dates.length; i++) {
+                var dayText = dates[i].innerText;
+                console.log(dayText);
+
+                // var moment = require('moment');
+                var dayName = moment(dayText, "YYYY-MM-DD HH:MM:SS").format("ddd");
+                console.log(dayName);
+
+
+            };
+
+
+
         },
+
         error: function (jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
-        }
+        },
+
     });
-}
+};
+
+// //get the text from each p.day element as a string(ie: 2018-11-19 21:00:00)
+//     var myDates = $("p.day").text();
+//     console.log(myDates.length);
+
+// //replace the text with day of the week (ie - Mon)
+//     $("myDates").each().replaceWith(function () {
+//         moment().format("ddd");
+//     });
+
+
+
+
+
+
+//loop thru all 5 days and get the weather icon id
+// var id =  list.i.weather.0.icon
+// return id;
+//  $.ajax(id){
+//     method: 'GET',
+//         url: "http://http://openweathermap.org/img/w/" + id + ".png&APPID=2207b3e862065d895f387f6ef45e43ad",
+//     dataType: "json",
+
+//     success: function (data) {
+//         //fill template with data
+//         var html2 = template2(data);
+//         $('#weather-forecast').html(html2)
+//     },
+//     error: function (jqXHR, textStatus, errorThrown) {
+//         console.log(textStatus);
+//     }
+// });
