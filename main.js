fdb40f92220a616dcf6b45bbf2f393c990064f17
Matthew Fine <matthewfine@Matthews-MacBook-Pro-2.local>
2018-11-18 21:40:15 -0500

    Complete requirements for basic functionality

--- main.js
+++ main.js
@@ -1,99 +1,183 @@
+//global variable declaration
 var forecasts = [];
+var currentWeather = {};
 
-var dailyForecast = {};
-
-var addForecast = function (data) {
+var declareCurrentWeather = function (data) {
 
   var temp = function () {
     if (data.main.temp) {
       return data.main.temp;
-    } else {
-      return null;
     }
   };
 
-  var weather = function() {
+  var weather = function () {
     if (data.weather[0].main) {
       return data.weather[0].main;
-    } else {
-      return null;
     }
   };
 
-  var location = function() {
+  var location = function () {
     if (data.name) {
       return data.name;
-    } else {
-    return null;
     }
   };
 
   var icon = function () {
     if (data.weather[0].icon) {
       return data.weather[0].icon;
-    } else {
-      return null
     }
-  }
+  };
 
-  dailyForecast = {
+  currentWeather = {
     icon: icon(),
     temp: temp(),
     location: location(),
     weather: weather()
-  }
+  };
 
-  forecasts.push(dailyForecast);
-  console.log(forecasts)
-  renderTodaysForecast();
+  renderCurrentWeather();
 
-  console.log(dailyForecast);
-  return dailyForecast;
 };
 
-var fetch = function (query) {
+
+var addWeeklyForecast = function (data) {
+  forecasts = [];
+
+  for (var i = 0; i < data.list.length; i+=8) {
+
+    var forecastData = data.list[i];
+
+    var temp = function () {
+      if (forecastData.main.temp) {
+        return forecastData.main.temp;
+      } else {
+        return null;
+      }
+    };
+
+    var weather = function() {
+      if (forecastData.weather[0].main) {
+        return forecastData.weather[0].main;
+      } else {
+        return null;
+      }
+    };
+
+    var location = function() {
+      if (data.city.name) {
+        return data.city.name;
+      } else {
+      return null;
+      }
+    };
+
+    var icon = function () {
+      if (forecastData.weather[0].icon) {
+        return forecastData.weather[0].icon;
+      } else {
+        return null;
+      }
+    };
+
+    var date = function () {
+      if (forecastData.dt_txt) {
+        return moment(forecastData.dt_txt).format('dddd');
+      } else {
+      return null;
+      }
+    };
+
+    var forecast = {
+      date: date(),
+      icon: icon(),
+      temp: temp(),
+      location: location(),
+      weather: weather()
+    };
+
+    forecasts.push(forecast);
+  }
+  console.log(forecasts)
+  renderWeeklyForecast();
+};
+
+
+//fetch requests from openDataAPI
+var fetchCurrentWeather = function (query) {
   $.ajax({
     method: "GET",
     url: "https://api.openweathermap.org/data/2.5/weather?q="+query+"&APPID=fa9a0e4c4fd59f4be6924cdceb975e9b&units=imperial",
     dataType: "json",
     success: function(data) {
-      addForecast(data);
-
+      declareCurrentWeather(data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
-      console.log(textStatus);
+      alert('Data for requested city has not been found. Please try again.')
     }
   });
 };
 
+var fetchWeeklyForecast = function(query) {
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/forecast?q="+query+"&APPID=fa9a0e4c4fd59f4be6924cdceb975e9b&units=imperial",
+    dataType: "json",
+    success: function(data) {
+      addWeeklyForecast(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+    }
+  });
+};
+
+
 //render function
-var renderTodaysForecast = function () {
+var renderCurrentWeather = function() {
   $('#todays-weather').empty();
-  var todaysWeather = renderWithHandlebars(dailyForecast)
+  var todaysWeather = renderCurrentWeatherWithHandlebars(currentWeather);
   $('#todays-weather').append(todaysWeather);
 }
 
-var renderWithHandlebars = function(weatherObject){
+var renderCurrentWeatherWithHandlebars = function(currentWeatherObject){
   var source = $('#current-weather-template').html();
   var template = Handlebars.compile(source);
-  var newHTML = template(weatherObject);
+  var newHTML = template(currentWeatherObject);
   return newHTML;
 };
+
+var renderWeeklyForecast = function() {
+  $('#weeks-forecast').empty();
+  forecasts.forEach(function(day) {
+    var dailyForecast = renderWeeksForecastWithHandlebars(day);
+    $('#weeks-forecast').append(dailyForecast);
+  })
+};
+
+var renderWeeksForecastWithHandlebars = function(dailyForecastObject) {
+  var source = $('#weeks-forecast-template').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(dailyForecastObject);
+  return newHTML;
+};
+
 
 //click event
 $('#find-weather').on('click', function() {
   var city = $('#search-query').val();
   if (city !== '') {
-    fetch(city);
-  }
-  else {
+    $("#search-query").focus( function() {
+      $(this).val("");
+    });
+    fetchCurrentWeather(city);
+    fetchWeeklyForecast(city)
+  }  else {
     alert('Please enter valid search parameters!')
   }
   // $('search-query[placeholder]').html('')
-})
+});
 
 
-//\appid: fa9a0e4c4fd59f4be6924cdceb975e9b,
+//appid: fa9a0e4c4fd59f4be6924cdceb975e9b,
 
 
 //make a JSON file with country codes
