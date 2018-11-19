da3b4ea5345be70a312f4885d18223bd49bf9bb0
PGNQ <41378170+PGNQ@users.noreply.github.com>
2018-11-18 22:52:40 -0500

    Add city, state, and country to UI. Add comments. Project Functional.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 /*
 This weather app should accept City, State or City, Country input by the user. The browser should display the current weather conditions, and also the five-day forecast for that city.
+
+Current weather:
+User will see the rendered data from weatherDataCurrent, as well as the data from searchCity, searchState, and searchCountry.
+
+Five-Day Forecast:
+The view of the five-day forecast seen by the user should be render from weatherDataFiveDay. weatherDataForecast stores the data from openweathermap's forecast API call. weatherDataFiveDay is built after some statistical evaluation/processing of the bulk data from weatherDataForecast.
 */
 
 var weatherDataCurrent = [];
@@ -9,10 +15,10 @@ var searchCity = '';
 var searchState = '';
 var searchCountry = '';
 
-
-
+/*
+The view should be rendered by only two functions, renderWeatherCurrent() and renderWeatherFiveDay().
+*/
 var renderWeatherCurrent = function () {
-
   $(".current-weather-container").empty();
 
   var sourceCurrent   = $('#current-template').html();
@@ -22,9 +28,7 @@ var renderWeatherCurrent = function () {
   $(".current-weather-container").append(htmlCurrent);
 }
 
-
 var renderWeatherFiveDay = function () {
-
   for (let i = 0; i <=4; i++) {
     $("#day" + i).empty();
   }
@@ -38,6 +42,10 @@ var renderWeatherFiveDay = function () {
   }
 }
 
+/*
+The function fetch() should perform three API requests. The first request is to the mapQuest API to geocode the user requested "City, State" or "City, Country" query into coordinates. The next two API requests are to the openweathermap API, to get current weather and a weather forecast.
+Using the mapQuest API to geocode user queries provides a more robust handling of search requests, and also increases the accuracy that the returned weather data is what the user desired.
+*/
 var fetch = function (query) {
 
   var mapQuestKey =	"7a2hT4ahEixE9RFtJANEdClERCVnnfAd";
@@ -76,7 +84,6 @@ var fetch = function (query) {
     url: "http://www.mapquestapi.com/geocoding/v1/address?key="+ mapQuestKey +"&location=" + cityToCoordinatesParameter,
     dataType: "json",
     success: function(data) {
-      console.log(data);
       var longitude = data.results[0].locations[0].latLng.lng;
       var latitude = data.results[0].locations[0].latLng.lat;
       searchCity = data.results[0].locations[0].adminArea5;
@@ -90,10 +97,11 @@ var fetch = function (query) {
   });
 }
 
+
 var addWeatherDataCurrent = function (data) {
   weatherDataCurrent = [];
 
-  //loop through data and build that daily objects
+//The openweathermap API sometimes provides multiple conditions for the current weather. This function should grab all conditions of the current weather.
   var condition = function (data) {
     var conditionString = "";
     if (data.weather) {
@@ -108,7 +116,7 @@ var addWeatherDataCurrent = function (data) {
 
   var weather = {
     temperature: Math.round(data.main.temp),
-    city: data.name,
+    city: searchCity,
     condition: condition(data),
     icon: "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png",
     state: searchState,
@@ -118,7 +126,6 @@ var addWeatherDataCurrent = function (data) {
   weatherDataCurrent.push(weather);
   renderWeatherCurrent();
 }
-
 
 
 var addWeatherDataForecast = function (data) {
@@ -131,6 +138,7 @@ var addWeatherDataForecast = function (data) {
       icon: "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png",
       day: moment(data.list[i].dt_txt).format('dddd')
     }
+    //The forecast information received from openweathermap sometimes contains data from the current day.
     if (moment().format('dddd') !== weather.day) {
       weatherDataForecast.push(weather);
     }
@@ -143,6 +151,9 @@ var addWeatherDataForecast = function (data) {
 var buildFiveDayForecast = function () {
   weatherDataFiveDay = [];
 
+  /*
+  In order to statistically process all of the data from the forecast API, the data is grouped into 5 arrays by the day of the week.
+  */
   var day1 = weatherDataForecast.filter(e => e.day === weatherDataForecast[0].day);
   var day2 = weatherDataForecast.filter(e => e.day === weatherDataForecast[day1.length].day);
   var day3 = weatherDataForecast.filter(e => e.day === weatherDataForecast[day1.length + day2.length].day);
@@ -153,16 +164,19 @@ var buildFiveDayForecast = function () {
 
   temporaryArray.forEach(function(eachDay){
 
-    var temperaturesArray = eachDay.map(el => el.temperature);
+    /*
+    Each day of the five-day forecast should show the high temperature for the day, the low temperature for the day, a weather "condition" (ie rain, clear, clouds), that was the most reported condition for the day, and the icon associated with that most common weather condition.
+    */
+    var temperaturesArray = eachDay.map(e => e.temperature);
     var sumOfConditions = _.chain(eachDay).countBy("condition").value();
     var mostCommonCondition = Object.keys(sumOfConditions).reduce((a,b) => sumOfConditions[a] > sumOfConditions[b] ? a : b);
-    var elementOfCommonCondition = eachDay.find(el => el.condition === mostCommonCondition);
+    var elementOfCommonCondition = eachDay.find(e => e.condition === mostCommonCondition);
     var mostCommonIcon = elementOfCommonCondition.icon.replace('n.png', 'd.png');
 
     var dayOfFiveDay = {
       day: eachDay[0].day,
-      temperature_high: temperaturesArray.reduce((a, el) => Math.max(a, el)),
-      temperature_low: temperaturesArray.reduce((a, el) => Math.min(a, el)),
+      temperature_high: temperaturesArray.reduce((a, b) => Math.max(a, b)),
+      temperature_low: temperaturesArray.reduce((a, b) => Math.min(a, b)),
       condition: mostCommonCondition,
       icon: mostCommonIcon
     }
@@ -171,10 +185,9 @@ var buildFiveDayForecast = function () {
 }
 
 
-//add event listener to search button
-//enforce certain
-
 $('#citySearchButton').on('click', function () {
-  var query = $('#citySearch').val();
-  fetch(query);
+  if ($('#citySearch').val()) {
+    var query = $('#citySearch').val();
+    fetch(query);
+  }
 });
