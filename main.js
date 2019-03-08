124a1f6e2dcccdc0eae152d117f4174bf2b986cf
andres alcocer <andresalcocer7@yahoo.com>
2019-03-08 16:47:12 -0500

    Now able to display main current weather, also validated user input

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ const weatherModule = () => {
     condition: '',
   }
 
+  /** getter */
+  var getAttribute = (attribute) => {
+    if (state.hasOwnProperty(attribute)) {
+      return state[attribute];
+    }
+  }
+
   /**
    * Gets the weather forecast with the user input as a query. Makes a
    * call to the OpenWeatherMap Api
@@ -23,35 +30,57 @@ const weatherModule = () => {
    * to OpenWeatherMap.
    * @param { String } city - The city to get the current weather
    */
-  var getCurrentWeather = async (city) => {
-    const apiCall = await fetch('http://api.openweathermap.org/data/2.5/weather?q=durham&appid=60271f8873cd6fca6c2b2ce6c281a2c6');
-    const data = await apiCall.json();
-    console.log('current weather: ', data);
+  var getCurrentWeather = (city) => {
+    const res = fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${state.API_KEY}`)
+      .then(result => {
+        if (result.ok) {
+          return result.json();
+        } else {
+          return false;
+        }
+      })
+      .then(data => {
+        const fahrenheit = Math.ceil((data.main.temp - 273) * 9 / 5 + 32);
+        state['city'] = data.name;
+        state['temperature'] = fahrenheit;
+        state['condition'] = data.weather[0].main;
+        return true;
+      })
+      .catch(err => {
+        return false;
+      });
+
+    return res;
   }
 
   return {
     getWeatherForecast: getWeatherForecast,
-    getCurrentWeather: getCurrentWeather
+    getCurrentWeather: getCurrentWeather,
+    getAttribute: getAttribute
   }
-}
-
-const weather = weatherModule();
-weather.getWeatherForecast();
-weather.getCurrentWeather();
+};
 
 
 $(document).ready(function () {
-  $('#get-weather').on('click', function (e) {
+  const weather = weatherModule();
+
+  $('#get-weather').on('click', async function (e) {
     e.preventDefault();
-    $('.current-weather--container').empty();
+    var searchInput = $(e.target).closest('.form-container').find('#weather-input').val();
 
-    var template = Handlebars.compile($('#weather-template').html());
-    var newHtml = template({ degrees: 41, city: 'Durham NC', weather: 'cloudy' });
-    $('.current-weather--container').append(newHtml);
+    var resultIsOk = await weather.getCurrentWeather(searchInput);
+    if (resultIsOk) {
+      var template = await Handlebars.compile($('#weather-template').html());
 
-    var forecastTemplate = Handlebars.compile($('#forecast-weather-template').html());
-    var newForecastHtml = forecastTemplate({ degrees: 68, city: 'Chapel Hill', weather: 'sunny' });
-    $('.forecast-weather--container').append(newForecastHtml);
+      var newHtml = template({
+        degrees: weather.getAttribute('temperature'),
+        city: weather.getAttribute('city'),
+        weather: weather.getAttribute('condition')
+      });
+      $('.current-weather--container').append(newHtml);
+    } else {
+      alert(`Sorry the location "${searchInput}" was not found. Please check your search criteria and try again.`);
+    }
 
   });
 });
