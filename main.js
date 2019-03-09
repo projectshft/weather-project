7f37dfc0b9832cfc3a4e7d86823f1cb42d891788
Knox <knox.brand@gmail.com>
2019-03-08 19:22:16 -0500

    Framed up module design - searchFunc, renderFunc created and working.

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
-//72e02d7e6f07aa32ae20388abf818118 API key
+//72e02d7e6f07aa32ae20388abf818118 API key backup
+//data.name city name
+//data.weather.main condition
+//data.main.temp (in Kelvin) (data.main.temp − 273.15) × 9/5 + 32
 
-fetch('https://api.openweathermap.org/data/2.5/weather?q=Durham&APPID=72e02d7e6f07aa32ae20388abf818118')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
-  });
+
+var WeatherApp = function() {
+
+  var searchFunc = function() { //Hard coded API call function
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=Durham&APPID=72e02d7e6f07aa32ae20388abf818118')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      renderFunc(myJson);
+    });
+  }
+
+  var renderFunc = function(data) { //Render function - testing with console
+    console.log("Temp:", data.main.temp, " Name:", data.name, " Conditions:", data.weather[0].main);
+  }
+
+  return {
+    searchFunc: searchFunc
+  }
+}
+
+var myWeatherApp = WeatherApp();
+myWeatherApp.searchFunc(); //testing module
