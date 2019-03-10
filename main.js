1c7a4086df42c06fa5110843f2ee669b07283336
Jason Park <jason.j.park79@gmail.com>
2019-03-09 19:10:27 -0500

    wrote out though process in JS file

---
+++ main.js
@@ -0,0 +1,7 @@
+// 1. Get HTML down & libraries rolled out.
+// 2. Link HTML to Javascript via jquery
+// 3. Get Handlebars in
+// 4. Go through API documentation and gain understanding of what needs to be drawn in and how.
+// 5. Get API to pull properly and link files
+// 6. Make pretty with Bootstrap & css
+// 7. Profit???
