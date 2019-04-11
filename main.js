1b9b87cb7bb98a4ac2e88f48ec3adc2b3acdf6cb
Luc Saunders <lucsaunders@gmail.com>
2019-04-10 22:09:57 -0400

    Refine 5-day-forecast api call. Add weather icons. Add use-current-location and default-location settings. Improve styling.

--- main.js
+++ main.js
@@ -1,133 +1,160 @@
-// USE LOCAL STORAGE to store location
-// USE MOMENT for timestamp
-// USE POJO(s) to store data received from API
-
-/*****************************************
-
+/*************************************************
    OPENWEATHER API doesn't work with "https://"
    Browser address bar must use "http://"
-   
-******************************************/
+**************************************************/
 
-// OpenWeather API Keys
+// OpenWeather API Keys (2)
 
-// Key: 2b2a168d8dac44dd31cfd36829f44f93   Name: Lucas
 // Key: add32f7253f0ba69178d0ca6ee5ef2b3   Name: Default
+// Key: 2b2a168d8dac44dd31cfd36829f44f93   Name: Lucas
 
-'use strict';
+/**********************************************************/
 
-/*****************************************
-// Add a function to return a formatted date.
+// Global variables
+const apiKey = 'add32f7253f0ba69178d0ca6ee5ef2b3';
+var locationInput;
+var days = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
 
-// Get current date if numbersOfDaysToAdd = 0
-var getFutureDate = function(day) {};
-******************************************/
-/*****************************************
-// Add a function to determine cloud conditions
+getWeather = () => {
+  var current;
+  var fiveDayForecast = [{}, {}, {}, {}, {}];
+  /************************************
+     API call for Current Weather
+  *************************************/
+  var initApiCall = () => {
+    $.getJSON(
+      'http://api.openweathermap.org/data/2.5/weather?' +
+        locationInput +
+        '&units=imperial&appid=' +
+        apiKey,
+      function(weatherData) {
+        current = {};
+        // Round temperature received from OWM to integer, no decimals
+        current.temp = Math.round(weatherData.main.temp) + '°';
+        current.city = weatherData.name;
+        current.conditions = weatherData.weather[0].description;
+        // Reference OWM's weather icons
+        current.icon =
+          'http://openweathermap.org/img/w/' +
+          weatherData.weather[0].icon +
+          '.png';
+        renderCurrentWeather();
+        getFiveDayForecast();
+      }
+    );
+  };
 
-var getConditions = function(cloudiness) {
-  // var getConditions = (cloudiness) => { }
-  var conditions;
-  //  ...   ...   ...
-  return conditions;
+  /**************************************************
+    Render current weather to Handlebars template
+  ***************************************************/
+  var renderCurrentWeather = () => {
+    $('.current-weather-display').empty();
+    var source = $('#weather-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template(current);
+    $('.current-weather-display').append(newHTML);
+    defaultCity();
+  };
+
+  /************************************
+     API call for Five-Day Forecast
+  *************************************/
+  var getFiveDayForecast = () => {
+    $.getJSON(
+      'http://api.openweathermap.org/data/2.5/forecast?' +
+        locationInput +
+        '&units=imperial&appid=' +
+        apiKey,
+      // Loop: iterate five times to fill out five-day forecast
+      function(weatherData) {
+        for (let i = 0; i < 5; i++) {
+          fiveDayForecast[i].conditions =
+            weatherData.list[i * 8].weather[0].description;
+          // Round temperature received from OWM to integer, no decimals
+          fiveDayForecast[i].temp =
+            Math.round(weatherData.list[i * 8].main.temp) + '°';
+          // Reference OWM's weather icons
+          fiveDayForecast[i].icon =
+            'http://openweathermap.org/img/w/' +
+            weatherData.list[i * 8].weather[0].icon +
+            '.png';
+          var day = new Date(weatherData.list[i * 8].dt_txt);
+          fiveDayForecast[i].day = days[day.getDay()];
+        }
+        renderFiveDayForecast();
+      }
+    );
+  };
+
+  /**************************************************
+    Render five-day forecast to Handlebars template
+  ***************************************************/
+  var renderFiveDayForecast = () => {
+    $('.five-day-forecast-display').empty();
+    for (let i = 0; i < 5; i++) {
+      var source = $('#five-day-forecast-template').html();
+      var template = Handlebars.compile(source);
+      var newHTML = template(fiveDayForecast[i]);
+      $('.five-day-forecast-display').append(newHTML);
+    }
+  };
+
+  var getCurrentWeather = () => {
+    return current.city;
+  };
+
+  return {
+    initApiCall,
+    getCurrentWeather
+  };
 };
-******************************************/
 
-// A function to fill out the main-weather Handlebar template
-var getWeather = function(theWeather) {
-  // var getWeather = (theWeather) => {
+app = getWeather();
 
-  // City label
-  $('#results').html(theWeather.city.name); // is .city.name right here???
-
-  // Turn template into html
-  var source = $('#main-weather-template').html();
-
-  // Compile template html to Handlebars
-  var template = Handlebars.compile(source);
-
-  // Create loop to allow for a flexible number of days for which to retrieve weather data
-
-  // In the API response object, "list" is the key name paired with the weather data.
-  for (var i = 1; i < theWeather.list.length; i++) {
-    // Get future dates
-    var futureDate = futureDate(i);
-    var cloudCondition = getConditions(theWeather.list[i].clouds);
-
-    // Weather object for Handlebars
-    // SEE myPlay notes
-    var weatherObj = {
-      now: dayInTheFuture,
-      cloudInfo: cloudsCondition.cloudNumber,
-      cloudInfoText: cloudsCondition.cloudText
-    };
-
-    // Fill template with weatherObj
-    var newHTML = template(weatherObj);
-
-    // Append the new html to the page
-    $('.page-container').append(newHTML);
-  }
-};
-
-// A function to call api.openweathermap.com
-
-var APICall = function(theCity) {
-  // API url
-  var weatherUrl =
-    'api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}';
-
-  // API key and an alternate, second key
-  var apiKey = 'add32f7253f0ba69178d0ca6ee5ef2b3';
-
-  var apiKeyAlt = '2b2a168d8dac44dd31cfd36829f44f93';
-
-  // Set unit type to imperial
-  var unitType = 'imperial';
-
-  // Total number of days for forecast
-  var daysTotal = 5; // should this be 5? 6? 7? 8?
-
-  // jQuery API call
-};
-
-// Click handler for search bar button
-// Make APICall with textarea input
-$('#getWeather').on('click', function(e) {
-  // Prevent natural form submit event
-  e.preventDefault();
-
-  // Ensure search box has value
-  // IS THIS TAKEN CARE OF BY "REQUIRED" IN HTML CLASS???
-  if (
-    $('#city-name')
-      .val()
-      .trim() === '' ||
-    $('#city-name')
-      .val()
-      .trim() === null
-  ) {
-    // No action taken if textarea is not filled out
-    return;
-  } else {
-    // Clear the textarea
-    $('.section').remove();
-
-    // Get textarea string and invoke APICall function
-    var cityName = $('#city-name')
-      .val()
-      .trim();
-    $('#city-name').val('');
-    APICall(cityName);
-  }
+/*********************
+   Event handlers
+**********************/
+// "Search" button
+$('.search').click(function() {
+  locationInput = 'q=';
+  locationInput += $('.location-input').val();
+  locationInput += ',US';
+  app.initApiCall();
 });
 
-/*****************************************
-    
-  
-******************************************/
+// "Use my current location" button
+$('.location').click(function() {
+  // Invoke read-only property "navigator.geolocation" to get user's current location. (Returns a geolocation object.)
+  navigator.geolocation.getCurrentPosition(function(position) {
+    locationInput =
+      'lat=' + position.coords.latitude + '&lon=' + position.coords.longitude;
+    app.initApiCall();
+  });
+});
 
-/*****************************************
- * From openweathermap website: "Better call API by city ID instead of a city name, city coordinates or zip code to get a precise result. The list of cities' IDs is availabe on their site" (JSON file)
-  
-******************************************/
+// "Set as default location" button
+var defaultCity = () => {
+  $('#default-city').click(function() {
+    locationInput = 'q=';
+    locationInput += app.getCurrentWeather();
+    locationInput += ',US';
+    localStorage.setItem('city', locationInput);
+    alert('Default location set to ' + app.getCurrentWeather() + '.');
+  });
+};
+
+$(document).ready(function() {
+  // Reference local storage for location
+  if (localStorage.getItem('city')) {
+    locationInput = localStorage.getItem('city');
+    app.initApiCall();
+  }
+});
