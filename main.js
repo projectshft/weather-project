d1f0a7b2446afb40ff3cd1770555aab6e4687b1c
Jake Galligan <galligan1996@gmail.com>
2019-06-21 14:14:46 -0400

    Updating function which stores user query when the search button is clicked to add a conditional that checks to see if string was inserted into input form

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
+//model to store data weather that is retrieved from API
 var weatherData = [];
 
-
+//when the search button is clicked the input value from the input is taken and then put into a function to fetch it from the API
 $('#search-button').on('click', function() {
-  var searchQuery = $('#search-query').val();
-  fetch(searchQuery);
+  var search = $('#search-query').val();
+  if (search !== '') {
+    fetch(search);
+} else {
+    alert("ERROR: Please insert the name of a city");
+}
 })
