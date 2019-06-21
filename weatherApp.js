953e2493367020a7ce6459da22526846432d9e33
dposse <dposse.125@gmail.com>
2019-06-21 10:36:25 -0400

    Handlebars template working with hardcoded data

--- weatherApp.js
+++ weatherApp.js
@@ -10,10 +10,24 @@
   * [X] create static title and search elements
   * [X] section containers where handlebar templates will be inserted
   * [ ] handlebar templates
-  *   [ ] main centered current weather
+  *   [X] main centered current weather
   *   [ ] daily forecast - maybe use {{#each ...}}
   * 
   * [ ] get api key
   * [ ] fetch request at current location
   * [ ] insert data into handlebar templates
   */
+
+  const WeatherApp = () => {
+
+
+
+  };
+
+
+  const app = WeatherApp();
+
+  //testing handlebars template
+  const currentWeatherTemplate = Handlebars.compile($('#current-weather-content').html());
+  const testItem = currentWeatherTemplate({ city: 'Durham', temperature: '80', condition: 'sunny' });
+  $('#current-weather').append(testItem);
