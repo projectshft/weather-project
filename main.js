9d5664070bc184d2f54c8ce9d4c4782983915592
Kate Kremer <katrinakremer@gmail.com>
2019-06-21 14:39:11 -0400

    add current weather view

--- main.js
+++ main.js
@@ -2,33 +2,41 @@
 var CurrentWeatherModel = function(data){
 
   //current weather data object, uses data info from API
-  var currentWeatherModel = {
+  var attributes = {
     city: data.name,
     currentTemp: data.main.temp,
-    currentConditions: data.weather[0].description
+    currentConditions: data.weather[0].description,
+    iconURL: data.weather[0].icon
   }
 
   //add get and set methods
 
 
   return{
-    currentWeatherModel: currentWeatherModel
+  attributes: attributes
   }
 };
 
 //current weather view
 var updateCurrentWeatherView = function(model){
+  //empty current view
+  $('.card').empty();
   //use handlebars template to render
+  var source = $('#current-weather-template').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(model);
+  $('.card').append(newHTML);
 };
 
 var fetchCityWeather = function(city){
   $.ajax({
     method: "GET",
-    url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=10bb4951fc681d2db61a0d80f9c3ec1a",
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=imperial"+ "&APPID=10bb4951fc681d2db61a0d80f9c3ec1a",
     dataType: "json",
-    success: function(data, city){
-      console.log(CurrentWeatherModel(data));
-      //call updateCurrentWeatherView
+    success: function(data){
+      var currentModel = CurrentWeatherModel(data);
+      console.log(currentModel);
+      updateCurrentWeatherView(currentModel);
     },
   });
 };
