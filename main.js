1a619428c1a8e7a5bce50a8c1b46361fb5ead039
Kyle Kimber <kimber.kyle@gmail.com>
2019-06-21 17:15:11 -0400

    Hardcoded json and made handlebars template.

---
+++ main.js
@@ -0,0 +1,104 @@
+// var fetch = function (zip) {
+//   $.ajax({
+//     method: "GET",
+//     url: "api.openweathermap.org/data/2.5/forecast?zip=" + zip + "&appid=991e4a2975c05e3ab3fbb3b9bdc470c9",
+//     dataType: "json",
+//     success: function(data) {
+//       console.log(data);
+//       // addForcast(data);
+//     },
+//     error: function(textStatus) {
+//       console.log(textStatus);
+//     }
+//   });
+// };
+// // var emptyWeather = function() {
+// //   $('.weather').empty()
+
+// //render function which will listen to the update of the empty weather obj and populate the forcast based on search query into div 
+// var renderForecast = function () {
+//   $('.weather').empty();
+
+//       var postTemplate = Handlebars.compile($('#forecast-template').html());
+//       var newHTML = postTemplate(postForecast);
+
+//       // var postView = View(postBooks, postTemplate);
+
+//       $('.weather').append(newHTML);
+//   }
+// };
+
+// var forecast ={
+//   "coord": {
+//     "lon": -1.58,
+//     "lat": 54.78
+//     },
+//     "weather": [
+    
+//     }
+
+
+
+// $('.search').on('click', function () {
+//   var search = $('#search-query').val();
+
+//   fetch(search);
+// });
+
+
+var currentWeather = {
+  "coord": {"lon": -122.08,"lat": 37.39},
+  "weather": [
+    {
+      "id": 800,
+      "main": "Clear",
+      "description": "clear sky",
+      "icon": "01d"
+    }
+  ],
+  "base": "stations",
+  "main": {
+    "temp": 296.71,
+    "pressure": 1013,
+    "humidity": 53,
+    "temp_min": 294.82,
+    "temp_max": 298.71
+  },
+  "visibility": 16093,
+  "wind": {
+    "speed": 1.5,
+    "deg": 350
+  },
+  "clouds": {
+    "all": 1
+  },
+  "dt": 1560350645,
+  "sys": {
+    "type": 1,
+    "id": 5122,
+    "message": 0.0139,
+    "country": "US",
+    "sunrise": 1560343627,
+    "sunset": 1560396563
+  },
+  "timezone": -25200,
+  "id": 420006353,
+  "name": "Mountain View",
+  "cod": 200
+}
+
+console.log(currentWeather.name);
+console.log(currentWeather.main.temp);
+console.log(currentWeather.weather[0].main);
+
+var renderWeather = function () {
+  $(".weather").empty();
+  var source = $('#weather-template-current').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(currentWeather);
+  $('.weather').append(newHTML);
+};
+
+console.log(currentWeather)
+renderWeather()
+
