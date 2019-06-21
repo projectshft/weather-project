fa5ce8b2095907ee5e738a1caf46d29bd7b8129b
snpdolan <snpdolan@gmail.com>
2019-06-21 14:56:03 -0400

    implemented set weather function using example url

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// const axios = require('axios');
 var my_key = 'c8ff39d1982ba18e6de83f8d7febeb2b';
 var city_call = 'api.openweathermap.org/data/2.5/weather?id=2172797';
 var icon_str = 'http://openweathermap.org/img/w/10d.png';
@@ -7,34 +8,51 @@ var WeatherApp = function(){
 
   //data model storage for current city and 5 day forecast
   var weatherData = {};
-
+  var changeCallback = null;
+  
   //set where in our DOM we'll be rendering weather info
   var $weather = $('.weather');
 
   //function to fetch our current city info
-  var fetchCurrentWeather = function () {
+  var _fetchCurrentWeather = function () {
+    //update URL based on location string
     let currentWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=London,UK&units=imperial&appid=${my_key}`;
-    let currentCityWeather = fetch(currentWeatherURL).then(response => response.json()).then(data => console.log(data));
-
-    //console.log(currentCityWeather);
+    return fetch(currentWeatherURL).then(response => response.json()).then(data =>{
+      console.log(data); 
+      return data;
+      });
   };
+
+  var _setWeatherData = async function () {
+    console.log('calling fetch weather...');
+    var result = await _fetchCurrentWeather();
+    weatherData.temp = result.main.temp;
+    weatherData.name = result.name;
+    weatherData.description = result.weather[0].description;
+    weatherData.icon = `http://openweathermap.org/img/w/${result.weather[0].icon}.png`;
+
+    _renderCurrentWeather(weatherData);
+  };
+  
 
   //function to fetch our 5 day forecast info
 
   //render function for current day
-  var renderCurrentWeather = function () {
-
+  var _renderCurrentWeather = function (weatherData) {
+    var currentWeatherTemplate = Handlebars.compile($('#current-weather').html());
+    var currentWeatherHTML = currentWeatherTemplate(weatherData);
+    $weather.append(currentWeatherHTML);
   }
 
   //render function for 5 day forecast
-  var renderFiveDayForecast = function () {
+  var _renderFiveDayForecast = function () {
 
   }
 
   //general render function
   var renderWeather = function(){
     //determine weather model type current/5day
-
+    _setWeatherData();
     //update for both
 
   }
@@ -42,13 +60,13 @@ var WeatherApp = function(){
   //function to grab Handlebars model type
   
   return {
-    fetchCurrentWeather: fetchCurrentWeather
+    renderWeather: renderWeather
   };  
   
 };
 
 var app = WeatherApp();
-app.fetchCurrentWeather();
+app.renderWeather();
 
 //respond to search click
 $('.weather').on('click', '#search-button', function (e) {
