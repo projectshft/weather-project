9962ba31295bb6dd73d56e8ce885a26149c9dbf0
dposse <dposse.125@gmail.com>
2019-06-21 09:39:57 -0400

    Initial plan

---
+++ weatherApp.js
@@ -0,0 +1,18 @@
+/**
+ * Weather Project
+ * Author: Daniel Posse
+ * 
+ *  uses openweathermap.org api to get weather at user specified city
+ */
+
+ /**
+  * [ ] create static title and search elements
+  * [ ] section containers where handlebar templates will be inserted
+  * [ ] handlebar templates
+  *   [ ] main centered current weather
+  *   [ ] daily forecast - maybe use {{#each ...}}
+  * 
+  * [ ] get api key
+  * [ ] fetch request at current location
+  * [ ] insert data into handlebar templates
+  */
