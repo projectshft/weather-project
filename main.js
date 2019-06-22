91f34d945a217399e8847360c9a9bc815a2c6582
K. Bender <kristinajbender@gmail.com>
2019-06-22 14:10:24 -0400

    Add icons, drop-down menu for country, form validation, and error messages

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// open weather map *API key*: ed5a139b64c682afc125b2cec0f6c859
-// api.openweathermap.org/data/2.5/weather?id=4464368&APPID=ed5a139b64c682afc125b2cec0f6c859
-// https://api.openweathermap.org/data/2.5/weather?id=4464368&APPID=ed5a139b64c682afc125b2cec0f6c859
-// https://api.openweathermap.org/data/2.5/weather?id=4464368&units=imperial&APPID=ed5a139b64c682afc125b2cec0f6c859
-
 var apiKey = "ed5a139b64c682afc125b2cec0f6c859"; 
 var STORAGE_ID = 'my-saved-location';
 var $loading = $('#loader');
+var $country_selection = $('#country-selection');
+
+$( document ).ready(function() {
+  $country_selection.html(countryList);
+});
 
 var WeatherApp = function () {
   var $current_weather = $('#current-weather');
@@ -16,11 +16,24 @@ var WeatherApp = function () {
     renderCurrentWeather();
   });
 
+  var forecastWeatherModel = Model(); 
+  forecastWeatherModel.change(function () {
+    renderWeatherForecast();
+  });
+
   var renderCurrentWeather = function () {
     $current_weather.empty();
     var currentWeatherTemplate = Handlebars.compile($('#current-weather-template').html()); 
     var currentWeatherView = View(currentWeatherModel, currentWeatherTemplate); 
     $current_weather.html(currentWeatherView.render());
+  };
+
+  var renderWeatherForecast = function () {
+    // loop through for each of the five subsequent days
+    $day_weather.empty();
+    var forecastWeatherTemplate = Handlebars.compile($('#weather-template').html()); 
+    var forecastWeatherView = View(forecastWeatherModel, forecastWeatherTemplate); 
+    $day_weather.html(forecastWeatherView.render());
   };
   
   var setCurrentWeather = function (data) {
@@ -28,11 +41,25 @@ var WeatherApp = function () {
     var temperature = data.main.temp ||  null;
     var condition = data.weather[0].main || null;
     var icon = data.weather[0].icon || null; 
+    var country = data.sys.country || null; 
 
     currentWeatherModel.set('location', location); 
     currentWeatherModel.set('temperature', temperature); 
     currentWeatherModel.set('condition', condition); 
-    currentWeatherModel.set('icon', icon); 
+    currentWeatherModel.set('icon', 'https://openweathermap.org/img/w/' + icon + '.png');
+    currentWeatherModel.set('country', country); 
+  };
+
+  var setWeatherForecast = function (data) {
+    var temperature = data.list[0].main.temp ||  null;
+    var condition = data.list[0].weather[0].main || null;
+    var icon = data.list[0].weather[0].icon || null; 
+    var day = "day";
+
+    forecastWeatherModel.set('day', day); 
+    forecastWeatherModel.set('temperature', temperature); 
+    forecastWeatherModel.set('condition', condition); 
+    forecastWeatherModel.set('icon', 'https://openweathermap.org/img/w/' + icon + '.png'); 
   };
 
   var fetchCurrentWeather = function (query) {
@@ -49,6 +76,27 @@ var WeatherApp = function () {
           setCurrentWeather(data);
         },
         error: function(jqXHR, textStatus, errorThrown) {
+          $loading.hide();
+          $current_weather.html("Sorry, no locations match your search.");
+          console.log(textStatus);
+        }
+      });
+  };
+
+  var fetchWeatherForecast = function (query) {
+    $.ajax({
+        method: "GET",
+        url: 'https://' + "api.openweathermap.org/data/2.5/forecast?q=" + query + "&units=imperial" + "&APPID=" + apiKey, 
+        dataType: "json",
+        beforeSend: function() {
+          $loading.html('<img src="ajax-loader.gif"/>');
+          $loading.show();
+        },
+        success: function(data) {
+          $loading.hide();
+          setWeatherForecast(data);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
           console.log(textStatus);
         }
       });
@@ -57,67 +105,38 @@ var WeatherApp = function () {
   return {
     renderCurrentWeather: renderCurrentWeather,
     setCurrentWeather: setCurrentWeather,
-    fetchCurrentWeather: fetchCurrentWeather
+    fetchCurrentWeather: fetchCurrentWeather,
+    renderWeatherForecast: renderWeatherForecast,
+    setWeatherForecast: setWeatherForecast,
+    fetchWeatherForecast: fetchWeatherForecast
   }; 
 
 }; 
 
 /*
-var currentWeatherObjectTest = {
+var weatherObjectTest = {
     location: 'Durham, NC',
     temperature: 88,
     condition: 'Cloudy'
 };
-
-var addWeather = function (data) {
-  console.log(JSON.stringify(data));
-      var location = data.name || null;
-      var temperature = data.main.temp ||  null;
-      var condition = data.weather[0].main || null;
-
-      var weatherObject = {
-        location: location,
-        temperature: temperature,
-        condition: condition
-      };
-      console.log(JSON.stringify(weatherObject));
-
-    var myTemplate = Handlebars.compile($('#current-weather-template').html()); 
-    var myView = myTemplate(weatherObject); 
-    $('#current-weather').html(myView);
-
-    var myTemplate2 = Handlebars.compile($('#weather-template').html()); 
-    var myView2 = myTemplate2(weatherObject); 
-    $('.day-weather').html(myView2);
-   };
-
-
-
-var fetch = function (query) {
-    $.ajax({
-        method: "GET",
-        url: 'https://' + "api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial" + "&APPID=" + apiKey, 
-        //"api.openweathermap.org/data/2.5/weather?q=" + query,
-        //durham, nc ?id=4464368
-        dataType: "json",
-        beforeSend: function() {
-          $loading.html('<img src="ajax-loader.gif"/>');
-          $loading.show();
-        },
-        success: function(data) {
-          $loading.hide();
-          addWeather(data);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-          console.log(textStatus);
-        }
-      });
-  };
+console.log(JSON.stringify(data));
 */
 
 var app = WeatherApp();
 
 $('.search').on('click', function () { 
-    var search = $('#search-query').val();
+    var cityValue = $('#search-query').val() || null; 
+    //console.log(cityValue);
+    if (!cityValue) { 
+      $('#current-weather').html("<div>Enter a city name.</div>"); 
+      $('.day-weather').html(''); 
+    } 
+    else { 
+    var countryValue = $country_selection.val();
+    //console.log(countryValue);
+    var search = (!countryValue)? cityValue : cityValue + ',' + countryValue;
+    //console.log(search);
     app.fetchCurrentWeather(search); 
+    app.fetchWeatherForecast(search); 
+  }
 });
