4e8c2ba0a1e2c847543289c2f796f7644d7a6eb5
Jake Galligan <galligan1996@gmail.com>
2019-06-22 18:22:51 -0400

    Adding function which adds ability to retrieve 5 day forecast from API, as well as a render function (including handlebar templates) which render the function after the forecast is added to the model array

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 //model to store data weather that is retrieved from API
 var weatherData = [];
 
-//when fetch is called get the serach query from the API
-var fetch = function(query) {
-
-  //setting base url for queries
-  var queryUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ query.replace(' ', '%20') + "&units=imperial&APPID=40767c2b5aacbb8e4c3a6c5162279cd6"
-
+//when fetchCurrentDay is called it gets the current weather data from the API for the given search query
+var fetchCurrentDay = function(query) {
+  //setting base url for current day temperature queries
+  var currentDayQueryUrl = "https://api.openweathermap.org/data/2.5/weather?q="+ query.replace(' ', '%20') + "&units=imperial&APPID=40767c2b5aacbb8e4c3a6c5162279cd6";
   $.ajax ({
     method: "GET",
-    url:queryUrl,
+    url:currentDayQueryUrl,
     dataType: "json",
     success: function (data) {
       addWeather(data);
@@ -19,34 +17,80 @@ var fetch = function(query) {
   }
 });
 };
-//take the data returned from the api and then push it to our model array
+
+//when fetchForecast is called it gets the 5 day forecast weather data from the API for the given search query
+var fetchForecast = function(query) {
+  //setting base url for forecast temperature queries
+  var forecastQueryUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+ query.replace(' ', '%20') + "&units=imperial&APPID=40767c2b5aacbb8e4c3a6c5162279cd6";
+  $.ajax ({
+    method: "GET",
+    url:forecastQueryUrl,
+    dataType: "json",
+    success: function (data) {
+      addWeather2(data);
+    },
+    error: function (textStatus) {
+      alert(textStatus);
+    }
+  })
+};
+
+
+//take the current weather data returned from the api and then push it to our model array
 var addWeather = function(data) {
   weatherData.push(data);
 //after data is pushed call a function that renders it
   renderCurrentDay();
 };
-//using handlebars render the temperature location and weather description to the view
+//take the 5 day forecast data returned form the api and then push it to our model array
+var addWeather2 = function (data) {
+  weatherData.push(data);
+  renderForecast();
+}
+//using handlebars render the temperature location and weather description to the view for the curent day
 var renderCurrentDay = function() {
 $('.todays-weather').empty();
-for (let i = 0; i < weatherData.length; i++) {
+  var imageIcon = weatherData[0].weather[0].icon;
+  //set handlebars template
   var source = $('#current-weather-template').html();
   var template = Handlebars.compile(source);
   var newHTML = template({
-    temperature: weatherData[i].main.temp,
-    city: weatherData[i].name,
-    weatherDescription: weatherData[i].weather[0].description
+    temperature: weatherData[0].main.temp,
+    city: weatherData[0].name,
+    country: weatherData[0].sys.country,
+    weatherDescription: weatherData[0].weather[0].description,
+    icon: "http://openweathermap.org/img/w/" + imageIcon + ".png"
   })
     $('.todays-weather').append(newHTML);
-}
-
-
 };
+
+// using handlebars to render the temperature loacton and weither dsicpriton to the view for a forecast
+var renderForecast = function () {
+  $('.forecast').empty();
+  //set handlebars template 
+  for (let i=0; i < weatherData[1].list.length; i++) {
+    var imageIcon = weatherData[1].list[i].weather[0].icon;
+    var source = $('#current-weather-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template({
+      temperature: weatherData[1].list[i].main.temp,
+      city: weatherData[1].city.name,
+      country: weatherData[1].city.country,
+      weatherDescription: weatherData[1].list[i].weather[0].description,
+      icon: "http://openweathermap.org/img/w/" + imageIcon + ".png"
+    })
+      $('.forecast').append(newHTML);
+  }
+};
+
+
 
 //when the search button is clicked the input value from the input is taken and then put into a function to fetch it from the API
 $('#search-button').on('click', function() {
   var search = $('#search-query').val();
   if (search !== '') {
-    fetch(search);
+    fetchCurrentDay(search);
+    fetchForecast(search);
 } else {
     alert("ERROR: Please insert the name of a city");
 }
