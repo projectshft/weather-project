5493b4dc6d35477a8e1c7f696ae14b7aeec32467
K. Bender <kristinajbender@gmail.com>
2019-06-24 07:37:18 -0400

    Clean up comments for final submission

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
+// Populate drop-down menu with list of countries (keeps html file shorter). 
 $(document).ready(function() {
   $('#country-selection').html(countryList);
 });
 
+// Create weather application for managing model/view. 
 var WeatherApp = function() {
+  // Set variables for API key and commonly-used jquery selectors. 
   var apiKey = "ed5a139b64c682afc125b2cec0f6c859";
   var $loading = $('#loader');
   var $current_weather = $('#current-weather');
   var $day_weather = $('.day-weather');
 
+  // Create model for current weather and watch for changes. 
   var currentWeatherModel = Model();
   currentWeatherModel.change(function() {
     renderCurrentWeather();
   });
 
+  // Create collection for five forecast models and watch for changes. 
   var forecastWeatherCollection = Collection();
   forecastWeatherCollection.change(function() {
     renderWeatherForecast();
   });
 
+  // Function to render current weather: clear div and create new view with template. 
   var renderCurrentWeather = function() {
     $current_weather.empty();
     var currentWeatherTemplate = Handlebars.compile($('#current-weather-template').html());
@@ -25,20 +31,23 @@ var WeatherApp = function() {
     $current_weather.html(currentWeatherView.render());
   };
 
+  // Function to render weather forecast: clear divs and create new views with template. 
   var renderWeatherForecast = function() {
     $day_weather.empty();
-    // Loop through collection to render each of the five days. 
+    // Loop through models in collection to render each of the five days. 
     for (var i = 0; i < forecastWeatherCollection.models.length; i++) {
       var dayModel = forecastWeatherCollection.models[i];
       var forecastWeatherTemplate = Handlebars.compile($('#weather-template').html());
       var forecastWeatherView = View(dayModel, forecastWeatherTemplate);
+      // Put each model in div with corresponding class name (by index). 
       $('.d' + i).html(forecastWeatherView.render());
     };
   };
 
+  // Function uses data from API call to set current weather model attributes. 
   var setCurrentWeather = function(data) {
     var location = data.name || null;
-    var temperature = Math.round(data.main.temp) || null;
+    var temperature = Math.round(data.main.temp) || null; // Rounded temps for better UX. 
     var condition = data.weather[0].main || null;
     var icon = data.weather[0].icon || null;
     var country = data.sys.country || null;
@@ -46,18 +55,23 @@ var WeatherApp = function() {
     currentWeatherModel.set('location', location);
     currentWeatherModel.set('temperature', temperature);
     currentWeatherModel.set('condition', condition);
+    // Include source information with icon code. 
     currentWeatherModel.set('icon', 'https://openweathermap.org/img/w/' + icon + '.png');
     currentWeatherModel.set('country', country);
   };
 
+  // Function transforms data from API call so it can be used in weather forecast models.
   var createFiveDayObjectsArray = function(data) {
-    // Transform items in forecast list to objects with day/temp/condition/icon/hour.
+    // Map items from forecast list array to new array of objects with day/temp/condition/icon/hour.
     var mappedDataArray = data.list.map(function(item) {
+      // Use given date and timezone offset (from UTC) to get time local to location so days are accurate. 
       var newTime = moment(item.dt_txt).add(data.city.timezone, 'seconds');
+      // Turn the above into a day of the week. 
       var dayName = moment(newTime._d.toString()).format('dddd');
       var temperature = item.main.temp;
       var condition = item.weather[0].main;
       var icon = item.weather[0].icon;
+      // Include hour for testing and validation. 
       var hour = moment(newTime._d.toString()).format('HH');
       return {
         day: dayName,
@@ -68,12 +82,18 @@ var WeatherApp = function() {
       };
     });
 
-    // Create new array with one object for each day (by max temp) with count for day.
+    // Reduce above array to create new array with one object for each day (by max temp) and count for day, 
+    // which is used to check number of data points for 'bookend' days.
     var reducedDataArray = mappedDataArray.reduce(function(acc, item) {
       var accLength = acc.length;
       var lastAccElem = acc[accLength - 1] || {};
+      // Check if object with corresponding day already exists in accumulator. 
       if (lastAccElem.day === item.day) {
         var itemCount = acc[accLength - 1].count;
+        // If temperature of current item is greater, override the accumulator object, 
+        // such that corresponding icon, condition, and hour are included. 
+        // This may not be the most accurate method, but it's the best way I can see to 
+        // deal with the format of the forecast (data in three-hour increments).
         if (lastAccElem.temperature < item.temperature) {
           acc.pop();
           var newItem = Object.assign({
@@ -82,9 +102,11 @@ var WeatherApp = function() {
           acc.push(newItem);
           acc[accLength - 1].temperature = acc[accLength - 1].temperature;
         } else {
+          // In this case (and the above case), day count is incremented. 
           acc[accLength - 1].count = itemCount + 1;
         }
       } else {
+        // If object with corresponding day doesn't exist in acc, put current item in and initiate count. 
         var newItem = Object.assign({
           count: 1
         }, item);
@@ -93,13 +115,14 @@ var WeatherApp = function() {
       return acc;
     }, []);
 
-    // Check if five or six days are included.
+    // Check if five or six days are included in forecast (which has 40 items).
     // If six, decide between first/last: include forecast of day with most data points;
     // i.e., it makes more sense to include forecast of "current day" if it's early,
-    // rather than last day in range (if data is limited, it won't be representative).
+    // rather than last day in range (if data is limited, 'high temp' won't be representative).
     var fiveForecastDays = [];
     if (reducedDataArray.length === 5) {
       fiveForecastDays = reducedDataArray;
+      // Compare day counts of bookend objects to determine which five to include. 
     } else if (reducedDataArray[0].count >= reducedDataArray[reducedDataArray.length - 1].count) {
       for (var i = 0; i < 5; i++) {
         fiveForecastDays.push(reducedDataArray[i]);
@@ -112,19 +135,23 @@ var WeatherApp = function() {
     return fiveForecastDays;
   };
 
+  // Function uses transformed forecast data to set forecast models in five-day collection. 
   var setWeatherForecast = function(data) {
+    // Clear collection first. 
     var len = forecastWeatherCollection.models.length;
     for (var i = 0; i < len; i++) {
       forecastWeatherCollection.remove(len - 1 - i);
     }
+    // Create transformed array. 
     var fiveDaysArray = createFiveDayObjectsArray(data);
-
+    // Create models in collection from array. 
     for (var i = 0; i < fiveDaysArray.length; i++) {
       var dayModel = Model(fiveDaysArray[i]);
       forecastWeatherCollection.add(dayModel);
     }
   };
 
+  // Function to fetch current weather: show/hide loader and alert user if an error occurs. 
   var fetchCurrentWeather = function(query) {
     $.ajax({
       method: "GET",
@@ -146,6 +173,7 @@ var WeatherApp = function() {
     });
   };
 
+  // Function to fetch weather forecast. 
   var fetchWeatherForecast = function(query) {
     $.ajax({
       method: "GET",
@@ -159,7 +187,7 @@ var WeatherApp = function() {
       }
     });
   };
-
+  // Only fetch functions are called externally, but returning others helps with testing. 
   return {
     renderCurrentWeather: renderCurrentWeather,
     setCurrentWeather: setCurrentWeather,
@@ -170,14 +198,18 @@ var WeatherApp = function() {
   };
 };
 
+// Create instance of weather application on page load. 
 var app = WeatherApp();
 
+// Event handler for click on search button: alert if no city is entered, otherwise 
+// trigger data fetch and subsequent changes to model. 
 $('.search').on('click', function() {
   var cityValue = $('#search-query').val() || null;
   if (!cityValue) {
     alert("Enter a city name.");
   } else {
     var countryValue = $('#country-selection').val();
+    // Include country ID if user specifies one in the drop-down menu. 
     var search = (!countryValue) ? cityValue : cityValue + ',' + countryValue;
     app.fetchCurrentWeather(search);
     app.fetchWeatherForecast(search);
