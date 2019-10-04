48e3be071f50497a32b30e40535464527725f93d
Billy DeLashmutt <delashmuttwa@gmail.com>
2019-10-04 13:45:11 -0400

    Implement current weather fetch and render

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-// Openweather API key: e1034943195c711c89bd0b021b9ad8c4
+const APIkey = "&APPID=e1034943195c711c89bd0b021b9ad8c4";
+
 let currentCityWeather = {
-  weather: [{
-    main: "Clear"
-  }],
-  main: {
-    temp: 88
-  },
-  name: "Durham"
+  condition: "cloudy",
+  location: "durbam",
+  degrees: 88
+}
+
+const setCurrentWeather = function (data) {
+  currentCityWeather = {
+    condition: data.weather[0].main,
+    location: data.name,
+    degrees: data.main.temp
+  }
+  renderWeather();
 }
 
 // Currently setup with pre-existing data
@@ -15,13 +21,34 @@ const renderWeather = function () {
   $('#current-weather').empty()
   const source = $('#current-weather-template').html();
   const template = Handlebars.compile(source);
+
   $('#current-weather').append(template({
-    "current-degrees": currentCityWeather.main.temp,
-    "current-condition": currentCityWeather.weather[0].main,
-    "current-location": currentCityWeather.name
+    "current-degrees": currentCityWeather.degrees,
+    "current-condition": currentCityWeather.condition,
+    "current-location": currentCityWeather.location
   }))
 }
 
+const fetchCurrentWeather = function (query) {
+  const searchURL =
+    "https://api.openweathermap.org/data/2.5/weather?units=imperial&zip=" +
+    query + APIkey;
+
+  $.ajax({
+    method: "GET",
+    url: searchURL,
+    dataType: "json",
+    success: function (data) {
+      setCurrentWeather(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+}
+
+
 $('button').on('click', function () {
-  renderWeather();
+  const location = $('input').val()
+  fetchCurrentWeather(location);
 })
