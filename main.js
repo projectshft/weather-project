6e742a3a1103a0ce851d921eb54ef0c87660cdd7
George Green <greezy@Georges-MacBook-Air.local>
2019-10-04 19:04:03 -0400

    Added some logic and working to start rendering the HTML through handlebars

--- main.js
+++ main.js
@@ -3,38 +3,83 @@
 //iterate through return values to pull out important data
 //transpose any data that requires changing
 //render html using handlebars
-
-$('#search').click(function(event){
+let currentWeather = {};
+//adding click event to search
+$('#search').click(function(event) {
   event.preventDefault();
   let city = $('#citySearch').val();
   fetchData(city)
   fetchFiveDay(city);
 })
 
-let addCurrentWeather = function (data) {
+//function to collect current conditions
+let addCurrentWeather = function(data) {
 
-  let currentWeather = {
+  currentWeather = {
     city: data.name,
     temperature: data.main.temp,
     conditions: data.weather[0].main,
     logo: data.weather[0].icon
   };
   console.log(currentWeather);
-
+  renderCurrent(currentWeather)
+}
+//function to convert Kelvin to tempFahrenheit
+let temperatureToFahrenheit = function(kelvin) {
+  let tempFahrenheit = (9 / 5 * (kelvin - 273) + 32);
+  tempFahrenheit = Math.trunc(tempFahrenheit);
+  return tempFahrenheit;
 }
 
-let addFiveDayWeather = function (data2){
+//function to render the current conditions
+renderCurrent = function (obj) {
+  $('.current-forcast').empty();
+
+    var source = $('#current-weather-template').html();
+    var template = Handlebars.compile(source);
+
+    //$('.current-forcast').append(contributorHTML)
+    $('.current-forcast').append(template({
+      "city": currentWeather.city,
+      "conditions": currentWeather.conditions,
+      "temperature": currentWeather.temperature
+    }))
+  }
+
+//function to render the days of the week
+let renderDaysOfWeek = function (obj) {
+  $('#five-day-forcast-template').empty();
+
+  for (var i = 0; i < obj.length; i++) {
+    var day = obj[i];
+
+    var source = $('#current-weather-template').html();
+    var template = Handlebars.compile(source);
+    var contributorHTML = template(day);
+
+    $('.current-forcast').append(contributorHTML)
+  };
+};
+
+
+
+//function to collect data for five day forcast from the API return
+let addFiveDayWeather = function(data2) {
   let fiveDayWeather = [];
   let todaysDate = moment().format();
-  var actualDate = todaysDate.slice(0,10);
+  var actualDate = todaysDate.slice(0, 10);
   for (i = 0; i < data2.list.length; i++) {
     let workingDate = data2.list[i].dt_txt;
     let workingDateParsed = workingDate.slice(0, 10);
-    let workingDateTime = workingDate.slice(11,13);
+    let workingDateTime = workingDate.slice(11, 13);
     let figureOutDayOfWeek = moment(workingDateParsed).format('dddd');
-    if(workingDateTime == 00) {
+    let tempInKelvin = data2.list[i].main.temp;
+    let workingFTemp = temperatureToFahrenheit(tempInKelvin);
+    console.log(workingFTemp);
+    //console.log(temperatureToFahrenheit);
+    if (workingDateTime == 00) {
       let workingFiveDay = {
-        temperature: data2.list[i].main.temp,
+        temperature: workingFTemp,
         conditions: data2.list[i].weather[0].main,
         logo: data2.list[i].weather[0].icon,
         day: figureOutDayOfWeek
@@ -42,46 +87,39 @@ let addFiveDayWeather = function (data2){
       }
       fiveDayWeather.push(workingFiveDay)
     }
-
-  
-    console.log(fiveDayWeather)
   }
-
-  let dayOne = {
-    temperature: data2.list[0].main.temp,
-    conditions: data2.list[0].weather[0].main,
-    logo: data2.list[0].weather[0].icon
-  }
-  console.log(fiveDayWeather);
+  renderDaysOfWeek(fiveDayWeather);
 }
 
 
+
+//API call for the current forcast
 let fetchData = function(city) {
   $.ajax({
     method: "GET",
     url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=dc4751020ac27d49bffd0f744199344f',
     dataType: 'jsonp',
     success: function(data) {
-    addCurrentWeather(data)
-    console.log(data);
-  },
-  error: function(jqXHR, textStatus, errorThrown) {
-    console.log(textStatus);
-  }
-});
+      addCurrentWeather(data)
+      console.log(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
 };
-
+//API call for five-day forcast
 let fetchFiveDay = function(city) {
   $.ajax({
     method: "GET",
     url: 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + ',us&APPID=dc4751020ac27d49bffd0f744199344f',
     dataType: 'jsonp',
     success: function(data2) {
-    addFiveDayWeather(data2);
-    console.log(data2);
-  },
-  error: function(jqXHR, textStatus, errorThrown) {
-    console.log(textStatus);
-  }
-});
+      addFiveDayWeather(data2);
+      console.log(data2);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
 };
