9dbba6d6926487ce7170243b97499626f00473f5
Tyler <laorik1@live.com>
2019-10-04 13:19:00 -0400

    updated render to display weather after being parsed. reworked some of the HTML for better formatting

--- main.js
+++ main.js
@@ -7,16 +7,21 @@ var getWeatherData = (search) => {
   fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&APPID=${API_KEY}&units=imperial`)
     .then(response => response.json())
     .then(data => {
-      renderWeather();
+      currWeather = {
+        temp: data.main ? Math.trunc(data.main.temp) : null,
+        weatherType: data.weather ? data.weather[0].main : null,
+        location: search
+      }
+      renderWeather(currWeather);
       console.log(data)
     })
     .catch(error => console.error(error))
 }
 
-var renderWeather = () => {
+var renderWeather = (currWeather) => {
   $weatherDisplay.empty();
   var weatherTemplate = Handlebars.compile($('#curr-weather-template').html());
-  $weatherDisplay.append(weatherTemplate());
+  $weatherDisplay.append(weatherTemplate(currWeather));
 }
 
 const API_KEY = '4f479c5fa18add48ba9381407334d58b';
