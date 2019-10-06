4d3f567dcd50bccfdff7e8a8ca1c62a995055faf
Tyler <laorik1@live.com>
2019-10-06 11:58:48 -0400

    added api call to 5 day forecast and processing that data into a useable format of only 5 days (instead of 40 instances of 3 hours periods across 5 days)

--- main.js
+++ main.js
@@ -11,13 +11,61 @@ var getWeatherData = (search) => {
         temp: data.main ? Math.trunc(data.main.temp) : null,
         weatherType: data.weather ? data.weather[0].main : null,
         location: search,
-        iconID: data.weather ? data.weather[0].icon : null
+        iconID: data.weather ? data.weather[0].icon : null,
+        forecast: build5DayForecastStorage()
       }
-      renderWeather(currWeather);
       console.log("Location Data: ");
       console.log(data);
+      return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${search}&APPID=${API_KEY}&units=imperial`);
     })
+    .then(response => response.json())
+    .then(data => {
+      if (data.list) {
+        let dayIndex = 0;
+        //loop through each of the intervals provided by the api and find the
+        //first instace of each after our forecast date "keys"
+        data.list.forEach((forecast) => {
+          //if we still have days left
+          if (dayIndex < currWeather.forecast.length) {
+            let forecastTime = moment.unix(forecast.dt ? forecast.dt : 0);
+            //if after our date, this is the closest after instance so save that and move on/start comparing against the next day
+            if (forecastTime.isAfter(currWeather.forecast[dayIndex].date)) {
+              currWeather.forecast[dayIndex].weather = {
+                temp: forecast.main ? Math.trunc(forecast.main.temp) : null,
+                weatherType: forecast.weather ? forecast.weather[0].main : null,
+                iconID: forecast.weather ? forecast.weather[0].icon : null
+              }
+              dayIndex++;
+            }
+          }
+        }
+        )
+      }
+      renderWeather(currWeather);
+      console.log("Forecast Data: ");
+      console.log(data);
+      console.log("Saved Data: ")
+      console.log(JSON.stringify(currWeather));
+    }
+    )
     .catch(error => console.error(error))
+}
+
+//builds an array of objects who's date value is noon of each day starting with today and moving ahead 5 days
+var build5DayForecastStorage = () => {
+  var storage = [];
+  for (i = 0; i < 5; i++) {
+    //set each date to noon today
+    var date = moment();
+    date.hours(12);
+    date.minutes(0);
+    date.seconds(0)
+    //add i days
+    date.add(i, 'days');
+    //push an empty weather object with property of that date
+    storage.push({ date: date, weather: {} });
+  }
+  return storage;
 }
 
 var renderWeather = (currWeather) => {
