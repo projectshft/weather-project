42d7d0dc3039d94bd8ae83ec72e3d22a41886fd2
daveman235 <awad.david@gmail.com>
2019-10-06 23:42:13 -0400

    revision that includes 5 day forecast functionality

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//array that takes in information from setCurrentWeather function 
 var currentWeatherData = [
     { temperature: "",
      city: "",
@@ -5,74 +6,50 @@ var currentWeatherData = [
      icon: ""
     }
 ];
+// array that takes in information from setFiveDayForecast
+var forecast = [];
 
-var mondayForecast = [
-    { temperature: "", 
-     condition: "", 
-     icon: "", 
-     day: ""
-    }   
-];
-
-// var tuesdayForecast = [
-//     {
-//      temperature: "",
-//      condition: "",
-//      icon: "", 
-// }];
-
-// var wednesdayForecast = [
-//     {
-//         temperature: "",
-//         condition: "",
-//         icon: "",
-//     }];
-
-// var thursdayForecast = [
-//     {
-//         temperature: "",
-//         condition: "",
-//         icon: "",
-//     }];
-
-// var fridayForecast = [
-//     {
-//         temperature: "",
-//         condition: "",
-//         icon: "",
-//     }];
-
-
+//function that the information from the JSON file and passes to thd forecast array
 var setFiveDayForecast = function (setFive) {
-    mondayForecast[0].temperature = setFive.list[0].main.temp.toPrecision(2) + "°";
-    mondayForecast[0].condition = setFive.list[0].weather[0].description;
-    mondayForecast[0].icon = setFive.list[0].weather[0].icon;
-    mondayForecast[0].day = setFive.list[0].dt_txt;
-
-    renderFiveDayForecast();
+    forecast = [];
+        for (var i = 0; i < setFive.list.length; i++) {
+            if (setFive.list[i].dt_txt.includes("00:00:00")) {
+                var forecastObj = {
+                    "temperature": setFive.list[i].main.temp.toPrecision(2) + "°",
+                    "condition": setFive.list[i].weather[0].description,
+                    "icon": setFive.list[i].weather[0].icon,
+                    "day": setFive.list[i].dt_txt
+                    }
+                forecast.push(forecastObj);
+                }
+            
+            }
+            renderFiveDayForecast();
     
 };
 
+// function that renders information from forecast to HTML 
 var renderFiveDayForecast = function (renderForecast) {
     $('#forecast-cards').empty();
-    for (var i = 0; i < mondayForecast.length; i++) {
-        var weekDay = mondayForecast[i];
+    
+    for (var i = 0; i < forecast.length; i++) { 
+        var weekDay = forecast[i];
 
-    var source = $('#monday-template').html();
+    var source = $('#week-template').html();
     var template = Handlebars.compile(source);
     
     var fiveDayForecastHTML = template({
-        "condition": mondayForecast[0].condition,
-        "icon": "http://openweathermap.org/img/wn/" + mondayForecast[0].icon + "@2x.png",
-        "temperature": mondayForecast[0].temperature,
-        "day": moment(mondayForecast[0].day).format('dddd')
-
+        "condition": forecast[i].condition,
+        "icon": "http://openweathermap.org/img/wn/" + forecast[i].icon + "@2x.png",
+        "temperature": forecast[i].temperature,
+        "day": moment(forecast[i].day).format('dddd')
+    
     
     });
     $('#forecast-cards').append(fiveDayForecastHTML);
-
 }};
 
+//function that renders information from the array to HTML 
 var renderCurrentWeather = function (icon) {
     $('#weatherblurb').empty();
     for (var i = 0; i < currentWeatherData.length; i++) {
@@ -90,6 +67,7 @@ var renderCurrentWeather = function (icon) {
     }
 };
 
+//function that allows specific items in JSON file to be accessed in global array currentWeatherData
 var setCurrentWeather = function (setData) { 
     currentWeatherData[0].temperature = setData.list[0].main.temp.toPrecision(2) + "°";
     currentWeatherData[0].city = setData.list[0].name;
@@ -99,6 +77,7 @@ var setCurrentWeather = function (setData) {
     renderCurrentWeather();
 };
 
+//current weather api request 
 var fetchCurrentWeather = function (city) {
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/find?q=" + city + "&units=imperial" + "&APPID=1860a69c6a9afd9a53fdd80c14c3bb78",
@@ -106,7 +85,6 @@ var fetchCurrentWeather = function (city) {
         dataType: "jsonp",
         success: function (currentDayData) {
             setCurrentWeather(currentDayData);
-            
         },
         error: function (jqXHR, textStatus, errorThrown) {
             console.log(textStatus);
@@ -114,7 +92,7 @@ var fetchCurrentWeather = function (city) {
     });
 };
 
-
+//five day forecast api request 
 var fiveDayForecast = function(city) {
     $.ajax({
         url: "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=1860a69c6a9afd9a53fdd80c14c3bb78",
@@ -122,7 +100,7 @@ var fiveDayForecast = function(city) {
         dataType: "jsonp",
         success: function (fiveDayData) {
             setFiveDayForecast(fiveDayData);
-            console.log(fiveDayData);
+            
 
         },
         error: function (jqXHR, textStatus, errorThrown) {
@@ -131,7 +109,6 @@ var fiveDayForecast = function(city) {
     });
 
 }
-
 
 //click handler takes in values, resets form, prevents empty form clicks
 $('#search').on('click', function () {
