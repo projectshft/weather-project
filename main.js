891648caeafd9dcd2dedf72cb1bb7e489da79575
Patrick Crawford <53802272+pjcrawford@users.noreply.github.com>
2019-10-08 16:17:55 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,67 +1,175 @@
-var weather = [];
-var forecastData = [];
+var currentConditions = {};
+var forecasts = [];
 
-$('#search-city').on('click', function () {
-  var search = $('#city-name').val();
-  fetchCurrentConditions(search);
-  fetchFiveDayForecast(search);
-});
-//AJAX API -> used template from class
+//variable to declare the objects within the current conditions
+var declarecurrentConditions = function (data) {
 
-var fetchCurrentConditions = function (query) {
+  var weather = function () {
+    if (data.weather[0].main) {
+      return data.weather[0].main;
+    }
+  };
+  var location = function () {
+    if (data.name) {
+      return data.name;
+    }
+  };
+  var temp = function () {
+    if (data.main.temp) {
+      return Number(data.main.temp).toFixed();
+    }
+  };
+  var icon = function () {
+    if (data.weather[0].icon) {
+      return data.weather[0].icon;
+    }
+  };
+
+  currentConditions = {
+    icon: icon(),
+    temp: temp(),
+    location: location(),
+    weather: weather()
+  };
+
+  weatherRendering();
+
+};
+
+//function that adds objects for each following day's weather forecast
+var addWeeklyForecast = function (data) {
+
+  forecasts = [];
+// for loop to pull out 5 days only
+  for (var i = 0; i < 5; i++) {
+
+    var forecastData = data.list[i];
+
+    var temp = function () {
+      if (forecastData.main.temp) {
+        return Number(forecastData.main.temp).toFixed();
+      } else {
+        return null;
+      }
+    };
+
+    var weather = function() {
+      if (forecastData.weather[0].main) {
+        return forecastData.weather[0].main;
+      } else {
+        return null;
+      }
+    };
+
+    var location = function() {
+      if (data.city.name) {
+        return data.city.name;
+      } else {
+      return null;
+      }
+    };
+
+    var icon = function () {
+      if (forecastData.weather[0].icon) {
+        return forecastData.weather[0].icon;
+      } else {
+        return null;
+      }
+    };
+// converting the date from string of numbers to whichever day of the week it is eg:'dddd' = Friday with moment helper
+    var date = function () {
+      if (forecastData.dt_txt) {
+        return moment(forecastData.dt_txt).format('dddd');
+      } else {
+      return null;
+      }
+    };
+
+    var forecast = {
+      date: date(),
+      icon: icon(),
+      temp: temp(),
+      location: location(),
+      weather: weather()
+    };
+//adding the new forecast we searched for into the "forecasts" array created above
+    forecasts.push(forecast);
+  }
+
+  renderWeeklyForecast();
+};
+
+
+//API request code, based on the bookshelf project.  added alert for invalid city entries
+var fetchcurrentConditions = function (query) {
   $.ajax({
     method: "GET",
-    url: "https://api.openweathermap.org/data/2.5/weather?q=",
+    url: "https://api.openweathermap.org/data/2.5/weather?q="+query+"&APPID=fa9a0e4c4fd59f4be6924cdceb975e9b&units=imperial",
     dataType: "json",
     success: function(data) {
-      currentConditions(data);
-    },    error: function(jqXHR, textStatus, errorThrown) {
-      alert('Invalid Entry');
-    }
-  });
-};
-
-
-// API for 5 day forecast
-
-var fetchFiveDayForecast = function (query) {
-  $.ajax({
-    method: "GET",
-    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + query + '&APPID=486f4890e6823edadc8d626bbb26cdc7',
-    dataType: "json",
-    success: function(forecastData) {
-      console.log(forecastData);
-      fiveDayForecast(forecastData);
+      declarecurrentConditions(data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
-      console.log(textStatus);
+      alert('Invalid City!  Check spelling and try again');
     }
   });
 };
 
-// API for the weather currently
+var fetchWeeklyForecast = function(query) {
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/forecast?q="+query+"&APPID=fa9a0e4c4fd59f4be6924cdceb975e9b&units=imperial",
+    dataType: "json",
+    success: function(data) {
+      addWeeklyForecast(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+    }
+  });
+};
 
+//click event.  same format we have been using in class
+$('#find-weather').on('click', function() {
+  var city = $('#search-query').val();
+    if (city !== '') {
+      fetchcurrentConditions(city);
+      fetchWeeklyForecast(city)
+    }   else {
+      alert('Invalid City!  Check spelling and try again')
+    }
+    $("#search-query").val('');
+});
 
-var renderWeather = function () {
-  $('.weather').empty();
-  var $weather = $('.weather-template');
-  for (var i = 0; i < currentConditions.length; i++) {
-    var image = weather.hasOwnProperty('imageLinks') ? weather.imageLinks.smallThumbnail : null;
-    var newHTML = template({ temp, city, weather, weatherConditions, icon });
-    $currentConditions.append(newHTML);
-  }
- 
-  renderWeather();
- };
- 
+//rendering the weather currently by passing in the current conditions
 
- var currentConditions = function (data) {
-  weather = [{
-    temp: ((data.main.temp)),
-    city: data.name,
-    weatherConditions: data.weather[0].main,
-    icon: 'http://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
-  }];
-  renderWeather();
+var weatherRendering = function() {
+  $('#todays-weather').empty();
+  var todaysWeather = weatherRenderingWithHandlebars(currentConditions);
+  $('#todays-weather').append(todaysWeather);
 }
-;
+
+//Handlebars object template conversion
+var weatherRenderingWithHandlebars = function(currentObj){
+  var source = $('#current-weather-template').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(currentObj);
+  return newHTML;
+};
+
+// render the 5 day forecast.  used a for each and passed in each day
+var renderWeeklyForecast = function() {
+  $('#weeks-forecast').empty();
+  forecasts.forEach(function(day) {
+    var dailyForecast = renderWeeksForecastWithHandlebars(day);
+    $('#weeks-forecast').append(dailyForecast);
+  })
+};
+//same as above but for forecast instead of current
+var renderWeeksForecastWithHandlebars = function(dailyForecastObject) {
+  var source = $('#weeks-forecast-template').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(dailyForecastObject);
+  return newHTML;
+};
+
+//test for commit
