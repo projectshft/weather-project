3add8b7feaf03156bdf5788903b4ce88c556eb8b
Katy Andersen <katyandersen@Katys-MacBook-Air.local>
2020-01-31 16:49:42 -0500

    Resolve formatting issues using Bootstrap

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 var weather = [];
+var fiveDayWeather = [];
 
 $('.search').on('click', function () {
   var search = $('#search-query').val();
   $(this).html('<span class="spinner-border spinner-border-sm"></span> Loading...');
 
   fetch(search);
+  fetchFiveDayWeather(search);
 });
 
 var fetch = function (query) {
-  var key = '&appid=fd9c1cb4eb6ab4ecba76da13be5e4e52'
 
   $.ajax({
     method: "GET",
-    url: "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial" + key,
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial" + "&appid=fd9c1cb4eb6ab4ecba76da13be5e4e52",
     dataType: "json",
     success: function(data) {
       populateWeather(data);
@@ -24,25 +25,68 @@ var fetch = function (query) {
 };
 
 var populateWeather = function (data) {
-  weather = []
-  // for (var i = 0; i < data.length; i++){
+  weather = [];
+
     weather.push({
       temperature: Math.round(data.main.temp),
       cityName: data.name,
       weatherDescription: data.weather[0].main,
+      weatherIcon: "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
     })
-   // }
   renderWeather()
 };
 
 var renderWeather = function() {
   $('.search').html('<span></span> Search')
   $('.weather').empty();
-debugger;
+
   for (var i = 0; i < weather.length; i++) {
     var source = $('#weather-template').html();
     var template = Handlebars.compile(source);
     var newHTML = template(weather[i]);
     $('.weather').append(newHTML);
+
   }
+};
+
+var renderFiveDayWeather = function() {
+  $('.fiveDayWeather').empty();
+
+  for (var i = 0; i < fiveDayWeather.length; i++) {
+    var source = $('#fiveDayWeather-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template(fiveDayWeather[i]);
+    $('.fiveDayWeather').append(newHTML)
+  }
+}
+
+var fetchFiveDayWeather = function (query2) {
+
+  $.ajax({
+    method: "GET",
+    url: "http://api.openweathermap.org/data/2.5/forecast?q=" + query2 + "&units=imperial" + "&appid=fd9c1cb4eb6ab4ecba76da13be5e4e52" + "&count=5",
+    dataType: "json",
+    success: function(data) {
+      populateFiveDayWeather(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+};
+
+var populateFiveDayWeather = function (data) {
+  fiveDayWeather = [];
+
+ for (var i = 0; i < data.list.length; i = i + 8) {
+
+    fiveDayWeather.push({
+      time: moment(data.list[i].dt_txt).format('dddd'),
+      temperature: Math.round(data.list[i].main.temp),
+      cityName: data.city.name,
+      weatherDescription: data.list[i].weather[0].main,
+      weatherIcon: "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png"
+    })
+  }
+  renderFiveDayWeather()
 };
