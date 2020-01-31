714b749a726151e658ee7b9c1232a126b23d3fa1
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2020-01-31 18:55:10 -0500

    forcast functionality added

--- main.js
+++ main.js
@@ -11,7 +11,10 @@ let WeatherProject = function() {
 		imageURL: '',
 		dayOfWeek: ''
 	}
+	//div to display current weather
 	let $currentWeather = $('.current-weather');
+
+	//div to display 5 days forcast
 	let $forcast = $('.forcast');
 
 	//after the weather API call we format data for later use 
@@ -45,13 +48,51 @@ let WeatherProject = function() {
 		}
 	}
 
+	let formatForcastData = function(result) {
+		for(let i = 0; i < result.list.length; i++) {
+			forcastObj = {};
+			forcastObj.city = result.city.name;
+			forcastObj.state = '';
+			forcastObj.country = result.city.country;
+			forcastObj.temperature = result.list[i].main.temp;
+			forcastObj.description = result.list[i].weather[0].main;
+
+			let iconCode = result.list[i].weather[0].icon;
+			//get the icon from the openweathermap.org
+			let iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png"
+			forcastObj.imageURL = iconURL;
+
+			let timestamp = moment.unix(result.list[i].dt_txt);
+			let dayOfWeek = moment(timestamp).format('dddd');
+			console.log(dayOfWeek);
+			forcastObj.dayOfWeek = dayOfWeek;
+
+			forcast.push(forcastObj);
+		 }   
+		
+		console.log(forcast);
+
+		return forcast;
+
+	}
+
+	let renderForcast = function() {
+
+		$forcast.empty();
+
+		for(let i = 0; i < forcast.length; i++) {
+			let forcastTemplate = Handlebars.compile($('#forcast-template').html());
+			let forcastView = forcastTemplate(forcast[i]);
+			$forcast.append(forcastView);
+		}
+	}
 
 	return {
 		forcast: forcast,
-		//renderForcast: renderForcast,
+		renderForcast: renderForcast,
 		renderCurrentWeather: renderCurrentWeather,
 		formatCurrentData: formatCurrentData,
-		//formatForcastData: formatForcastData
+		formatForcastData: formatForcastData
 	}
 }
 
@@ -75,6 +116,20 @@ const fetchCurrentWeather = async(query) => {
   	console.log(json);
 	app.formatCurrentData(json);
 	app.renderCurrentWeather();
+
+	//get data for 5 days forcast
+	fetchFiveDaysForcast(query);
+}
+
+const fetchFiveDaysForcast = async(query) => {
+	const res = await fetch(
+	    'https://api.openweathermap.org/data/2.5/forecast?q='+query+'&units=imperial&appid=8349ad5e6bba395d3c4a42b77ef38130'
+	  );
+	const json = await res.json();
+
+  	console.log(json);
+	app.formatForcastData(json);
+	app.renderForcast();
 }
 
 
