06056feca55bd6b3dbc039f7950a3f30f9c7a275
Casey Smith <caseysmith@Caseys-MacBook-Pro.local>
2020-01-31 12:04:59 -0500

    initial plan layout and notes for complete

---
+++ main.js
@@ -0,0 +1,22 @@
+// var weekForcast = [];
+
+// HandleBars
+
+// rendersPage with new search
+
+// fetch API
+
+// Gathers data from API to send to template
+
+// clickHandler
+    // Make sure click button is working and receiving info
+    // ? may want to think about a different function that handels
+    // Edge case scenarios? blank...but should accept city & city/country
+
+// 1. Click sends data to fetch
+// 2. Fetch gathers data and sends to a function to display data
+// 3. 5 Day will need some sort of container so handlbars can create a template
+//     -HandleBars 2 temps: 1 day and 5 day
+//     -Post data to view
+//     -Way to render page with new search info
+// 4. Think about hard coding first to see if code is working
