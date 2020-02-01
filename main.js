3e88dc402e0b9534b8f523310482820474948cec
Tatiana Grigorieva <tatianagrigorieva@Tatianas-MacBook-Pro.local>
2020-01-31 20:45:53 -0500

    add 5 days forcast

--- main.js
+++ main.js
@@ -17,11 +17,13 @@ let WeatherProject = function() {
 	//div to display 5 days forcast
 	let $forcast = $('.forcast');
 
+	let timeOfDay = '';
+
 	//after the weather API call we format data for later use 
 	let formatCurrentData = function(result) {	
 		forcast = [];
 		forcastObj = {};
-		
+
 		forcastObj.city = result.name;
 		forcastObj.state = '';
 		forcastObj.country = result.sys.country;
@@ -55,27 +57,45 @@ let WeatherProject = function() {
 		forcast = [];
 
 		for(let i = 0; i < result.list.length; i++) {
-			forcastObj = {};
-			forcastObj.city = result.city.name;
-			forcastObj.state = '';
-			forcastObj.country = result.city.country;
-			forcastObj.temperature = Math.round(result.list[i].main.temp);
-			forcastObj.description = result.list[i].weather[0].main;
+			//we need the time to match with the time of the forcast
+			timeOfDay = moment.unix(result.list[1].dt).format('h:mm:ss a');
+			//console.log('Time of day: ' + timeOfDay);
 
-			let iconCode = result.list[i].weather[0].icon;
-			//get the icon from the openweathermap.org
-			let iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png"
-			forcastObj.imageURL = iconURL;
+			//we get the time of the data element to match with the time of forcast
+			let timeToMatch = moment.unix(result.list[i].dt).format('h:mm:ss a');
+			//console.log('Time to match: ' + timeToMatch);
 
-			let timestamp = moment.unix(result.list[i].dt);
-			let dayOfWeek = moment(timestamp).format('dddd');
-			console.log(dayOfWeek);
-			forcastObj.dayOfWeek = dayOfWeek;
+			//if timeOfDay matches timeToMatch, get the data to display the 5 days forcast
+			if (timeOfDay === timeToMatch) {
+				//convert value from the data to a format we can use 
+				let timestamp =  moment.unix(result.list[i].dt);
 
-			forcast.push(forcastObj);
+				//clear the object
+				forcastObj = {};
+
+				forcastObj.city = result.city.name;
+				forcastObj.state = '';
+				forcastObj.country = result.city.country;
+				forcastObj.temperature = Math.round(result.list[i].main.temp);
+				forcastObj.description = result.list[i].weather[0].main;
+
+				let iconCode = result.list[i].weather[0].icon;
+
+				//get the icon from the openweathermap.org
+				let iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png"
+				forcastObj.imageURL = iconURL;
+
+				
+				let dayOfWeek = moment(timestamp).format('dddd');
+				//console.log(dayOfWeek);
+				forcastObj.dayOfWeek = dayOfWeek;
+
+				forcast.push(forcastObj);
+			}
+
 		 }   
 		
-		console.log(forcast);
+		//console.log(forcast);
 
 		return forcast;
 
