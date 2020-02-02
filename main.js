4f53d68634d736a59fc0fe7dab4d0e3f1955dbe3
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-02-02 12:19:39 -0500

    with the current weather centered and more comments added

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 //all the data is in a function
 const WeatherProject = () => {
-  //the data structure where my data is going
+  //the data structure where the currentweather is contained
   var currentWeatherArray = [];
+  //the data structure for the 5 day forecast
   var fiveDayWeatherArray = [];
-  //
+  //when you search you should get the currentweather & fivedayforecast
   $('.search').on('click', function() {
     //when it's loading it lets you know!
     $(this).html(
       '<span class="spinner-border spinner-border-sm"></span> Loading...'
     );
     var search = $('#search-query').val();
-    //
+    //if there is input in the search query form
     if ($('#search-query').val().length !== 0) {
-
-      //
-      getCityData(search);
-      //
+      //then it invokes this function with the
+      //search term which gets the current weather
+      getCurrentWeather(search);
+       //then it invokes this function with the
+        //search term which gets the five day forecast
       getFiveDayForecast(search);
       //
       // getMapLocation(search);
+      //edge cases alerts the user that they submitted an empty form
     } else {
       alert("you can't submit an empty search! Please, enter a city and continue")
     }
   });
 
-  //
-  var getCityData = function(cityName) {
+  //this function uses a GET method to call the api at openweathermap api
+  //and get the data of the search term
+  var getCurrentWeather = (cityName) => {
     //my API key
     var key = 'b8fd8577d64e0b0873cfe4bfc8820fa6';
     //
@@ -42,10 +46,11 @@ const WeatherProject = () => {
       }
     });
   };
-
-  var addCurrentWeather = function(data) {
+  //this function takes the data from the openweathermap api and pushes the data we want
+  //into the currentWeatherArray as objects
+  var addCurrentWeather = (data) => {
     currentWeatherArray = [];
-
+    //converts the icon from the data into a url
     var icon = data.weather[0].icon
     var iconurl = "http://openweathermap.org/img/w/" + icon + ".png";
 
@@ -55,11 +60,11 @@ const WeatherProject = () => {
       weatherDescription: data.weather[0].main || null,
       icon: iconurl || null,
     })
-
-    renderWeather();
+    //renders the weather to the page
+    renderCurrentWeather();
   };
-
-  var renderWeather = function() {
+  //this function renders the current weather to the page
+  var renderCurrentWeather = () =>{
     //
     $('.search').html('<span></span> Search')
     $('.weatherrow').empty();
@@ -74,7 +79,7 @@ const WeatherProject = () => {
 
   };
 
-  var getFiveDayForecast = function(cityName) {
+  var getFiveDayForecast = (cityName) => {
     //my API key
     var key = 'b8fd8577d64e0b0873cfe4bfc8820fa6';
     //
@@ -83,7 +88,7 @@ const WeatherProject = () => {
       url: 'http://api.openweathermap.org/data/2.5/forecast?appid=' + key + '&q=' + cityName + '&units=imperial' + '&count=5',
       dataType: "json",
       success: function(data) {
-        addFiveDayWeather(data);
+        addFiveDayForecast(data);
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.log(textStatus);
@@ -91,7 +96,7 @@ const WeatherProject = () => {
     });
   };
   //
-  var addFiveDayWeather = function(data) {
+  var addFiveDayForecast = (data) => {
     //
     fiveDayWeatherArray = [];
 
@@ -105,24 +110,44 @@ const WeatherProject = () => {
         icon: "http://openweathermap.org/img/w/" + data.list[i].weather[0].icon + ".png",
       });
       //
-      renderFiveDayWeather();
+      renderFiveDayForecast();
     }
 
   };
 
-  var renderFiveDayWeather = function() {
+//renders the five day forecast to the page
+  var renderFiveDayForecast = () => {
     $('.search').html('<span></span> Search')
+    //empties the current html
     $('.fivedayweatherrow').empty();
-
+    //loops through the find the data in the datastructure
     for (var i = 0; i < fiveDayWeatherArray.length; i++) {
-
+      //handlebars
       var source = $('#fivedayweather-template').html();
       var template = Handlebars.compile(source);
       var newHTML = template(fiveDayWeatherArray[i]);
-      // append our new html to the page
+      // append the new html to the page in the handlebars template
       $('.fivedayweatherrow').append(newHTML);
     }
   };
+
+  // var getMapLocation = function(cityName) {
+  //     //my API key
+  //     var googleMapsKey = 'AIzaSyDhcRQvPZgKQMxT8-RIxsO9nCHsEku_Rkc';
+  //     //
+  //     $.ajax({
+  //       method: "GET",
+  //       url: "https://www.google.com/maps/embed/v1/search?q= + cityName + &key= + googleMapsKey",
+  //       dataType: "json",
+  //       success: function(data) {
+  //         console.log(data);
+  //       },
+  //       error: function(jqXHR, textStatus, errorThrown) {
+  //         console.log(textStatus);
+  //       }
+  //     });
+  //   };
+
 };
 
 
