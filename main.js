31245c24283fd9ad4d477aca0321dd918d43a7fc
Ann Schnabel <annschnabel@Anns-MacBook-Pro.local>
2020-05-15 13:38:58 -0400

    abilities to addCity and renderCity complete

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
+var currentCity = [];
+
+var addCity = function(data) {
+  // get rid of old city
+  currentCity = []
+  // take data and format it
+  temp = Math.round(Number(data.main.temp));
+  var newCity = {temperature: temp, city: data.name, country: data.sys.country , weather: data.weather[0].main}
+  // push it to currentCity
+  currentCity.push(newCity);
+  renderCity()
+}
+
+var renderCity = function() {
+  var source = $('#stats-template').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(currentCity[0]);
+  $('#stat-container').append(newHTML);
+}
+
+// fetch data with city input as query
 var fetch = function(query) {
   $.ajax({
     method: "GET",
-    url: `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=16e8800e45b79f25abb73f07cc2f92ce`,
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${query}&units=imperial&appid=16e8800e45b79f25abb73f07cc2f92ce`,
     dataType: "json",
+    // if successful, send data to addCity
     success: function(data) {
-      console.log(data);
+      addCity(data);
     },
+    // if unsuccessful, console log error
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
     }
