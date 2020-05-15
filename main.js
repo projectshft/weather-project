374646357a52175b782dd9bba773980aff42b7cb
MCola <mcolapietro502@gmail.com>
2020-05-15 13:26:45 -0400

    added object for controlling data and adding to data model & added view object to render data to html

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ var fetchCurrentWeather = function (query) {
     method: "GET",
     //REPLACE BOSTON WITH INPUT QUERY ONCE CLICK FUNCTION FOR SEARCH IS COMPLETED
     //REPLACE HARD CODED API KEY WITH WEATHERMODEL GETAPIKEY() FUNCTION;
-    url: "https://api.openweathermap.org/data/2.5/weather?q=boston&appid=2f39d6282bde1fde1a7949c43e939422",
+    url: "https://api.openweathermap.org/data/2.5/weather?q=boston&units=imperial&appid=2f39d6282bde1fde1a7949c43e939422",
     dataType: "json",
     success: function(data) {
       //ADD RETURN DATA TO WEATHER DATA MODEL EX: addFaces(data);
@@ -34,15 +34,36 @@ var fetchCurrentWeather = function (query) {
   });
 };
 
+//object to add results of query to model
+var controller = {
+  addCurrentWeather(data) {
+    //gather needed data from API return
+    var weather = {
+      temp: data.main.temp,
+      city: data.name,
+      weather: data.weather[0].main
+    }
+    //push API data to data model
+    weather.push(weatherModel.currentWeatherData);
+    
+  }
+
+}
+
+//object to render model data to html
+var view = {
+  renderCurrentWeather() {
+    $('.current-weather-results').empty();
+
+    
+  }
+}
 
 //Add user's input values to API query
 $('.search').on('click', function () {
   var search = $('#search-query').val();
 
-  fetch(search);
+  fetchCurrentWeather(search);
 });
 
 
-// var view = {
-
-// }
