839b72257ec7defb320e77850fd5806e44b21a2b
Christina Kurpiel <kurpielcm@gmail.com>
2020-05-15 14:22:49 -0400

    add improved javascrip comments and function descriptions. Add css style to weather description

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
+// Empty array that stores most recent city data. There will only ever be one object
+//in this array as all other searches are removed before new ones are added
 cityWeather = [];
 
+//function that takes the data returned from the API/fetchData function, and pushes it to the empty weather array
 var addWeather = function (data) {
+  cityWeather.pop();//removes the last (and only) city object in the array
   var weather = {
-    temp: data.main.temp,
+    temp: Math.round(data.main.temp) + "°",
     name: data.name,
     description: data.weather[0].description
   }
-  cityWeather.push(weather)
+
+  cityWeather.push(weather);//adds the new data to the cityWeather array
 };
 
 // function responsible for taking data in cityWeather array and passing
 // it through Handlebars and appending to current-weather div
 renderWeather = function () {
-  var weather = cityWeather[0];//the only item in the array
+  $('#current-weather').empty();//empties the div so only one item shows at a time
+  var weather = cityWeather[0];//can be hardcoded as there is only ever one object in the cityWeather array
   var source = $('#search-template').html();
   var template = Handlebars.compile(source);
   var weatherHTML = template(weather);
@@ -20,26 +26,28 @@ renderWeather = function () {
   $('#current-weather').append(weatherHTML);
 };
 
-
+// function that gets data from the API and if successful, invokes addWeather
+//(where the data will be added to the array) and invokes renderWeather, where the new data will show up as HTML
 var fetchData = function (city) {
   $.ajax({
     method: "GET",
-    url: "https://api.openweathermap.org/data/2.5/weather?q=" +city+ "&appid=2fa3bf852e1baf47ec1a2ca2ecc407f2",
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" +city+ "&units=imperial&appid=2fa3bf852e1baf47ec1a2ca2ecc407f2",
     dataType: "json",
     success: function(data) {
-      console.log(data)
       addWeather(data);
       renderWeather();
     },
     error: function(jqXHR, testStatus, errorThrown){
-      console.log(textStatus);
+      console.log(testStatus);
+      alert("Please enter valid US city in text field")
     }
   });
 };
 
+//function invoked when the search button is clicked that takes the user's input city
+//and passes it as an argument to the fetchData function
 $('.search').on('click', function () {
   var city = $('.city-input').val();
 
   fetchData(city);
 });
-  renderWeather();
