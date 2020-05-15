b5bf6b4b587255a70f9f00c153ef2a211a688d28
Kyle Stefano <kstefano327@gmail.com>
2020-05-15 11:06:02 -0400

    getting weather to append to the page

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
 var weather = [
-  
+  {temperature: 75, city: "Raleigh", conditions: "Sunny",}
 ]
+
+var renderWeather = function () {
+  weatherDisplay = weather[0]
+
+  var source = $('#current-weather-template').html();
+  var template = Handlebars.compile(source);
+  var weatherHTML = template(weatherDisplay)
+
+  $('#weather').append(weatherHTML)
+}
+
+renderWeather()
 
 
 $('#search').on('click', function () {
