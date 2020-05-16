472888733fcf507d95ad2ef39fb5b750bf304cc2
Pierre Lourens <plourens3@gmail.com>
2020-05-16 18:52:37 -0400

    Uses Open Weather API for data in current-conditions handlebars template

--- main.js
+++ main.js
@@ -1,57 +1,77 @@
 const Weather = () => {
   const weatherData = {
     currentConditions: {
-      temperature: 76,
-      location: 'Durham',
-      description: 'cloudy',
+      temperature: null,
+      location: null,
+      description: null,
     },
   };
 
   // We need to invoke the API with the search-button being clicked
   const searchWeatherAPI = (locationInput) => {
-    console.log(locationInput);
+    console.log(`The json openWeather data for ${locationInput} is`);
 
-    // We need to pull the current conditions from OpenWeather
-    // update weatherData object with temperature
-    // update weatherData object with location name
-    // update weatherData object with description
+    $.ajax({
+      method: "GET",
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${locationInput}&units=imperial&appid=4a88c029caa1e00e6735e625a0ee4cad`,
+      dataType: "json",
+      success: function (openWeatherData) {
+        console.log(openWeatherData);
+
+        // grabbing data here to pass into updateWeatherData
+        let temp = openWeatherData.main.temp;
+        let location = openWeatherData.name;
+        let description = openWeatherData.weather[0].main;
+        updateWeatherData(temp, location, description);
+
+        renderCurrentWeather();
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(textStatus);
+      },
+    });
+  };
+
+  const updateWeatherData = (temp, location, description) => {
+    // update our weather data with input from API
+    weatherData.currentConditions.temperature = temp;
+    weatherData.currentConditions.location = location;
+    weatherData.currentConditions.description = description;
   };
 
   const renderCurrentWeather = () => {
     // empty the current contents of div
-    $('.current-conditions-box').empty();
+    $(".current-conditions-box").empty();
 
     // we'll grab the current weather data to plug into the template & page
-    let source = $('#current-weather-template').html();
+    let source = $("#current-weather-template").html();
     let template = Handlebars.compile(source);
     let currentWeatherHTML = template(weatherData.currentConditions);
 
     // actually update the page
-    $('.current-conditions-box').append(currentWeatherHTML);
+    $(".current-conditions-box").append(currentWeatherHTML);
   };
 
   // If someone presses enter in search field, we need to fire event
   const searchBarEnterKeyListener = () => {
-    $('#search-input').on('keypress', function (event) {
+    $("#search-input").on("keypress", function (event) {
       if (event.which == 13) {
         event.preventDefault();
-        let locationInput = $('#search-input').val();
+        let locationInput = $("#search-input").val();
         searchWeatherAPI(locationInput);
-        renderCurrentWeather();
         // reset the search input once complete
-        $('#search-input').val('');
+        $("#search-input").val("");
       }
     });
   };
 
   // if someone clicks the button to search
   const searchButtonListener = () => {
-    $('.search-location').click(function () {
-      let locationInput = $('#search-input').val();
+    $(".search-location").click(function () {
+      let locationInput = $("#search-input").val();
       searchWeatherAPI(locationInput);
-      renderCurrentWeather();
       // reset the search input once complete
-      $('#search-input').val('');
+      $("#search-input").val("");
     });
   };
 
