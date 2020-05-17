77cad8d04529f29f695a98f9722df20e46f334e6
Pierre Lourens <plourens3@gmail.com>
2020-05-17 14:21:18 -0400

    Average temperatures working in 5-day forecast

--- main.js
+++ main.js
@@ -6,38 +6,7 @@ const Weather = () => {
       description: null,
       icon: null,
     },
-    future: [
-      {
-        description: "cloudy",
-        temperature: 76,
-        icon: "http://openweathermap.org/img/wn/03n@2x.png",
-        day: "Monday",
-      },
-      {
-        description: "cloudy",
-        temperature: 76,
-        icon: "http://openweathermap.org/img/wn/03n@2x.png",
-        day: "Tuesday",
-      },
-      {
-        description: "cloudy",
-        temperature: 76,
-        icon: "http://openweathermap.org/img/wn/03n@2x.png",
-        day: "Wednesday",
-      },
-      {
-        description: "cloudy",
-        temperature: 76,
-        icon: "http://openweathermap.org/img/wn/03n@2x.png",
-        day: "Thursday",
-      },
-      {
-        description: "cloudy",
-        temperature: 76,
-        icon: "http://openweathermap.org/img/wn/03n@2x.png",
-        day: "Friday",
-      },
-    ],
+    future: [],
   };
 
   // We need to invoke the API with the search-button being clicked
@@ -62,12 +31,13 @@ const Weather = () => {
   const getForecastWeather = (locationInput) => {
     $.ajax({
       method: "GET",
-      url: `http://api.openweathermap.org/data/2.5/forecast?q=${locationInput}&appid=4a88c029caa1e00e6735e625a0ee4cad`,
+      url: `http://api.openweathermap.org/data/2.5/forecast?q=${locationInput}&units=imperial&appid=4a88c029caa1e00e6735e625a0ee4cad`,
       dataType: "json",
-      success: function (data) {
-        console.log(data);
-        // let unpackedForecast = unpackForecastFromAPI(forecastWeatherJSON);
-        // setForeCastWeatherData(unpackedForecast);
+      success: function (forecastWeatherJSON) {
+        console.log("The forecast weather is: ");
+        console.log(forecastWeatherJSON);
+        let unpackedForecast = unpackForecastFromAPI(forecastWeatherJSON);
+        setForeCastWeatherData(unpackedForecast);
       },
       error: function (jqXHR, textStatus, errorThrown) {
         console.log(textStatus);
@@ -87,7 +57,82 @@ const Weather = () => {
   };
 
   const unpackForecastFromAPI = (forecastJSON) => {
-    // divide the days
+    // accessing the midnights in all the forecasts will let us divide the days
+    let midnights = forecastJSON.list.filter((threeHourChunk) => {
+      // each object in forecast has a date string as text with the second half
+      // after a space as the hour. I'm only concerned with the hour...
+      let dateStr = threeHourChunk.dt_txt.split(" ");
+
+      // the first time we see midnight should be our next day
+      return dateStr[1] === "00:00:00";
+    });
+
+    console.log("The objects in forecast list that start at midnight are: ");
+    console.log(midnights);
+
+    let dividedForecasts = [];
+
+    // in order to reduce data points, we need subsets of the original forecast
+    for (let midnight = 0; midnight < midnights.length; midnight++) {
+      // find the spot in forecastJSON where the next day begins
+      let startOfDayIndex = forecastJSON.list.findIndex((threeHourChunk) => {
+        return threeHourChunk.dt == midnights[midnight].dt;
+      });
+
+      // for all the days except the last day, we want to slice from midnight to midnight
+      if (midnight < midnights.length - 1) {
+        // find the spot in forecastJSON where the day after the next day begins
+        let startOfNextDayIndex = forecastJSON.list.findIndex(
+          (threeHourChunk) => {
+            return threeHourChunk.dt === midnights[midnight + 1].dt;
+          }
+        );
+
+        // push an array of all the elements between startOfDayIndex and startOfNextDayIndex
+        dividedForecasts.push(
+          forecastJSON.list.slice(startOfDayIndex, startOfNextDayIndex)
+        );
+      } else {
+        // the last forecast does not necessarily end with a midnight forecast
+        let lastAvailableForecastIndex = forecastJSON.list.length;
+
+        dividedForecasts.push(
+          forecastJSON.list.slice(startOfDayIndex, lastAvailableForecastIndex)
+        );
+      }
+    }
+
+    console.log("The sub divided forecasts are");
+    console.log(dividedForecasts);
+
+    let unpackedData = [];
+
+    // reduce the data points for each subset of the overall forecast
+    dividedForecasts.forEach((dayArray) => {
+      let totalTemp = null;
+
+      // construct an object to be stored in our model
+      let singleDayObj = dayArray.reduce(
+        (dayForecast, threeHourChunk, numberOfChunk) => {
+          // calculate the average temp
+          totalTemp += threeHourChunk.main.temp;
+
+          // recalculate average temperature; index starts at 0 so we increment
+          dayForecast.avgTemp = Math.round(totalTemp / (numberOfChunk + 1));
+
+          // determine the midday description
+
+          // determine the midday icon
+          return dayForecast;
+          // see which
+        },
+        {}
+      );
+
+      unpackedData.push(singleDayObj);
+    });
+
+    return unpackedData;
   };
 
   const setCurrentWeatherData = (unpackedCurrentWeather) => {
@@ -105,12 +150,10 @@ const Weather = () => {
     // reset our model's 5-day forecast data
     weatherData.future = [];
 
-    /* {
-      description: "cloudy",
-      temperature: 76,
-      icon: "http://openweathermap.org/img/wn/03n@2x.png",
-      day: "Monday",
-    }, */
+    // push the unpacked Day Forecast to
+    unpackedForecast.forEach((dayForecast) => {
+      weatherData.future.push(dayForecast);
+    });
 
     renderForecastWeather();
   };
