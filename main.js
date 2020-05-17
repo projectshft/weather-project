9079bec5f45ce3662e58035b3842cf7b13f0ea4b
Spalding Vance <spaldingvance@Spaldings-MacBook-Pro.local>
2020-05-17 14:28:47 -0400

    added functionality to prevent duplicate weather data on html, added functionality to display day of week

--- main.js
+++ main.js
@@ -8,15 +8,19 @@
 //   ]
 // };
 
-var weatherData = { weather: []};
+var weatherData = {
+  weather: []
+};
+
+var cityTimeZone = 0;
 
 var buildWeatherTemplate = function() {
-  console.log(weatherData);
   //using handelbars.js to create template from weatherData
-  var source= $('#weather-template').html();
+  var source = $('#weather-template').html();
   var template = Handlebars.compile(source);
   var newHTML = template(weatherData);
   //appending template to weather-display div
+  $('.weather-display').empty();
   $('.weather-display').append(newHTML);
 }
 
@@ -25,27 +29,36 @@ var calculateAverageTemperatures = function(data) {
   var averageTemperatures = [];
   var dayTemperatures = [];
   //looping through weather data, calculating average every 8 indexes (24 hours) and pushing to averageTemperatures
-  for(let i = 0; i < data.length; i++) {
+  for (let i = 0; i < data.length; i++) {
     dayTemperatures.push(data[i].main.temp);
-    if((i+1)%8 === 0) {
-      averageTemperatures.push(Math.round(dayTemperatures.reduce((acc, curr) => acc + curr)/8));
+    //if statement to exectue every 8 indexes (24 hours)
+    if ((i + 1) % 8 === 0) {
+      averageTemperatures.push(Math.round(dayTemperatures.reduce((acc, curr) => acc + curr) / 8));
       dayTemperatures = [];
     }
   }
   return averageTemperatures;
 }
 
-var buildWeatherData = function(data) {
+var buildWeatherData = function(totalData) {
+  //resetting weather data to empty array to prevent duplicates
+  weatherData.weather = [];
+  var data = totalData.list;
+  //setting city time zone for getting day of week
+  cityTimeZone = totalData.city.timezone;
   //imgURLStart is prexif to url for weather icon
   var imgURLStart = "http://openweathermap.org/img/wn/";
   var averageTemperatures = calculateAverageTemperatures(data);
   //looping through data, 8 (24 hours) at a time
-  for(var i = 0; i < data.length; i+= 8) {
+  for (var i = 0; i < data.length; i += 8) {
     var weatherObj = {};
     //setting object key value pairs from api data
+    //using momentjs to parse unix timestamp into day of week
+    weatherObj.weekDay = moment.weekdays(moment.unix(data[i].dt).day());
     weatherObj.forecast = data[i].weather[0].main;
     weatherObj.imgURL = imgURLStart + data[i].weather[0].icon + ".png";
-    weatherObj.temperature = averageTemperatures[(i/8)];
+    //getting average temperature using index/8 to correspond with averageTemperatures index for each day
+    weatherObj.temperature = averageTemperatures[(i / 8)];
     weatherData.weather.push(weatherObj);
   }
   buildWeatherTemplate();
@@ -56,6 +69,6 @@ $(".btn-primary").click(function() {
   var cityName = $(".form-control").val();
   //caling weather api with cityName
   fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=094c0f37c693e9aa814d7b6b5368f063&units=imperial')
-  .then(response => response.json())
-  .then(data => buildWeatherData(data.list));
+    .then(response => response.json())
+    .then(data => buildWeatherData(data));
 })
