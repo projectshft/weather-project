4dcc098288649da806bbde2a21e89ea9856ed8aa
Clyde Weed <clydeweed@gmail.com>
2020-05-18 00:07:28 -0400

    Created basic structure of fetch request and data storage for Search button. Both currently not functioning

--- main.js
+++ main.js
@@ -2,20 +2,58 @@
 // Creating test template to verify data is shown in web page
 
 let weatherDataFromAPI = [
-    {
-      temp: "72*",
-      city: "Durham, NC",
-      description: "Sunny",
-      icon: "http://openweathermap.org/img/wn/01d@2x.png"}
+
+    // Commenting out static data temporarily to test functionality
+    // {
+    //
+    //   temp: "72*",
+    //   city: "Durham, NC",
+    //   description: "Sunny",
+    //   icon: "http://openweathermap.org/img/wn/01d@2x.png"}
 
 ];
+
+
+// Extracting data from the Search input and storing it in an array.
+
+let addWeather = (data) => {
+
+  let tempWeatherDataFromAPI = {
+    temp: data.name,
+
+  }
+
+
+};
+
+
+
+
+// Creating a function to request weather data from OpenWeatherMap API using AJAX
+
+let fetchWeather = (query) => {
+
+  $.ajax({
+
+    method: "GET",
+    url: "api.openweathermap.org/data/2.5/weather?q={city name}&appid=0f9391bf663647fd9cad13780bf4eff1",
+    dataType: "json",
+    success: function(data) {
+      addWeather(data);
+    },
+
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+};
 
 
 // Creating render function to display weather array in web page
 
 let renderWeather = () => {
 
-// Empty weather array to append new data
+// Empty weather div to append new data
   $('#weatherInfo').empty();
 
 
@@ -33,9 +71,12 @@ let renderWeather = () => {
 
 
 // Listening for a click to submit form data
-$('#searchCity').on('click', function () {
+$('#searchCity').on('click', () => {
 
-  console.log("OMG");
+  let locationWeatherData = $('#locationName').val();
+
+  console.log(locationWeatherData);
+
   renderWeather();
 
 });
