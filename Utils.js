610504578f7a33f16aa3cd84d1591ea45f5eae52
Daniel Renteria <d.renteria_a@yahoo.com>
2020-08-28 12:13:59 -0400

    Add Model & View functionality

--- Utils.js
+++ Utils.js
@@ -31,9 +31,46 @@ const Collection = (config) => {
 };
 
 const Model = (config) => {
+    let attributes = {}
 
+    let changeCallback = null
+
+    const init = () => Object.assign(attributes,config)
+
+    const set = (prop, value) => {
+        let tempObj = Object.assign({}, attributes)
+
+        tempObj[prop] = value
+
+        if (!_.isEqual(attributes, tempObj)) {
+            attributes[prop] = value
+
+            if (changeCallback) {
+                changeCallback();
+            }
+        }
+    };
+
+    const getAttributes = () => attributes;
+    const get = (prop) => attributes[prop];
+    const change = (fx) => changeCallback = fx;
+
+    return {
+        set,
+        getAttributes,
+        get,
+        change
+    }
 };
 
 const View = (model, template) => {
+    const render = () => {
+        let attrs = model.getAttributes();
 
+        return template(attrs);
+    };
+
+    return {
+        render
+    };
 };
