a3ebbdf9255014c3ebe0c5cdf08099697e4a6577
Daniel Renteria <d.renteria_a@yahoo.com>
2020-08-28 10:35:11 -0400

    Project goals

---
+++ main.js
@@ -0,0 +1,28 @@
+//Set up HTML 
+    //links & sources
+    //Bootstrap
+    //Handlebars
+    //Add Search bar
+    //Add display for one day 
+    //Add display for one week
+//
+
+//Set up CSS as needed    
+
+//Set up JS
+    //Setup VMC separations according to rules of data flow
+        //Model holds data to be rendered and any changes called by controller
+        //View holds render info and re-renders when changes to the model occur
+        //Controller holds functions that model takes in to make changes
+
+    //Link API
+        //AJAX
+
+    //Input field takes a city as an argument
+        //Search weather data on-click
+    
+    //Display current temperature
+    //Display current weather conditions
+    //Replace data from first search after new search
+    //Display 5-day weather forecast
+        //Try Moment.js to convert Unix to days of the week
