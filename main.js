aa836c5e605560a22166aa01d78de412c7273d82
PJ Foley <pjfoley@gmail.com>
2020-08-29 12:49:44 -0700

    Major features all working

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+/*
+Flow thoughts
+(pt3 grab saved data)
+Render initial screen
+wait for search event
+(test if valid)
+Fetch feeds. 
+Success of each feed formats data into objects for handlebars
+Trigger render again (where is best)
+reset applicable vars
+wait for events
+*/
+
 /* 
 1. bash out placeholder html for wireframe of pt 1
 2. test even handler for search button and getting of input
@@ -39,7 +52,7 @@ const owmApiKey = `3e31940f7e296490f329375344b9bf68`;
 //want to get these out of global scope at some point?
 var currentData = {};
 var forecastData = {};
-var secsInDay = 0;  // start at 0, increment in loop
+var secsInDay = 0; // start at 0, increment in loop
 var dayOfWeek = new Date();
 console.log('DayOfWeek: ', dayOfWeek.getDay());
 var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -61,7 +74,7 @@ var weekday = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
 //upon success of getting current conditions, build an object for it
 var formatCurrent = function (currentData) {
   console.log('formatCurrent() called');
-  currentData = {
+  currentWeather = {
     currentTemp: parseInt(currentData.main.temp),
     cityName: currentData.name,
     conditions: currentData.weather[0].main,
@@ -69,7 +82,7 @@ var formatCurrent = function (currentData) {
   }
   //maybe return object to be called in renderWeather()?
   //for now, let's just send to renderWeather then prep for forecast data
-  renderWeather();
+  // renderWeather();
 }
 
 var formatForecast = function (forecastData) {
@@ -80,23 +93,23 @@ var formatForecast = function (forecastData) {
   var numOfEntries = forecastData.cnt;
   // console.log('numOfEntries: ', numOfEntries);
   // Get timestamp of first (index 0) forecast data
-  
+
   var startingPoint = forecastData.list[0].dt;
-  
+
   // console.log('---->DT<-----', startingPoint);
-  
+
   //  Loop through obj, and get each subsequent day's info
-    // until we run out of entries.  (But we really should limit to
-    // 5 days regardless
-    
+  // until we run out of entries.  (But we really should limit to
+  // 5 days regardless
+
   for (var i = 0; i < numOfEntries; i++) {
     //I don't know a faster way to traverse the JSON...yet
     // secsInDay counter starts at 0, then adds 86400 per interation 
     // to find next day's forecast.  Weakness at this point is that
     // it will always report the forecast for approx the same TOD
     // that this is launched rather than a daily high/low.  
-    
-    
+
+
     if (forecastData.list[i].dt === startingPoint + secsInDay) {
       console.log('sID: ', secsInDay);
       // if (forecastsRead === 4) {
@@ -106,14 +119,14 @@ var formatForecast = function (forecastData) {
         futureTemp: parseInt(forecastData.list[i].main.temp),
         futureConditions: forecastData.list[i].weather[0].main,
         futureIcon: forecastData.list[i].weather[0].icon
-      };
+      }
       // this is just a test
       var futureTemp = forecastData.list[i].main.temp;
-   
+
       var futureConditions = forecastData.list[i].weather[0].main;
-      
+
       var futureIcon = forecastData.list[i].weather[0].icon;
-      console.log('forecastsRead', forecastsRead);  
+      console.log('forecastsRead', forecastsRead);
       console.log('futureTemp: ', futureTemp);
       console.log('futureConditions: ', futureConditions);
       console.log('futureIcon: ', futureIcon);
@@ -124,6 +137,8 @@ var formatForecast = function (forecastData) {
       weatherForecast.push(forecastDay);
     }
   }
+  // is this the best place for this?  Still haven't decided.
+  renderWeather();
   // console.log('final sID: ', secsInDay);
 }
 
@@ -132,23 +147,23 @@ var renderWeather = function () {
   console.log('renderWeather() called');
   $('.current-info').empty();
   $('.forecast').empty();
-  console.log('currentData', currentData);
+
+  console.table('currentWeather', currentWeather);
+
   var currentSource = $('#currentweather-template').html();
-  console.log('currentSource', currentSource);
   var templateCurrent = Handlebars.compile(currentSource);
-  var currentWeatherHTML = templateCurrent(currentData);
-
-  // for (var i = 0; i < weatherForecast.length; i++) {
-
-  //var forecastSource = $('.forecastweather-template').html();
-  // DONE split off forecast from current
-
-  //var templateForecast = Handlebars.compile()
-  // for future use
-  // var templateForecast = templateForecast(weatherForecast[i]);
+  var currentWeatherHTML = templateCurrent(currentWeather);
   $('.current-info').append(currentWeatherHTML);
-  console.log('leaving renderWeather');
-  // }
+  // do as a forEach() for fancy points?
+  for (var i = 0; i < weatherForecast.length; i++) {
+
+    var forecastSource = $('#forecastweekday-template').html();
+    var templateForecast = Handlebars.compile(forecastSource);
+    var forecastDayHTML = templateForecast(weatherForecast[i]);
+    $('.forecast').append(forecastDayHTML);
+
+    console.log('leaving renderWeather');
+  }
 
 
 
