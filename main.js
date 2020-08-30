26d69b1a8db0dc71bcdc3fe918e6873364539ff2
Clark Hollar <clarkhollar@Clarks-MacBook-Pro.local>
2020-08-30 13:58:43 -0400

    includes the addition of the forecast endpoint

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 // encapsulate moving parts into a module for better efficiency/durability/reusability
 const projectWeatherAPI = () => {
 
-// array that will hold the weather data for the searched city
+// arrays that will hold the weather and forecast data for the searched city
     let weatherArray = [];
+    let forecastArray1 = [];
+    let forecastArray2 = [];
+    let forecastArray3 = [];
+    let forecastArray4 = [];
+    let forecastArray5 = [];
 
-    let addWeatherEntries = function (data) {
+// function to sort desired data from API and push it into the weather array
+    let addWeatherEntries = (data) => {
         // convert the standard kelvin temp to fahrenheit 
-        let degrees = data.main.temp
-        degrees = Math.round((degrees - 273.15) * 9/5 + 32);
+        let degrees = Math.round((data.main.temp - 273.15) * 9/5 +32);
         // create a variable to grab the icon image for the weather type
-        let weatherIcon = data.weather[0].icon;
-        weatherIcon = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+        let weatherIcon = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
         let weatherInfo = {
             degrees: degrees,
             cityState: data.name,
@@ -21,23 +25,106 @@ const projectWeatherAPI = () => {
             weatherArray.push(weatherInfo);
             
     };
-        
-  let renderWeather = () => {
-        // ensure that the weather div is cleared out before every render 
+
+    // function to sort desired info from API and push it into the correct forecast array
+    let addForecastEntries = (info) => {
+
+        let forecastInfo1 = {
+            conditions: info.list[0].weather[0].main,
+            degrees: Math.round((info.list[0].main.temp - 273.15) * 9/5 + 32),
+            weatherIcon: `http://openweathermap.org/img/wn/${info.list[0].weather[0].icon}@2x.png`,
+            day: info.list[0].dt
+        }    
+        let forecastInfo2 = {
+            conditions: info.list[8].weather[0].main,
+            degrees: Math.round((info.list[8].main.temp - 273.15) * 9/5 + 32),
+            weatherIcon: `http://openweathermap.org/img/wn/${info.list[8].weather[0].icon}@2x.png`,
+            day: info.list[8].dt            
+        }
+        let forecastInfo3 = {
+            conditions: info.list[16].weather[0].main,
+            degrees: Math.round((info.list[16].main.temp - 273.15) * 9/5 + 32),
+            weatherIcon: `http://openweathermap.org/img/wn/${info.list[16].weather[0].icon}@2x.png`,
+            day: info.list[16].dt     
+        }
+        let forecastInfo4 = {
+            conditions: info.list[24].weather[0].main,
+            degrees: Math.round((info.list[24].main.temp - 273.15) * 9/5 + 32),
+            weatherIcon: `http://openweathermap.org/img/wn/${info.list[24].weather[0].icon}@2x.png`,
+            day: info.list[24].dt           
+        }
+        let forecastInfo5 = {
+            conditions: info.list[32].weather[0].main,
+            degrees: Math.round((info.list[32].main.temp - 273.15) * 9/5 + 32),
+            weatherIcon: `http://openweathermap.org/img/wn/${info.list[32].weather[0].icon}@2x.png`,
+            day: info.list[32].dt          
+        }
+        forecastArray1.push(forecastInfo1);
+        forecastArray2.push(forecastInfo2);
+        forecastArray3.push(forecastInfo3);
+        forecastArray4.push(forecastInfo4);
+        forecastArray5.push(forecastInfo5);
+
+    }
+   
+  // function to render all content from the arrays to the view  
+  let renderWeatherForecast = () => {
+        // ensure that the weather and forecast div are cleared out before every render 
         $('.weather').empty();
+        $('.forecast').empty();
         // loop through the weather array and append the info into our HTML file using handlebars
         for (let i=0; i<weatherArray.length; i++) {
             let weatherInfo = weatherArray[i];
             let source = $('#weather-template').html();
             let template = Handlebars.compile(source);
             let newHTML = template(weatherInfo);
+            $('.weather').append(newHTML);
+        }
+        // loop through each forecast array and append the info into our HTML using handlebars
+        for (let i=0; i<forecastArray1.length; i++) {
+            let forecastInfo1 = forecastArray1[i];
+            let source = $('#forecast-template').html();
+            let template = Handlebars.compile(source);
+            let newHTML = template(forecastInfo1);
             $('.forecast').append(newHTML);
         }
-         // empty out the weather array in order to not have duplicate posts on the page when search is clicked again
+        for (let i=0; i<forecastArray2.length; i++) {
+            let forecastInfo2 = forecastArray2[i];
+            let source = $('#forecast-template').html();
+            let template = Handlebars.compile(source);
+            let newHTML = template(forecastInfo2);
+            $('.forecast').append(newHTML);
+        }
+        for (let i=0; i<forecastArray3.length; i++) {
+            let forecastInfo3 = forecastArray3[i];
+            let source = $('#forecast-template').html();
+            let template = Handlebars.compile(source);
+            let newHTML = template(forecastInfo3);
+            $('.forecast').append(newHTML);
+        }
+        for (let i=0; i<forecastArray4.length; i++) {
+            let forecastInfo4 = forecastArray4[i];
+            let source = $('#forecast-template').html();
+            let template = Handlebars.compile(source);
+            let newHTML = template(forecastInfo4);
+            $('.forecast').append(newHTML);
+        }
+        for (let i=0; i<forecastArray5.length; i++) {
+            let forecastInfo5 = forecastArray5[i];
+            let source = $('#forecast-template').html();
+            let template = Handlebars.compile(source);
+            let newHTML = template(forecastInfo5);
+            $('.forecast').append(newHTML);
+        }
+         // empty out the arrays in order to not have duplicate posts on the page when search is clicked again
          weatherArray = [];
+         forecastArray1 = [];
+         forecastArray2 = [];
+         forecastArray3 = [];
+         forecastArray4 = [];
+         forecastArray5 = [];
     };
-
-    
+ 
     // utilize the weather and forecast endpoints to get the current weather/5 day forecast based on the search parameters
     const fetch = function (query) {
         $.ajax({
@@ -46,7 +133,6 @@ const projectWeatherAPI = () => {
             dataType: "json",
             success: function(data) {
             addWeatherEntries(data);
-            renderWeather();
             },
             error: function(jqXHR, textStatus, errorThrown) {
             alert('Please enter a valid city name')
@@ -57,9 +143,8 @@ const projectWeatherAPI = () => {
             url: `http://api.openweathermap.org/data/2.5/forecast?q=${query}&APPID=5a31d79a77b97402b850d4f7196d3274`,
             dataType: "json",
             success: function(info) {
-            // addForecastEntries(info);
-            // renderForecast();
-            console.log(info)
+            addForecastEntries(info);
+            renderWeatherForecast();
             },
             error: function(jqXHR, textStatus, errorThrown) {
             alert('Please enter a valid city name')
@@ -76,10 +161,12 @@ const projectWeatherAPI = () => {
 
 const app = projectWeatherAPI();
 
-
+// on click return the information from the API endpoints
 $('.search').on('click', function () {
     const search = $('#search-query').val();
    
     app.fetch(search);
+    // reset search bar to default after each search
+    $('form').find('input').val('');
     
 });
