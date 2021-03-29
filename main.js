4b7b75e706dc4919ad7b79b305de16f2c19f45d9
Connor DiPietro <connor.dipietro@gmail.com>
2021-03-29 17:12:53 -0400

    adds input field style, search button linked to input field

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('hello world');
+//upon click of search button, pull val from input field and assign to cityName
+$('#search-button').click(function () {
+  var cityName = $('.city-name-input').val()
+  //console.log(cityName);
+})
+
