c28d21481934bde44015e5a26e6ca55768fe3dc7
unknown <joshkamrass@gmail.com>
2021-03-31 10:06:54 -0400

    completes 5 day forecast functionality

--- main.js
+++ main.js
@@ -1,35 +1,36 @@
 var weatherInfo = {};
-var forecastInfo = [{
-    day: "Thursday",
-    forecastedTemperature: 60,
-    forecastedWeatherConditions: "Cloudy",
-    forecastedWeatherConditionsIcon: "http://openweathermap.org/img/wn/04d@2x.png",
-  },
-  {
-    day: "Friday",
-    forecastedTemperature: 60,
-    forecastedWeatherConditions: "Cloudy",
-    forecastedWeatherConditionsIcon: "http://openweathermap.org/img/wn/04d@2x.png",
-  },
-  {
-    day: "Saturday",
-    forecastedTemperature: 60,
-    forecastedWeatherConditions: "Cloudy",
-    forecastedWeatherConditionsIcon: "http://openweathermap.org/img/wn/04d@2x.png",
-  },
-  {
-    day: "Sunday",
-    forecastedTemperature: 60,
-    forecastedWeatherConditions: "Cloudy",
-    forecastedWeatherConditionsIcon: "http://openweathermap.org/img/wn/04d@2x.png",
-  },
-  {
-    day: "Monday",
-    forecastedTemperature: 60,
-    forecastedWeatherConditions: "Cloudy",
-    forecastedWeatherConditionsIcon: "http://openweathermap.org/img/wn/04d@2x.png",
-  }
-]
+var forecastInfo = [{},{},{},{},{}];
+// var forecastInfo = [{
+//     day: "Wednesday",
+//     forecastedTemperature: 60,
+//     forecastedWeatherConditions: "Cloudy",
+//     forecastedWeatherConditionsIcon: "http://openweathermap.org/img/wn/04d@2x.png",
+//   },
+//   {
+//     day: "Thursday",
+//     forecastedTemperature: 60,
+//     forecastedWeatherConditions: "Cloudy",
+//     forecastedWeatherConditionsIcon: "http://openweathermap.org/img/wn/04d@2x.png",
+//   },
+//   {
+//     day: "Friday",
+//     forecastedTemperature: 60,
+//     forecastedWeatherConditions: "Cloudy",
+//     forecastedWeatherConditionsIcon: "http://openweathermap.org/img/wn/04d@2x.png",
+//   },
+//   {
+//     day: "Saturday",
+//     forecastedTemperature: 60,
+//     forecastedWeatherConditions: "Cloudy",
+//     forecastedWeatherConditionsIcon: "http://openweathermap.org/img/wn/04d@2x.png",
+//   },
+//   {
+//     day: "Sunday",
+//     forecastedTemperature: 60,
+//     forecastedWeatherConditions: "Cloudy",
+//     forecastedWeatherConditionsIcon: "http://openweathermap.org/img/wn/04d@2x.png",
+//   }
+// ]
 
 //Displays Current Conditions portion of page
 var renderCurrentConditions = function() {
@@ -55,11 +56,12 @@ var renderForecast = function () {
 
 $(".search-button").on('click', function () {
   var city = $("#city").val();
-  fetch(city);
+  fetchCurrentConditions(city);
+  fetchFiveDayForecast(city);
 })
 
-//Gets needed weather data from Open Weather API
-var fetch = function(city) {
+//Gets needed weather data on current conditions from Open Weather API
+var fetchCurrentConditions = function(city) {
   $.ajax({
     method: "GET",
     url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=59b871a25f174e2019ec1f4fbbe6807c`,
@@ -75,10 +77,74 @@ var fetch = function(city) {
   })
 }
 
+//Gets needed weather data from Open Weather API for 5 day forecast
+var fetchFiveDayForecast = function(city) {
+  $.ajax({
+    method: "GET",
+    url: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=59b871a25f174e2019ec1f4fbbe6807c`,
+    dataType: "json",
+    success: function (data) {
+      forecastInfo = [{},{},{},{},{}];
+      setForecastInfo(data);
+      renderForecast();
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    },
+  })
+}
+
+//Fills the weatherInfo object with the relevant current conditions data
 var setWeatherInfo = function(OpenWeatherdata) {
   weatherInfo.city = OpenWeatherdata.name;
   //Data from open weather is in Kelvin: Kelvin to Fahrenheit conversion: (0K − 273.15) × 9/5 + 32
   weatherInfo.temperature = Math.round((OpenWeatherdata.main.temp-273.15)*9/5+32);
   weatherInfo.weatherConditions = OpenWeatherdata.weather[0].description;    //weather[0].main;
   weatherInfo.weatherConditionsIcon = `http://openweathermap.org/img/wn/${OpenWeatherdata.weather[0].icon}@2x.png`;
+}
+
+//Sets the forecastInfo array with the relevant 5 day forecast data
+var setForecastInfo = function(OpenWeatherdata) {
+  //Finds the current day of the week
+  var currentTime = new Date();
+  var currentDayOfWeek = currentTime.getDay();
+  var allForecastPoints = OpenWeatherdata.list;
+
+  // Finds the needed forecast information for each of the next 5 days (the first day is the current day)
+  for(let i = 0; i < 5; i++) {
+    //Sets the day of the week we are getting forecasts for
+    var selectedDay = currentDayOfWeek + i;
+    selectedDay = selectedDay > 6 ? 0 : selectedDay;
+
+    //Finds all the forecast points for the day
+    var forecastsForSelectedDay = allForecastPoints.filter(function (forecastPoint) {
+      var forecastTime = new Date(forecastPoint.dt_txt)
+      var forecastDayOfWeek = forecastTime.getDay();
+      return forecastDayOfWeek === selectedDay
+    })
+
+    //Finds the high temperature for the day
+    var highTemperature = forecastsForSelectedDay.reduce(function(currentHigh, forecastPoint) {
+      var temperature = forecastPoint.main.temp
+      return currentHigh >= temperature ? currentHigh : temperature;
+    }, 0)
+
+    //Finds the weather conditions at 3pm for the day (if the first forecast point for the day is already past 3pm then it finds the conditions at the first forecast point)
+    //TODO: Improve so that takes into account more than just the 3pm conditions in deciding what conditions to display
+    var indexThreePM = forecastsForSelectedDay.findIndex(function(forecastPoint) {
+      var forecastTime = forecastPoint.dt_txt.split(' ')[1];
+      return forecastTime === '15:00:00'
+    })
+    indexThreePM = indexThreePM === -1 ? 0 : indexThreePM;
+    var forecastedWeatherConditions = forecastsForSelectedDay[indexThreePM].weather[0].description;
+    var forecastedWeatherConditionsIcon = forecastsForSelectedDay[indexThreePM].weather[0].icon;
+
+
+    forecastInfo[i].forecastedTemperature = Math.round((highTemperature-273.15)*9/5+32);
+    forecastInfo[i].forecastedWeatherConditions = forecastedWeatherConditions;
+    forecastInfo[i].forecastedWeatherConditionsIcon = `http://openweathermap.org/img/wn/${forecastedWeatherConditionsIcon}@2x.png`
+    //Gets the day of the week in text for the day
+    var options = { weekday: 'long'};
+    forecastInfo[i].day = new Intl.DateTimeFormat('en-US', options).format(new Date(forecastsForSelectedDay[0].dt_txt))
+  }
 }
