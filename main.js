5e07e593b6aeec7a06c34510b8deed4c5e12f11c
Lauren Nicholls <lauren.nicholls1@gmail.com>
2021-04-15 09:08:38 -0400

    user can save city as default

--- main.js
+++ main.js
@@ -1,180 +1,198 @@
-var currentCityWeatherData = [];
-var fiveDayCityWeatherData = [];
+$(document).ready(function() {
 
-$('.search').on('click', function() {
-  var $currentCity = $('#search-query').val();
-  
-  currentCityWeatherData = [];
+  var currentCityWeatherData = [];
+  var fiveDayCityWeatherData = [];
 
-  $('#search-query').val('');
+  $('.search').on('click', function() {
+    var $currentCity = $('#search-query').val();
+    
+    currentCityWeatherData = [];
 
-  fetchDailyWeather($currentCity);
-  fetchFiveDayForecast($currentCity);
-});
+    $('#search-query').val('');
+    $('.set-default').css('display', 'block');
 
-$("#search-query").keypress(function(event) {
-  if (event.keyCode === 13) {
+    fetchDailyWeather($currentCity);
+    fetchFiveDayForecast($currentCity);
+  });
+
+  $("#search-query").keypress(function(event) {
+    if (event.keyCode === 13) {
+        $('.search').click();
+    }
+  });
+
+  $('.set-default').on('click', function () {
+      var defaultCity = currentCityWeatherData[0].city;
+      localStorage.setItem('cityName', defaultCity);
+  }); 
+
+  window.onload = function() {
+    var defaultCity = localStorage.getItem('cityName');
+    if (defaultCity !== null) {
+      $('#search-query').val(defaultCity); 
       $('.search').click();
+    }
+ }
+
+  var addCurrentCityWeatherData = function (data) {
+    var currentWeatherImageCode = data.weather[0].icon;
+    var imageSrc = 'http://openweathermap.org/img/wn/' + currentWeatherImageCode +'@2x.png';
+    $('#current-weather-icon').attr('src', imageSrc);
+    var weather_condition = data.weather[0].main;
+
+    var city = data.name;
+    var mapSrc = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyCDyFxxpN5RtNUPm8u2aCKZ8XMqc9mfxdE&q=' + city;
+    $('#map').attr('src', mapSrc);
+    $('.map').css('display', 'block');
+    
+
+    currentCityWeatherData.push({
+      temperature: data.main.temp,
+      city: data.name,
+      weather_conditions: data.weather[0].main,
+    });
+
+    tempToFahrenheit();
+    renderCurrentWeatherCityData();
+    generateBackgroundImage(weather_condition);
   }
-});
 
-var addCurrentCityWeatherData = function (data) {
-  var currentWeatherImageCode = data.weather[0].icon;
-  var imageSrc = 'http://openweathermap.org/img/wn/' + currentWeatherImageCode +'@2x.png';
-  $('#current-weather-icon').attr('src', imageSrc);
-  var weather_condition = data.weather[0].main;
+  var addFiveDayCityWeatherData = function (data) {
+    var forecastData = data.list;
+    var forecastDataArray = [];
+    forecastData.map(e => {
+      var temp = e.main.temp_max;
+      var conditions = e.weather[0].main;
+      var weatherIcon = e.weather[0].icon;
+      var date = new Date(e.dt_txt);
 
-  var city = data.name;
-  var mapSrc = 'https://www.google.com/maps/embed/v1/place?key=AIzaSyCDyFxxpN5RtNUPm8u2aCKZ8XMqc9mfxdE&q=' + city;
-  $('#map').attr('src', mapSrc);
-  $('.map').css('display', 'block');
-  
+      forecastDataArray.push({ temp: temp, conditions: conditions, weatherIcon: weatherIcon, date: date});
+    });
 
-  currentCityWeatherData.push({
-    temperature: data.main.temp,
-    city: data.name,
-    weather_conditions: data.weather[0].main,
-  });
+    convertToDayOfWeek(forecastDataArray);
+    compressWeeklyWeatherData(forecastDataArray);
+  }
 
-  tempToFahrenheit();
-  renderCurrentWeatherCityData();
-  generateBackgroundImage(weather_condition);
-}
+  var fetchDailyWeather = function (currentCity) {
+    $.ajax({
+      method: 'GET',
+      url: 'https://api.openweathermap.org/data/2.5/weather?q=' + currentCity + '&appid=345e1c9864ad7ebda8d87ea4d60c53f1',
+      dataType: 'json',
+      success: function (data) {
+        addCurrentCityWeatherData(data);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        alert(textStatus + ': City not found.');
+      }
+    })
+  }
 
-var addFiveDayCityWeatherData = function (data) {
-  var forecastData = data.list;
-  var forecastDataArray = [];
-  forecastData.map(e => {
-    var temp = e.main.temp_max;
-    var conditions = e.weather[0].main;
-    var weatherIcon = e.weather[0].icon;
-    var date = new Date(e.dt_txt);
+  var fetchFiveDayForecast = function (currentCity) {
+    $.ajax({
+      method: 'GET',
+      url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + currentCity + '&appid=345e1c9864ad7ebda8d87ea4d60c53f1',
+      dataType: 'json',
+      success: function (data) {
+        addFiveDayCityWeatherData(data);
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log(errorThrown);
+      }
+    })
+  }
 
-    forecastDataArray.push({ temp: temp, conditions: conditions, weatherIcon: weatherIcon, date: date});
-  });
+  var tempToFahrenheit = function () {
+    
+    var tempKelvin = currentCityWeatherData[0].temperature;
+    var tempFahrenheit = Math.floor(1.8 * (tempKelvin - 273) + 32);
 
-  convertToDayOfWeek(forecastDataArray);
-  compressWeeklyWeatherData(forecastDataArray);
-}
+    currentCityWeatherData[0].temperature = tempFahrenheit.toString() + '°';
+  }
 
-var fetchDailyWeather = function (currentCity) {
-  $.ajax({
-    method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/weather?q=' + currentCity + '&appid=345e1c9864ad7ebda8d87ea4d60c53f1',
-    dataType: 'json',
-    success: function (data) {
-      addCurrentCityWeatherData(data);
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      alert(textStatus + ': City not found.');
-    }
-  })
-}
+  var convertToDayOfWeek = function (forecastDataArray) {
+    var daysOfTheWeek = [ { 0: 'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday' } ];
+    
+    forecastDataArray.forEach(e => {
+      var dayCode = e.date.getDay();
+      return e.date = daysOfTheWeek[0][dayCode];
+    })
+  }
 
-var fetchFiveDayForecast = function (currentCity) {
-  $.ajax({
-    method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + currentCity + '&appid=345e1c9864ad7ebda8d87ea4d60c53f1',
-    dataType: 'json',
-    success: function (data) {
-      addFiveDayCityWeatherData(data);
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.log(errorThrown);
-    }
-  })
-}
+  var compressWeeklyWeatherData = function (forecastDataArray) {
+    var weeklyData = _.values(_.groupBy(forecastDataArray, 'date'));
 
-var tempToFahrenheit = function () {
-  
-  var tempKelvin = currentCityWeatherData[0].temperature;
-  var tempFahrenheit = Math.floor(1.8 * (tempKelvin - 273) + 32);
+    fiveDayCityWeatherData = [];
 
-  currentCityWeatherData[0].temperature = tempFahrenheit.toString() + '°';
-}
+    weeklyData.forEach(day => {
+      var weekday = day[0].date;
+      var highestTemp = 0;
+      var dailyConditions = [];
+      var dailyIcons = [];
 
-var convertToDayOfWeek = function (forecastDataArray) {
-  var daysOfTheWeek = [ { 0: 'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday' } ];
-  
-  forecastDataArray.forEach(e => {
-    var dayCode = e.date.getDay();
-    return e.date = daysOfTheWeek[0][dayCode];
-  })
-}
+      for (let i = 0; i < day.length; i++) {
+        if (day[i].temp > highestTemp) {
+          highestTemp = day[i].temp;
+        }
 
-var compressWeeklyWeatherData = function (forecastDataArray) {
-  var weeklyData = _.values(_.groupBy(forecastDataArray, 'date'));
-
-  fiveDayCityWeatherData = [];
-
-  weeklyData.forEach(day => {
-    var weekday = day[0].date;
-    var highestTemp = 0;
-    var dailyConditions = [];
-    var dailyIcons = [];
-
-    for (let i = 0; i < day.length; i++) {
-      if (day[i].temp > highestTemp) {
-        highestTemp = day[i].temp;
+      dailyConditions.push(day[i].conditions);  
+      dailyIcons.push(day[i].weatherIcon);
       }
 
-    dailyConditions.push(day[i].conditions);  
-    dailyIcons.push(day[i].weatherIcon);
-    }
+      var temp = Math.floor(1.8 * (highestTemp - 273) + 32);
+      temp = temp.toString() + '°';
 
-    var temp = Math.floor(1.8 * (highestTemp - 273) + 32);
-    temp = temp.toString() + '°';
+      var mostCommonDailyCondition = _.head(_(dailyConditions)
+        .countBy()
+        .entries()
+        .maxBy(_.last));
 
-    var mostCommonDailyCondition = _.head(_(dailyConditions)
-      .countBy()
-      .entries()
-      .maxBy(_.last));
+      var mostCommonWeatherIcon = _.head(_(dailyIcons)
+        .countBy()
+        .entries()
+        .maxBy(_.last));
 
-    var mostCommonWeatherIcon = _.head(_(dailyIcons)
-      .countBy()
-      .entries()
-      .maxBy(_.last));
+      var weatherIconSrc = 'http://openweathermap.org/img/wn/' + mostCommonWeatherIcon +'@2x.png';
 
-    var weatherIconSrc = 'http://openweathermap.org/img/wn/' + mostCommonWeatherIcon +'@2x.png';
+        fiveDayCityWeatherData.push({ day_of_the_week: weekday, temperature: temp, icon: weatherIconSrc, weather_conditions: mostCommonDailyCondition});
+    })
 
-      fiveDayCityWeatherData.push({ day_of_the_week: weekday, temperature: temp, icon: weatherIconSrc, weather_conditions: mostCommonDailyCondition});
-  })
+    if (fiveDayCityWeatherData.length > 5) {
+      fiveDayCityWeatherData.pop();
+    };
 
-  if (fiveDayCityWeatherData.length > 5) {
-    fiveDayCityWeatherData.pop();
-  };
-
-  renderWeekyWeatherData(fiveDayCityWeatherData);
-}
-
-var renderCurrentWeatherCityData = function () {
-  $('.current-city-data').empty();
-
-  var source = $('#current-city-weather-template').html();
-  var template = Handlebars.compile(source);
-  var newHTML = template(currentCityWeatherData[0]);
-
-  $('.current-city-data').append(newHTML);
-}
-
-var renderWeekyWeatherData = function (fiveDayCityWeatherData) {
-  $('.weekly-weather').empty();
-
-  fiveDayCityWeatherData.forEach(day => {
-    var source = $('#five-day-forecast-template').html();
-    var template = Handlebars.compile(source);
-    var newHTML = template(day);
-  
-    $('.weekly-weather').append(newHTML);
-  })
-}
-
-generateBackgroundImage = function (weather_condition) {
-  var weather_conditions = [{Thunderstorm: 'thunderstorm.jpg', Rain: 'rain.jpg', Drizzle: 'drizzle.jpg', Snow: 'snow.jpg', Mist: 'mist.jpg', Smoke: 'smoke.jpg', Haze: 'haze.jpg', Dust: 'dust.jpg', Fog: 'fog.jpg', Sand: 'sand.jpg', Ash: 'ash.jpg', Squall: 'squall.jpg', Tornado: 'tornado.jpg', Clear: 'clear.jpg', Clouds: 'clouds.jpg'}];
-  
-  var weatherImage = weather_conditions[0][weather_condition];
-
-  if (weatherImage) {
-    $('.background').css('background', 'url(images/' + weatherImage + ') no-repeat center center fixed');
+    renderWeekyWeatherData(fiveDayCityWeatherData);
   }
-}
+
+  var renderCurrentWeatherCityData = function () {
+    $('.current-city-data').empty();
+
+    var source = $('#current-city-weather-template').html();
+    var template = Handlebars.compile(source);
+    var newHTML = template(currentCityWeatherData[0]);
+
+    $('.current-city-data').append(newHTML);
+  }
+
+  var renderWeekyWeatherData = function (fiveDayCityWeatherData) {
+    $('.weekly-weather').empty();
+
+    fiveDayCityWeatherData.forEach(day => {
+      var source = $('#five-day-forecast-template').html();
+      var template = Handlebars.compile(source);
+      var newHTML = template(day);
+    
+      $('.weekly-weather').append(newHTML);
+    })
+  }
+
+  generateBackgroundImage = function (weather_condition) {
+    var weather_conditions = [{Thunderstorm: 'thunderstorm.jpg', Rain: 'rain.jpg', Drizzle: 'drizzle.jpg', Snow: 'snow.jpg', Mist: 'mist.jpg', Smoke: 'smoke.jpg', Haze: 'haze.jpg', Dust: 'dust.jpg', Fog: 'fog.jpg', Sand: 'sand.jpg', Ash: 'ash.jpg', Squall: 'squall.jpg', Tornado: 'tornado.jpg', Clear: 'clear.jpg', Clouds: 'clouds.jpg'}];
+    
+    var weatherImage = weather_conditions[0][weather_condition];
+
+    if (weatherImage) {
+      $('.background').css('background', 'url(images/' + weatherImage + ') no-repeat center center fixed');
+    }
+  }
+
+});
