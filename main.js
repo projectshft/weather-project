86f891c91efa89e6f275e61b386d034e1a799204
dalva19 <75229602+dalva19@users.noreply.github.com>
2021-04-16 16:20:59 -0400

    forcast script and render

--- main.js
+++ main.js
@@ -10,7 +10,7 @@
 //fetch fxn using current weather api
 //addCurrentWeather fxn to loop and render
 //find weather icons
-
+/*
 var apiKey = '76e35e9aadf52246c6368e03bedbcecb'; 
 
 var currentWeather = [];
@@ -37,7 +37,7 @@ addCurrentWeather = function (data) {
   currentWeather.push(currentCityWeather); 
   
   renderCurrentWeather();
-  renderCurrentWeatherIcon(); 
+  //renderCurrentWeatherIcon(); 
 };
 
 var fetch = function (query) {
@@ -62,7 +62,13 @@ var renderCurrentWeather = function () {
     var template = Handlebars.compile(source);
     var currentWeatherHTML = template(currentWeather[i]);
     $('.current-weather').append(currentWeatherHTML); 
+
+    var iconSource = $('#current-weather-icon-template').html();
+    var iconTemplate = Handlebars.compile(iconSource);
+    var currentWeatherIconHTML = iconTemplate(currentWeather[i]);
+    $('.current-weather').append(currentWeatherIconHTML); 
   };
+  
 }; 
 
 var renderCurrentWeatherIcon = function () {
@@ -74,6 +80,53 @@ var renderCurrentWeatherIcon = function () {
     $('.current-weather').append(currentWeatherIconHTML); 
   };
 }; 
+*/
 
 
+//story 2
+//forcast data
+//A user should be able to do all he/she could do in the first part.
+//When a user searches, they should additionally see a 5-day forecast, and each of the five days should have an associated day of the week, weather condition and temperature.
+
+var weatherForcast = [
+  {
+    forcastWeatherCondition: 'clouds',
+    forcastTemp: 66,
+    forcastWeatherIcon: "http://openweathermap.org/img/wn/10d@2x.png"
+  },
+  {
+    forcastWeatherCondition: 'clouds',
+    forcastTemp: 68,
+    forcastWeatherIcon: "http://openweathermap.org/img/wn/10d@2x.png"
+  },
+  {
+    forcastWeatherCondition: 'clouds',
+    forcastTemp: 70,
+    forcastWeatherIcon: "http://openweathermap.org/img/wn/10d@2x.png"
+  },
+  {
+    forcastWeatherCondition: 'clouds',
+    forcastTemp: 66,
+    forcastWeatherIcon: "http://openweathermap.org/img/wn/10d@2x.png"
+  }, 
+  {
+    forcastWeatherCondition: 'clouds',
+    forcastTemp: 66,
+    forcastWeatherIcon: "http://openweathermap.org/img/wn/10d@2x.png"
+  }
+];
+
+var renderForcastWeather = function () {
+  $('.forcast').empty(); 
+  
+  for(let i = 0; i < weatherForcast.length; i++) {
+    var dayForcast = weatherForcast[i]; 
+    var source = $('#weather-forcast').html(); 
+    var template = Handlebars.compile(source); 
+    var forcastHTML = template(dayForcast); 
+    $('.forcast').append(forcastHTML); 
+  };
+};
+
+renderForcastWeather(); 
 
