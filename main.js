3376effbb9c60689b8933bf371c9202456fad2b0
Daniel Piston <danielpiston@Daniels-MacBook-Pro.local>
2021-04-20 20:40:24 -0400

    added helper functions and current weather icon

--- main.js
+++ main.js
@@ -19,34 +19,40 @@ var fetch = function (searchCity) {
   });
 }
 
+// put helper functions here
+// convert temperature K to F and round it for prettiness
+var helperKelvin = function (currentTemp) {
+  return Math.round((9 / 5) * (currentTemp - 273) + 32) + "°"
+}
+
+// capitalize first letter of the weather description
+var helperCapitalizeFirstLetter = function (phrase) {
+  return phrase.charAt(0).toUpperCase() + phrase.slice(1);
+}
+
+// get weather icon
+var helperGetIcon = function (iconCode) {
+  return "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+}
+
 var addCurrentWeatherLeft = function (weatherDay) {
-  // convert temperature K to F and round it for prettiness
-  var tempF = Math.round((9 / 5) * (weatherDay.main.temp - 273) + 32) + "°"; 
-
-  // capitalize first letter of the weather description
-  var desc =
-    weatherDay.weather[0].description.charAt(0).toUpperCase() +
-    weatherDay.weather[0].description.slice(1);
-
-  var tempCurrentWeatherLeft = {
-    "current-temp": tempF || null,
-    "city": weatherDay.name || null,
-    "current-weather-desc": desc || null,
-  };
-
-  renderCurrentWeather(tempCurrentWeatherLeft);
+  // run renderCurrentWeather with current api retrieved data
+  renderCurrentWeather({
+    "current-temp": helperKelvin(weatherDay.main.temp),
+    "city": weatherDay.name,
+    "current-weather-desc": helperCapitalizeFirstLetter(weatherDay.weather[0].description),
+    "current-icon": helperGetIcon(weatherDay.weather[0].icon),
+  });
 };
 
-var renderCurrentWeather = function (x) {
+var renderCurrentWeather = (function (x) {
   $(".current-weather").empty();
 
   var source = $("#current-weather-template").html();
   var template = Handlebars.compile(source);
   var newHTML = template(x);
   $(".current-weather").append(newHTML);
-}
-
-// need to center the text and add weather icon
+})// need to center the text and add weather icon
 
 // weatherDay.weather[0].icon also exists
 // img is http://http://openweathermap.org/img/wn/' + icon + '@2x.png'
