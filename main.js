9bc12f9644a42532c9301148771a09b027920ea1
Corrine Luthy <corrine@corrines-air.home>
2021-04-21 16:34:44 -0400

    finish part 1

--- main.js
+++ main.js
@@ -1,23 +1,49 @@
-var currentWeather = [
-  {
-    temp: 98,
-    name: "Zebulon",
-    main: "Sunny",
-    icon: "SUN GRAPHIC"
-  }
-];
+var currentWeather = {};
 
+
+//on click, generates search query and invokes fetch function (API call)
 $('.search').on('click', function () {
-  console.log('clicked search')
+  var weatherSearch = $('#search-query').val();
+
+  fetch(weatherSearch);
 });
 
-//renders what's in the currentWeather array
+var fetch = function (query) {
+  var apiKey = "3ba2ed09725ebf9563a4db3c40b2c22f"
+
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial&appid=" + apiKey,
+    dataType: "json",
+    success: function(data) {
+      formatCurrentWeather(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+};
+
+// function that formats API search results & pushes them into the currentWeather object
+
+var formatCurrentWeather = function (data) {
+  currentWeather = {
+    temp: Math.round(data.main.temp) + '\u00B0',
+    name: data.name,
+    main: data.weather[0].main,
+    icon: 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png'
+  }
+
+  renderCurrentWeather();
+};
+
+//renders what's in the currentWeather object
 var renderCurrentWeather = function () {
   $('.current-weather').empty();
 
   var source = $('#current-weather-template').html();
   var template = Handlebars.compile(source);
-  var newHTML = template(currentWeather[0]);
+  var newHTML = template(currentWeather);
   
   $('.current-weather').append(newHTML);
 };
