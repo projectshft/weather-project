4f56bcd5c55c8b47f588f21e8169b4f42a65ead1
Daniel Piston <danielpiston@Daniels-MacBook-Pro.local>
2021-04-22 20:35:52 -0400

    js works out fine, HTML needs a lot of work

--- main.js
+++ main.js
@@ -6,16 +6,33 @@ $(".search").on("click", function () {
 })
 
 var fetch = function (searchCity) {
+  // get current weather
   $.ajax({
     method: "GET",
     url: "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=67dd541f698260dd8fa2d7b872c650a3",
     dataType: "json",
-    success: function(weatherDay) {
-      addCurrentWeatherLeft(weatherDay);
+    success: function (weatherDay) {
+      addCurrentWeather(weatherDay);
     },
     error: function (jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
     }
+  });
+
+  // get five-day forecast
+  $.ajax({
+    method: "GET",
+    url:
+      "https://api.openweathermap.org/data/2.5/forecast?q=" +
+      searchCity +
+      "&appid=67dd541f698260dd8fa2d7b872c650a3",
+    dataType: "json",
+    success: function (weatherFiveDay) {
+      addFiveDayWeather(weatherFiveDay);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    },
   });
 }
 
@@ -35,7 +52,7 @@ var helperGetIcon = function (iconCode) {
   return "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
 }
 
-var addCurrentWeatherLeft = function (weatherDay) {
+var addCurrentWeather = function (weatherDay) {
   // run renderCurrentWeather with current api retrieved data
   renderCurrentWeather({
     "current-temp": helperKelvin(weatherDay.main.temp),
@@ -45,6 +62,43 @@ var addCurrentWeatherLeft = function (weatherDay) {
   });
 };
 
+var addFiveDayWeather = function ( weatherFiveDay) {
+  let tempSum = 0;
+  let dataPerDay = weatherFiveDay.list.length / 5;
+  let fiveDayDescs = [];
+  let fiveDayTemps = [];
+  let fiveDayIcons = [];
+  let fiveDayDays = [];
+
+  for (let i=0; i<weatherFiveDay.list.length; i++) {
+    var temp = weatherFiveDay.list[i].main.temp;
+    tempSum += temp;
+    var icon = weatherFiveDay.list[i].weather[0].icon;
+    var desc = weatherFiveDay.list[i].weather[0].description;
+    var day = moment(weatherFiveDay.list[i].dt_txt).format("dddd");
+
+    if ((i + 1) % dataPerDay == 0) {
+      tempSum = tempSum / dataPerDay;
+      fiveDayTemps.push(helperKelvin(tempSum));
+      fiveDayIcons.push(helperGetIcon(icon));
+      fiveDayDays.push(day);
+      fiveDayDescs.push(desc);
+      tempSum = 0;
+    }
+  }
+
+  for (let j=0; j<5; j++) {
+    result = {
+      "five-day-desc": fiveDayDescs[j],
+      "five-day-temp": fiveDayTemps[j],
+      "five-day-icon": fiveDayIcons[j],
+      "five-day-weather-day": fiveDayDays[j],
+    };
+
+    renderFiveDayWeather(result); // prints out 31???
+  }
+};
+
 var renderCurrentWeather = (function (x) {
   $(".current-weather").empty();
 
@@ -52,7 +106,12 @@ var renderCurrentWeather = (function (x) {
   var template = Handlebars.compile(source);
   var newHTML = template(x);
   $(".current-weather").append(newHTML);
-})// need to center the text and add weather icon
+});
 
-// weatherDay.weather[0].icon also exists
-// img is http://http://openweathermap.org/img/wn/' + icon + '@2x.png'
+var renderFiveDayWeather = (function (x) {
+  var source = $("#five-day-weather-template").html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(x);
+  console.log(newHTML);
+  $(".five-day-weather").append(newHTML);
+});
