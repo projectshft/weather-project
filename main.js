99f4ca549430babd060e55ae11b8f8fcd1f5debd
Evgeny Ruzanov <evgenyruzanov@Evgenys-MacBook-Pro.local>
2021-04-29 10:16:55 -0400

    10-26-21 commit

--- main.js
+++ main.js
@@ -1,29 +1,29 @@
-var weather= [];
-
 $('.search').on('click', function () {
-  var appid = $('#search-query').val();
+  var cityName = $('#search-query').val();
 
   $('#search-query').val('');
 
-  fetch(appid);
+  fetch(cityName);
 });
 
-var addWeather = function (data) {;
-  weather.push({
-    city_name: data.city_name,
-    API_key: data.API_key
-  });
-
-  renderWeather();
-};
-
-var fetch = function (appid) {
-  $.ajax({
+var fetch = function (city) {
+ $.ajax({
     method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/weather?q={{city name}}' + 'appid={{API key}}',
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=d4ca3f29ba27c26dac6f9144d941ec22`,
     dataType: 'json',
     success: function (data) {
-      addWeather(data);
+      renderWeather(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  })
+  $.ajax({
+    method: 'GET',
+    url: `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=d4ca3f29ba27c26dac6f9144d941ec22`,
+    dataType: 'json',
+    success: function (data) {
+      renderForecast(data);
     },
     error: function (jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
@@ -31,18 +31,58 @@ var fetch = function (appid) {
   })
 };
 
-var renderWeather = function () {
+var renderWeather = function (data) {
   $('.weather').empty();
 
-  for (let i = 0; i < weather.length; i++) {
-    const city = weather[i];
+  
+    const weather = {
+      city: data.name,
+      temp: data.main.temp,
+      condition: data.weather[0].description,
+      icon: data.weather[0].icon
+   };
     
     var source = $('#weather-template').html();
     var template = Handlebars.compile(source);
-    var newHTML = template(city);
+    var newHTML = template(weather);
 
     $('.weather').append(newHTML);
-  }
-};
+  };
 
-renderWeather();
+  
+
+  var renderForecast = function (data) {
+    $('.forecast').empty();
+  
+    //var m = moment('data.list[7].dt', 'dddd');
+    //const day = data.list[7].dt;
+    //const date = new Date(day*1000);
+
+
+      const forecast = {
+        condition: data.list[7].weather[0].description,
+        temp: data.list[7].main.temp,
+        icon: data.list[7].weather[0].icon,
+        //date: m,
+        condition1: data.list[15].weather[0].description,
+        temp1: data.list[15].main.temp,
+        icon1: data.list[15].weather[0].icon,
+        condition2: data.list[23].weather[0].description,
+        temp2: data.list[23].main.temp,
+        icon2: data.list[23].weather[0].icon,
+        condition3: data.list[31].weather[0].description,
+        temp3: data.list[31].main.temp,
+        icon3: data.list[31].weather[0].icon,
+        condition4: data.list[39].weather[0].description,
+        temp4: data.list[39].main.temp,
+        icon4: data.list[39].weather[0].icon
+        
+      };
+      
+      var source = $('#forecast-template').html();
+      var template = Handlebars.compile(source);
+      var newHTML = template(forecast);
+  
+      $('.forecast').append(newHTML);
+    };
+    
