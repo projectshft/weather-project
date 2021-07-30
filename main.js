90d0583cf570184ca1d44b2cc4832d96f2929df9
Ryan Passer <rpasser93@gmail.com>
2021-07-30 15:17:31 -0400

    Convert data to imperial system

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ fiveDayWeatherData = [];
 var fetchCurrentWeatherData = function(city) {
   $.ajax({
     method: "GET",
-    url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=83b3a3ff4100ab8f87dc416f724b6120`,
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=83b3a3ff4100ab8f87dc416f724b6120`,
     dataType: "json",
     success: function(data) {
       addCurrentWeatherData(data);
