8ae6f0b3e98bb1facaecbc3f4293660bd59b9eef
Ryan Passer <rpasser93@gmail.com>
2021-07-30 15:58:58 -0400

    Add 5 day forecast into fetch function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 currentWeatherData  = [];
-fiveDayWeatherData = [];
+fiveDayForecastData = [];
 
-var fetchCurrentWeatherData = function(city) {
+var fetchWeatherData = function(city) {
   $.ajax({
     method: "GET",
     url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=83b3a3ff4100ab8f87dc416f724b6120`,
@@ -13,16 +13,47 @@ var fetchCurrentWeatherData = function(city) {
       console.log(textStatus);
     }
   });
+  $.ajax({
+    method: "GET",
+    url: `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=83b3a3ff4100ab8f87dc416f724b6120`,
+    dataType: "json",
+    success: function(data) {
+      addFiveDayForecastData(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
 };
 
 var addCurrentWeatherData = function(data) {
-console.log(data)
+  var city = data.name;
+  var temp = Math.round(data.main.temp);
+  var descrip = data.weather[0].description;
+  var iconURL = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  
+  var pushObj = {
+    city: city || null,
+    temp: temp || null,
+    descrip: descrip || null,
+    iconURL: iconURL || null
+  };
+
+  currentWeatherData.push(pushObj);
+  console.log(currentWeatherData); ////////
+};
+
+var addFiveDayForecastData = function(data) {
+  console.log('five day data:');
+  console.log(data);
 };
 
 $('button').on('click', function() {
-  var cityInput = $('input').val();
+  currentWeatherData  = [];
+  fiveDayForecastData = []; 
 
+  var cityInput = $('input').val();
   $('input').val();
 
-  fetchCurrentWeatherData(cityInput);
+  fetchWeatherData(cityInput);
 });
