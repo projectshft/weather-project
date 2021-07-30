f26e47132704648138bbf906631f3e2b2cdaec80
Ryan Passer <rpasser93@gmail.com>
2021-07-30 15:08:42 -0400

    Add initial HTML and set up search click event listener

---
+++ main.js
@@ -0,0 +1,28 @@
+currentWeatherData  = [];
+fiveDayWeatherData = [];
+
+var fetchCurrentWeatherData = function(city) {
+  $.ajax({
+    method: "GET",
+    url: `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=83b3a3ff4100ab8f87dc416f724b6120`,
+    dataType: "json",
+    success: function(data) {
+      addCurrentWeatherData(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+};
+
+var addCurrentWeatherData = function(data) {
+console.log(data)
+};
+
+$('button').on('click', function() {
+  var cityInput = $('input').val();
+
+  $('input').val();
+
+  fetchCurrentWeatherData(cityInput);
+});
