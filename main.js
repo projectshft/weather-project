529063814495f73990a4da7ca4fce41175a2f940
Ryan Passer <rpasser93@gmail.com>
2021-07-31 16:57:17 -0400

    Finalize CSS for rendered weather data

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ var addCurrentWeatherData = function(data) {
   currentWeatherData  = [];
 
   var city = data.name;
-  var temp = Math.round(data.main.temp);
+  var temp = Math.round(data.main.temp) + "\u00B0";
   var descrip = data.weather[0].main;
   var iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
   
@@ -42,11 +42,9 @@ var addCurrentWeatherData = function(data) {
   };
 
   currentWeatherData.push(pushObj);
-  // console.log(currentWeatherData); ////////
 };
 
 var addFiveDayForecastData = function(data) {
-  // console.log(data); ///////////
   fiveDayForecastData = [];
   var sumTemp1 = 0;
   var sumTemp2 = 0;
@@ -73,41 +71,39 @@ var addFiveDayForecastData = function(data) {
   };
 
   var obj1 = {
-    descrip: data.list[4].weather[0].main || null,
-    temp: Math.round(sumTemp1 / 8) || null,
-    iconUrl: `http://openweathermap.org/img/wn/${data.list[4].weather[0].icon}@2x.png` || null,
+    descrip: data.list[7].weather[0].main || null,
+    temp: Math.round(sumTemp1 / 8) + "\u00B0" || null,
+    iconUrl: `http://openweathermap.org/img/wn/${data.list[7].weather[0].icon}@2x.png` || null,
     dayOfWeek: moment().add(1,'days').format('dddd') || null
   };
   var obj2 = {
-    descrip: data.list[12].weather[0].main || null,
-    temp: Math.round(sumTemp2 / 8) || null,
-    iconUrl: `http://openweathermap.org/img/wn/${data.list[12].weather[0].icon}@2x.png` || null,
+    descrip: data.list[15].weather[0].main || null,
+    temp: Math.round(sumTemp2 / 8) + "\u00B0" || null,
+    iconUrl: `http://openweathermap.org/img/wn/${data.list[15].weather[0].icon}@2x.png` || null,
     dayOfWeek: moment().add(2,'days').format('dddd') || null
   };
   var obj3 = {
-    descrip: data.list[20].weather[0].main || null,
-    temp: Math.round(sumTemp3 / 8) || null,
-    iconUrl: `http://openweathermap.org/img/wn/${data.list[20].weather[0].icon}@2x.png` || null,
+    descrip: data.list[23].weather[0].main || null,
+    temp: Math.round(sumTemp3 / 8) + "\u00B0" || null,
+    iconUrl: `http://openweathermap.org/img/wn/${data.list[23].weather[0].icon}@2x.png` || null,
     dayOfWeek: moment().add(3,'days').format('dddd') || null
   };
   var obj4 = {
-    descrip: data.list[28].weather[0].main || null,
-    temp: Math.round(sumTemp4 / 8) || null,
-    iconUrl: `http://openweathermap.org/img/wn/${data.list[28].weather[0].icon}@2x.png` || null,
+    descrip: data.list[31].weather[0].main || null,
+    temp: Math.round(sumTemp4 / 8) + "\u00B0" || null,
+    iconUrl: `http://openweathermap.org/img/wn/${data.list[31].weather[0].icon}@2x.png` || null,
     dayOfWeek: moment().add(4,'days').format('dddd') || null
   };
   var obj5 = {
-    descrip: data.list[36].weather[0].main || null,
-    temp: Math.round(sumTemp5 / 8) || null,
-    iconUrl: `http://openweathermap.org/img/wn/${data.list[36].weather[0].icon}@2x.png` || null,
+    descrip: data.list[39].weather[0].main || null,
+    temp: Math.round(sumTemp5 / 8) + "\u00B0" || null,
+    iconUrl: `http://openweathermap.org/img/wn/${data.list[39].weather[0].icon}@2x.png` || null,
     dayOfWeek: moment().add(5,'days').format('dddd') || null
   };
-
+  
   fiveDayForecastData.push(obj1, obj2, obj3, obj4, obj5);
 
-  // console.log(fiveDayForecastData);/////////
   renderWeatherData();
-
 };
 
 var renderWeatherData = function() {
