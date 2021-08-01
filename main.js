bcd017b453414d87212bab89deada25a140815cd
Joshua Clarke Cushing <joshuacushing@Joshuas-Air.home>
2021-07-31 21:05:17 -0400

    create new columns

--- main.js
+++ main.js
@@ -32,6 +32,24 @@ var pushCityWeather = function (data) {
   renderWeather();
 };
 
+var pushCityForcast = function () {
+  var input = $('.input-box').val(); 
+  
+  var template = {
+    temp: `${data.main.temp}`,
+    city: `${input}`,
+    description: `${data.weather[0].description}`
+  };
+
+  var icon = `${data.weather[0].icon}`
+  var iconDisplay =  "<img src='http://openweathermap.org/img/wn/" + icon + "@4x.png'>"
+
+  cityWeather.push(template);
+  cityWeatherIcon.push(iconDisplay);
+
+  renderWeather();
+};
+
 var fetch = function (query) {
   $.ajax({
     method: "GET",
@@ -39,6 +57,18 @@ var fetch = function (query) {
     dataType: "json",
     success: function (data) {
       pushCityWeather(data);
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    },
+  });
+
+  $.ajax({
+    method: "GET",
+    url:'http://api.openweathermap.org/data/2.5/forecast?units=imperial&q=' + query + '&appid=9b71dd7687d5daeb5225c83041aa3ed4',
+    dataType: "json",
+    success: function (data) {
+      pushCityForcast(data);
     },
     error: function (jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
