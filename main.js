92e35303c6dbd005ee5c5a4610fcf76b2f6d7e09
Jason Stover <jstover8807@gmail.com>
2021-08-24 15:33:34 -0400

    Second commit daily weather working correctly

--- main.js
+++ main.js
@@ -11,17 +11,17 @@ var addWeather = function (data) {
     weather.push({
         temp: data.main.temp,
         name: data.name,
-        description: data.weather.description,
-        icon: data.weather.icon
+        description: data.weather[0].description,
+        icon: "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
     });
-    console.log(weather);
+    
     displayWeather();
 }
 
 var fetch = function (cityName) {
     $.ajax({
       method: 'GET',
-      url: 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=af4517becabe6b999a8031c609577a9a',
+      url: 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=af4517becabe6b999a8031c609577a9a',
       dataType: 'json',
       success: function (data) {
         addWeather(data);
@@ -34,7 +34,7 @@ var fetch = function (cityName) {
 
 var displayWeather = function () {
     $('.today-row').empty();
-    //$('.weekly row').empty();
+    $('#weekly row').empty();
 
     var today = weather[0];
     var sourceToday = $('#today-template').html();
@@ -42,15 +42,14 @@ var displayWeather = function () {
     var newHTML1 = todayTemplate(today);
 
     $('.today-row').append(newHTML1);
-/*
+
     for (var i = 1; i < weather.length; i++) {
         var weekly = weather[i];
         var sourceWeekly = $('#weekly-template').html();
         var weeklyTemplate = Handlebars.compile(sourceWeekly);
         var newHTML2 = weeklyTemplate(weekly);
 
-        $('.weekly row').append(newHTML2);
-    } 
-*/  
+        $('#weekly row').append(newHTML2);
+    }   
 };
-displayWeather();
+
