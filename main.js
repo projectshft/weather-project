3cf94bdabee9b403b6edaaf73641ebdc092cb4ac
Jason Stover <jstover8807@gmail.com>
2021-08-24 16:13:31 -0400

    Weekly forecast API key error

--- main.js
+++ main.js
@@ -1,30 +1,21 @@
-var weather = [];
+var todaysWeather = [];
+var weeklyWeather = [];
 
 
 $('.search').on('click', function () {
     var cityName = $('#search-query').val();  
     
-    fetch(cityName);
+    fetchToday(cityName);
+    fetchFiveDay(cityName);
   });
 
-var addWeather = function (data) {
-    weather.push({
-        temp: data.main.temp,
-        name: data.name,
-        description: data.weather[0].description,
-        icon: "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
-    });
-    
-    displayWeather();
-}
-
-var fetch = function (cityName) {
+var fetchToday = function (cityName) {
     $.ajax({
       method: 'GET',
       url: 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial&appid=af4517becabe6b999a8031c609577a9a',
       dataType: 'json',
       success: function (data) {
-        addWeather(data);
+        addTodaysWeather(data);
       },
       error: function (jqXHR, textStatus, errorThrown) {
         console.log(textStatus);
@@ -32,16 +23,60 @@ var fetch = function (cityName) {
    })
 };
 
-var displayWeather = function () {
-    $('.today-row').empty();
-    $('#weekly row').empty();
 
-    var today = weather[0];
+var addTodaysWeather = function (data) {
+    todaysWeather.push({
+        temp: data.main.temp,
+        name: data.name,
+        description: data.weather[0].description,
+        icon: "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+    });
+    console.log(todaysWeather);
+    displayTodaysWeather();
+}
+
+var displayTodaysWeather = function () {
+    $('.today-row').empty();    
+
+    var today = todaysWeather[0];
     var sourceToday = $('#today-template').html();
     var todayTemplate = Handlebars.compile(sourceToday);
     var newHTML1 = todayTemplate(today);
 
     $('.today-row').append(newHTML1);
+
+    
+    todaysWeather = []; 
+};
+
+var fetchFiveDay = function (cityName) {
+    $.ajax({
+        method: 'GET',
+        url: 'https://api.openweathermap.org/data/2.5/forecast/daily?q=' + cityName + '&cnt=5&appid=af4517becabe6b999a8031c609577a9a',
+        dataType: 'json',
+        success: function (data) {
+            addWeeklyWeather(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log(textStatus);
+        }
+    })
+};
+
+var addWeeklyWeather = function (data) {
+    weeklyWeather.push({
+        weeklyTemp: data.list.temp.day,
+        dayOfWeek: data.list.dt,
+        weeklyDescription: data.list.id,
+        weeklyIcon: "http://openweathermap.org/img/wn/" + data.list.weather.icon + "@2x.png"
+    });
+    console.log(weeklyWeather);
+    displayWeeklyWeather();
+}
+
+
+var displayWeeklyWeather = function () {
+    $('#weekly row').empty();
 
     for (var i = 1; i < weather.length; i++) {
         var weekly = weather[i];
@@ -50,6 +85,9 @@ var displayWeather = function () {
         var newHTML2 = weeklyTemplate(weekly);
 
         $('#weekly row').append(newHTML2);
-    }   
-};
+    }  
+
+    weeklyWeather = [];
+
+}
 
