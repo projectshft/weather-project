f6a2671bd7b9fea8329e15931ca7ea56e0a2e24a
Kush Patel <Kush@Kushs-MacBook-Pro-2.local>
2021-08-29 15:46:50 -0400

    added 5-day forecast

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-var APPID = 'ba4fcb43b4e6c7724e8c8d168e46fa49';
-
 $('.search').on('click', function () {
   var queryCity = $('#search-query').val();
-  fetch(queryCity);  
+  fetchCurrentWeather(queryCity);
+  fetchForecast(queryCity);  
 
   // reset input field and return focus
   var $searchQuery = $('#search-query');
@@ -21,10 +20,10 @@ var addCurrentWeather = function (data) {
   renderCurrentWeather(currentWeather);
 };
 
-var fetch = function (queryCity) {
+var fetchCurrentWeather = function (queryCity) {
   $.ajax({
     method: "GET",
-    url: 'https://api.openweathermap.org/data/2.5/weather?q=' + queryCity + '&appid=' + APPID,
+    url: 'https://api.openweathermap.org/data/2.5/weather?q=' + queryCity + '&appid=ba4fcb43b4e6c7724e8c8d168e46fa49',
     dataType: "json",
     success: function(data) {
       addCurrentWeather(data);
@@ -44,4 +43,54 @@ var renderCurrentWeather = function (currentWeather) {
   var newHTML = template(currentWeather);
 
   $('.current-weather').append(newHTML);
+};
+
+var addForecast = function (data) {
+  forecast = {
+    forecastDays: []
+  };
+
+  // choose every 8th item to create a forecast every 24 h
+  for (var i = 7; i < data.list.length; i += 8) {
+    var forecastInfo = data.list[i];
+
+    // retrieve and format date for local time of user
+    var dateText = forecastInfo.dt_txt + ' GMT';
+    var date = new Date(dateText);
+
+    var forecastDay = {
+      day: Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date),
+      description: forecastInfo.weather[0].description,
+      temperature: Math.round((forecastInfo.main.temp - 273.15) * 9/5 + 32),
+      descriptionImageUrl: 'https://openweathermap.org/img/wn/' + forecastInfo.weather[0].icon + '@2x.png'
+    }
+    
+    forecast.forecastDays.push(forecastDay);
+  }
+  
+  renderForecast();
+};
+
+var fetchForecast = function (queryCity) {
+  $.ajax({
+    method: "GET",
+    url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + queryCity + '&appid=ba4fcb43b4e6c7724e8c8d168e46fa49',
+    dataType: "json",
+    success: function(data) {
+      addForecast(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+};
+
+var renderForecast = function () {
+  $('.forecast').empty();
+
+  var source = $('#forecast-template').html();
+  var template = Handlebars.compile(source);
+  var newHTML = template(forecast);
+
+  $('.forecast').append(newHTML);
 };
