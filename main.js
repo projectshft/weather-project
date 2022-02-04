4707a25c3f05cdab6bf5a82848031195ffbe38cd
Ian Holmes <iqholmes@gmail.com>
2022-02-04 15:47:05 -0500

    current weather rendering to page

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
-var weather = {
-  icon: 'http://openweathermap.org/img/wn/10d@2x.png',
-  main: 'Clouds',
-  name: 'Apex',
-  temp: 292.09
-};
+var weather = {};
 var forecast = [
   {
     icon: 'http://openweathermap.org/img/wn/10d@2x.png',
@@ -40,8 +35,35 @@ var forecast = [
 $('.search').on('click', function () {
   var search = $('#search-query').val();
 
-  console.log(search);
+  fetch(search);
 });
+
+var showWeather = function(data) { 
+  var impTemp = Math.round((data.main.temp - 273.15) * 9/5 + 32);
+
+  weather = {
+    icon: "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png",
+    main: data.weather[0].main,
+    name: data.name,
+    temp: impTemp + "°"
+  };
+
+  renderWeather();
+}
+
+var fetch = function(query) {
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=6783e8fc2f1c1f45b18e23afa6f08304",
+    dataType: "json",
+    success: function(data) {
+      showWeather(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+}
 
 var renderWeather = function() {
   $('.weather').empty();
@@ -51,7 +73,6 @@ var renderWeather = function() {
   var newHTML = template(weather); 
   $('.weather').append(newHTML);
 }
-renderWeather();
 
 var renderForecast = function() {
   $('.forecast').empty();
@@ -63,4 +84,3 @@ var renderForecast = function() {
     $('.forecast-page').append(newHTML);
   }
 }
-renderForecast();
