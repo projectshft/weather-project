5314b65d7496cb20dbfe5876085763de1307a775
Ian Holmes <iqholmes@gmail.com>
2022-02-04 17:07:46 -0500

    forecast rendering

--- main.js
+++ main.js
@@ -1,36 +1,5 @@
 var weather = {};
-var forecast = [
-  {
-    icon: 'http://openweathermap.org/img/wn/10d@2x.png',
-    main: 'Clouds',
-    name: 'Apex',
-    temp: 292.09
-  },
-  {
-    icon: 'http://openweathermap.org/img/wn/10d@2x.png',
-    main: 'Clouds',
-    name: 'Apex',
-    temp: 292.09
-  },
-  {
-    icon: 'http://openweathermap.org/img/wn/10d@2x.png',
-    main: 'Clouds',
-    name: 'Apex',
-    temp: 292.09
-  },
-  {
-    icon: 'http://openweathermap.org/img/wn/10d@2x.png',
-    main: 'Clouds',
-    name: 'Apex',
-    temp: 292.09
-  },
-  {
-    icon: 'http://openweathermap.org/img/wn/10d@2x.png',
-    main: 'Clouds',
-    name: 'Apex',
-    temp: 292.09
-  }
-];
+var fiveDayForecast = [[],[],[],[],[]];
 
 $('.search').on('click', function () {
   var search = $('#search-query').val();
@@ -51,6 +20,37 @@ var showWeather = function(data) {
   renderWeather();
 }
 
+var showForecast = function(data) {
+  fiveDayForecast = [[],[],[],[],[]];
+  for (let i = 0; i < data.list.length; i++) {
+    var forecastData = data.list[i];
+    var impTemp = Math.round((forecastData.main.temp - 273.15) * 9/5 + 32);
+    var timestamp = forecastData.dt;
+    var date = new Date(timestamp * 1000);
+    var day = date.toLocaleString("en-US", {weekday: "long"})
+
+    var forecast = {
+      temp: impTemp + "°",
+      main: forecastData.weather[0].main,
+      icon: "https://openweathermap.org/img/wn/" + forecastData.weather[0].icon + "@2x.png",
+      day: day
+    }
+
+    if (i == 8) {
+      fiveDayForecast[0].push(forecast);
+    } else if (i == 16) {
+      fiveDayForecast[1].push(forecast)
+    } else if (i == 24) {
+      fiveDayForecast[2].push(forecast)
+    } else if (i == 32) {
+      fiveDayForecast[3].push(forecast)
+    } else if (i == 39) {
+      fiveDayForecast[4].push(forecast)
+    }
+  }
+  renderForecast();
+}
+
 var fetch = function(query) {
   $.ajax({
     method: "GET",
@@ -58,6 +58,17 @@ var fetch = function(query) {
     dataType: "json",
     success: function(data) {
       showWeather(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + query + "&appid=6783e8fc2f1c1f45b18e23afa6f08304",
+    dataType: "json",
+    success: function(data) {
+      showForecast(data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
@@ -77,10 +88,10 @@ var renderWeather = function() {
 var renderForecast = function() {
   $('.forecast').empty();
 
-  for (let i = 0; i < forecast.length; i++) {
+  for (let i = 0; i < fiveDayForecast.length; i++) {
     var source = $('#forecast-template').html();
     var template = Handlebars.compile(source);
-    var newHTML = template(forecast[i]); 
+    var newHTML = template(fiveDayForecast[i][0]); 
     $('.forecast-page').append(newHTML);
   }
 }
