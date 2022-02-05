36b8d91ae045073d837d1300bfbc25dc5d662e8d
Samuel Zimel <samuel.c.zimel@gmail.com>
2022-02-05 16:33:41 -0600

    working but not perfect

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
 var weather = [
-  {}
+  {
+  }
 ];
+var weather2 = [
+  {
+  } 
+];
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 var renderWeather = function() {
+  //weather
   $('.weather').empty();
-
   for (var i = 0; i < weather.length; i++) {
-    var source = $('#weather-template').html();
+    var source = $('#first-weather-template').html();
     var template = Handlebars.compile(source);
     var newHtml = template(weather[i]);
-
     $('.weather').append(newHtml);
+  }
+  //weather2
+  $('.weather2').empty();
+  for (var i = 0; i < weather2.length; i++) {
+    var source = $('#second-weather-template').html();
+    var template = Handlebars.compile(source);
+    var newHtml = template(weather2[i]);
+    $('.weather2').append(newHtml);
   }
 };
 renderWeather();
@@ -22,37 +35,66 @@ $('button').on('click', function() {
 });
 
 var fetch = function(search) {
+  //weather1
   $.ajax({
     method: "GET",
     url: "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=959d275eae1f10794b2be3475af21fdf",
     dataType: "json",
     success: function(data) {
       weatherInfo(data);
-      console.log(data);
+      // console.log(data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
       alert(search + " is not a valid city name.")
     }
   })
+  //weather2
+  $.ajax({
+    method: "GET",
+    url: "https://api.openweathermap.org/data/2.5/forecast?q=" + search + "&appid=959d275eae1f10794b2be3475af21fdf",
+    dataType: "json",
+    success: function(data) {
+      weather2Info(data);
+      // console.log(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+      // alert(search + " is not a valid city name.")
+    }
+  })
+};
+
+var weather2Info = function(data) {
+  weather2 = [];
+  for(i = 0; i < data.list.length; i += 8){
+    var info = data.list[i];
+    //getting temp
+    var kelvin = (info.main.temp || null);
+    var F = toF(kelvin);
+    //getting date
+    var whatDay = (info.dt_txt || null);
+    var day = getDay(whatDay);
+    info2 = {
+      condition: info.weather[0].main || null,
+      temp: F,
+      icon: info.weather[0].icon || null,
+      day: day
+    }
+    weather2.push(info2);
+  }
+  renderWeather();
 }
 
 var weatherInfo = function(data) {
   weather = [];
   //temp
   var kelvin = (data.main.temp || null);
-  console.log(kelvin)
   var F = toF(kelvin);
-  console.log(F);
-  //condition
-  var cond = (data.weather[0].id || null);
-  console.log(cond);
-  var C = toC(cond);
-  console.log(C);
   var info2 = {
     temp: F,
     city: data.name || null,
-    condition: C,
+    condition: data.weather[0].main || null,
     icon: data.weather[0].icon || null
   }
   weather.push(info2);
@@ -65,51 +107,10 @@ var toF = function(kelvin) {
   return F; 
 };
 
-//numbers to matching condition
-var toC = function(cond) {
-  var substitue = cond.toString();
-  if(substitue[0] == 2) {
-    alert('working');
-    return 'Thunderstorms';
-  } 
-  if(substitue[0] == 3) {
-    return 'Drizzle';
-  }
-  if(substitue[0] == 5) {
-    return 'Rain';
-  }
-  if(substitue[0] == 6) {
-    return 'Snow';
-  }
-  if(substitue[0] == 7) {
-    return 'Atmosphere';
-  }
-  if(substitue[0] == 8 && substitue[2] == 0) {
-    return 'Clear'
-  }
-  if(substitue[0] == 8 && substitue[2] !== 0) {
-    return 'Cloudy'
-  } else {
-    alert('Weather condition not read.')
-  }
-};
+//give correct day
+var getDay = function(e) {
+  e = new Date(e);
+  var date = e.getDay();
+  return weekday[date];
+}
 
-
-
-//(K − 273.15) × 9/5 + 32.
-
-
-// condition is data.weather.id --> 3 digit code
-// temp is data.main.temp --> kelvin
-// city is data.name --> all good
-// icon is data.weather.icon  --> all good
-
-
-
-
-
-
-
-// Information: api.openweathermap.org/data/2.5/weather?q={city name}&appid=959d275eae1f10794b2be3475af21fdf
-
-// icon: http://openweathermap.org/img/wn/{icon}@2x.png
