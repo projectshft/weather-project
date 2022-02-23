a6014c1b1b044ab1a266c7879724c5a1d9938e5b
Emily Freeman <freeman.ecf@gmail.com>
2022-02-23 09:26:36 -0500

    part 2 functioning

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
 var currentWeather = [];
 var currentWeatherIcon = [];
+var currentForecast = [];
 
 $('.search').on('click', function () {
   var search = $('#search-query').val();
 
-  fetch(search);
+  fetchLatandLong(search);
 });
 
-var fetch = function (query) {
+var fetchLatandLong = function (query) {
   $.ajax({
     method: "GET",
     url: "http://api.openweathermap.org/geo/1.0/direct?q=" + query + "&limit=" + 1 + "&appid=" + "e4f6ea6ff60bd89789f84c07b1f17a89",
     dataType: "json",
     success: function(data) {
       getCurrentWeather(data);
+      getForecast(data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
@@ -35,29 +37,23 @@ var getCurrentWeather = function (data) {
       console.log(textStatus);
     }
   });
-}
+};
 
 var createCurrentWeather = function (data) {
   currentWeather = [];
   currentWeatherIcon = [];
-  console.log(data);
   var iconPic = data.weather[0].icon;
   var weatherObject = {
-    temp: Math.floor((1.8 * (data.main.temp - 273) + 32)) + " degrees",
+    temp: Math.floor((1.8 * (data.main.temp - 273) + 32)),
     city: data.name,
     weather: data.weather[0].main,
+    icon: "http://openweathermap.org/img/wn/" + iconPic + "@2x.png",
   };
 
-  var weatherIconObject = {
-    icon: "http://openweathermap.org/img/wn/" + iconPic + "@2x.png",
-  }
- 
   currentWeather.push(weatherObject);
-  currentWeatherIcon.push(weatherIconObject);
 
   renderCurrentWeather();
-  renderCurrentWeatherIcon();
-}
+};
 
 
 var renderCurrentWeather = function () {
@@ -69,15 +65,71 @@ var renderCurrentWeather = function () {
     var newHTML = template(currentWeather[i]);
     $(".current-weather").append(newHTML);
   }
+};
+
+var getForecast = function (data) {
+  var latitude = data[0].lat;
+  var longitude = data[0].lon;
+  $.ajax({
+    method: "GET",
+    url: "http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + "e4f6ea6ff60bd89789f84c07b1f17a89",
+    dataType: "json",
+    success: function (data) {
+      createForecast(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+};
+
+var createForecast = function (data) {
+  currentForecast = [];
+  
+  for (let i = 7; i < data.list.length; i += 8) {
+    console.log(data.list[i]);
+
+    var forecastObject = {
+      weather: data.list[i].weather[0].main,
+      temp: Math.floor((1.8 * (data.list[i].main.temp - 273) + 32)),
+      icon: "http://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png",
+      date: function () {
+        var dateData = data.list[i].dt_txt;
+        var newDate = new Date(dateData);
+        var day = newDate.getDay();
+
+        switch (day) {
+          case 0:
+            return "Sunday";
+          case 1:
+            return "Monday";
+          case 2:
+            return "Tuesday";
+          case 3:
+            return "Wednesday";
+          case 4:
+            return "Thursday";
+          case 5:
+            return "Friday";
+          case 6: 
+            return "Saturday";
+        }
+      }
+    }
+
+    currentForecast.push(forecastObject)
+  }
+
+  renderForecast();
 }
 
-var renderCurrentWeatherIcon = function () {
-  $(".current-weather-icon").empty();
+var renderForecast = function () {
+  $('.forecast').empty();
 
-  for (let i = 0; i < currentWeatherIcon.length; i++) {
-    var source = $("#today-icon-template").html();
+  for (let i = 0; i < currentForecast.length; i++) {
+    var source = $("#forecast-template").html();
     var template = Handlebars.compile(source);
-    var newHTML = template(currentWeatherIcon[i]);
-    $(".current-weather-icon").append(newHTML);
+    var newHTML = template(currentForecast[i]);
+    $(".forecast").append(newHTML);
   }
 }
