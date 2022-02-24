fa1a67b3cf025915de067c472f4b1722e05476ff
HK Nelson <hknelson8@gmail.com>
2022-02-24 12:38:36 -0500

    add search on enter keypress and refactor

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-/* eslint-disable no-else-return */
-/* eslint-disable no-var */
-/* eslint-disable vars-on-top */
-/* eslint-disable prefer-arrow-callback */
-/* eslint-disable no-plusplus */
-/* eslint-disable object-shorthand */
-
-// TODO need avg mains for forecast (sort the highest count element?)
-// TODO need avg icons for forecast
-
 var weathers = [];
 
 var apiKey = 'be75809a756d3f3e627fb334d09f44b4';
@@ -24,11 +14,11 @@ var forecastDay = function (num) {
   ];
 
   var date = new Date();
-  var futureDay = date.getDay() + num;
+  var futureDay = date.getDay() + 1 + num;
 
-  if (days.indexOf(days[date.getDay() + num]) !== -1) {
+  if (days.indexOf(days[futureDay]) !== -1) {
     return days[futureDay];
-  } else if (days.indexOf(days[date.getDay() + num]) === -1) {
+  } else if (days.indexOf(days[futureDay]) === -1) {
     return days[futureDay - days.length];
   }
 };
@@ -40,7 +30,7 @@ var renderWeathers = function () {
     Handlebars.compile($('#weather-template').html())(weathers[0])
   );
 
-  $('.search-form')[0].reset();
+  $('#search-query').val('');
 };
 
 var addCurrent = function (data) {
@@ -48,11 +38,10 @@ var addCurrent = function (data) {
 
   var weather = {
     city: data.name,
-    tempCurrent: Math.floor(data.main.temp),
+    tempCurrent: Math.floor(data.main.temp) + '°',
     mainCurrent: data.weather[0].main,
     iconCurrent: data.weather[0].icon,
   };
-
   weathers.push(weather);
 
   renderWeathers();
@@ -60,33 +49,25 @@ var addCurrent = function (data) {
 
 var addForecast = function (data) {
   var allTemps = [];
-  var allMains = [];
-  var allIcons = [];
-
   var dailyTemps = [];
-  var dailyMains = [];
-  var dailyIcons = [];
-
   var avgTemps = [];
-  var avgMains = [];
-  var avgIcons = [];
+  var midMains = [];
+  var midIcons = [];
 
   for (var i = 0; i < data.list.length; i++) {
     allTemps.push(data.list[i].main.temp);
-    allMains.push(data.list[i].weather[0].main);
-    allIcons.push(data.list[i].weather[0].icon);
   }
 
   for (var j = 0; j < allTemps.length; j++) {
     dailyTemps.push(allTemps.splice(0, 8));
   }
 
-  for (var k = 0; k < allMains.length; k++) {
-    dailyMains.push(allMains.splice(0, 8));
+  for (var k = 0; k < data.list.length; k += 8) {
+    midMains.push(data.list[k].weather[0].main);
   }
 
-  for (var l = 0; l < allIcons.length; l++) {
-    dailyIcons.push(allIcons.splice(0, 8));
+  for (var l = 0; l < data.list.length; l += 8) {
+    midIcons.push(data.list[l].weather[0].icon);
   }
 
   for (var m = 0; m < dailyTemps.length; m++) {
@@ -99,58 +80,65 @@ var addForecast = function (data) {
   }
 
   var weather = {
-    dayForecast1: forecastDay(1),
-    tempForecast1: avgTemps[0],
-    mainForecast1: 'Rain',
-    iconForecast1: '10d',
+    dayForecast1: forecastDay(0),
+    tempForecast1: avgTemps[0]  + '°',
+    mainForecast1: midMains[0],
+    iconForecast1: midIcons[0],
 
-    dayForecast2: forecastDay(2),
-    tempForecast2: avgTemps[1],
-    mainForecast2: 'Rain',
-    iconForecast2: '10n',
+    dayForecast2: forecastDay(1),
+    tempForecast2: avgTemps[1]  + '°',
+    mainForecast2: midMains[1],
+    iconForecast2: midIcons[1],
 
-    dayForecast3: forecastDay(3),
-    tempForecast3: avgTemps[2],
-    mainForecast3: 'Rain',
-    iconForecast3: '10n',
+    dayForecast3: forecastDay(2),
+    tempForecast3: avgTemps[2]  + '°',
+    mainForecast3: midMains[2],
+    iconForecast3: midIcons[2],
 
-    dayForecast4: forecastDay(4),
-    tempForecast4: avgTemps[3],
-    mainForecast4: 'Rain',
-    iconForecast4: '10n',
+    dayForecast4: forecastDay(3),
+    tempForecast4: avgTemps[3]  + '°',
+    mainForecast4: midMains[3],
+    iconForecast4: midIcons[3],
 
-    dayForecast5: forecastDay(5),
-    tempForecast5: avgTemps[4],
-    mainForecast5: 'Rain',
-    iconForecast5: '10n',
+    dayForecast5: forecastDay(4),
+    tempForecast5: avgTemps[4]  + '°',
+    mainForecast5: midMains[4],
+    iconForecast5: midIcons[4],
   };
 
   Object.assign(weathers[0], weather);
 
   renderWeathers();
+
+  allTemps = [];
+  dailyTemps = [];
+  avgTemps = [];
+  midMains = [];
+  midIcons = [];
 };
 
 var fetch = function (query) {
   $.ajax({
     method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/weather?q=' + query 
-      + ',&units=imperial&appid=' + apiKey,
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${query},&units=imperial&appid=${apiKey}`,
     dataType: 'json',
     success: function (data) {
       addCurrent(data);
     },
     error: function (jqXHR, textStatus, errorThrown) {
+      alert('Please enter a valid or different city.');
+      $('#search-query').val('');
       console.log(textStatus);
     },
   });
 
   $.ajax({
     method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + query 
-      + ',&units=imperial&appid=' + apiKey,
+    url: `https://api.openweathermap.org/data/2.5/forecast?q=${query},&units=imperial&appid=${apiKey}`,
     dataType: 'json',
     success: function (data) {
       addForecast(data);
+      $('.weather-forecast .weather-text').css('border', '2px solid #000');
     },
     error: function (jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
@@ -161,6 +149,14 @@ var fetch = function (query) {
 $('.search').on('click', function () {
   var searchValue = $('#search-query').val();
   fetch(searchValue);
+});
+
+$('#search-query').on('keypress', function (event) {
+  if (event.keyCode === 13) {
+    var searchValue = $('#search-query').val();
+    fetch(searchValue);
+    return false;
+  }
 });
 
 $('.clear').on('click', function () {
