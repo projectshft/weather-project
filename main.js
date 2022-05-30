c293c6566c7621fd79408f2ca9e10bc6f075fb97
arcox80 <drew.r.cox@gmail.com>
2022-05-30 01:02:47 -0400

    Direct geocoding url, created searchValueTrimmer, added label in html

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-const fetch = $.ajax({
-  method: 'GET',
-  url: `https://api.openweathermap.org/data/2.5/forecast?id=524901&appid=4da99895dae25ae39743d1996fb14942`,
-  dataType: 'json',
-  success(data) {
-    console.log(data);
-  },
-  error(jqXHR, textStatus, errorThrown) {
-    console.log(textStatus);
-  },
-});
+const cityData = {};
+
+const fetch = function (query) {
+  $.ajax({
+    method: 'GET',
+    url: `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=1&appid=4da99895dae25ae39743d1996fb14942&units=imperial`,
+    dataType: 'json',
+    success(data) {
+      console.log(data);
+    },
+    error(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    },
+  });
+};
+
+const searchValueTrimmer = (query) => {
+  const queryArr = query.split(',');
+  const queryTrimmedArr = queryArr.map((index) => index.trim());
+  const finalQuery = queryTrimmedArr.join(',');
+  return finalQuery;
+};
 
 $('.search').on('click', () => {
   const search = $('#search-query').val();
-
-  fetch(search);
+  const searchTrimmed = searchValueTrimmer(search);
+  console.log(searchTrimmed);
+  fetch(searchTrimmed);
 
   $('#search-query').val('');
 });
@@ -23,8 +35,9 @@ $('input').on('keypress', (e) => {
   if (keycode === 13) {
     e.preventDefault();
     const search = $('#search-query').val();
-
-    fetch(search);
+    const searchTrimmed = searchValueTrimmer(search);
+    console.log(searchTrimmed);
+    fetch(searchTrimmed);
 
     $('#search-query').val('');
   }
