a796722c43bbb93c170e24d77716db622bf2d6ac
Dillon Hess <dhess268@gmail.com>
2022-08-09 17:52:26 -0400

    styling changes and edge cases

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
 const key = KEY;
 
-const days = {
-  0: 'Sunday',
-  1: 'Monday',
-  2: 'Tuesday',
-  3: 'Wednesday',
-  4: 'Thursday',
-  5: 'Friday',
-  6: 'Saturday',
-};
 // const day = moment.unix();
 
 function renderCurrentWeather(weatherData) {
+  // empty old weather data to overwrite it with new data
   $('.current-weather').empty();
   const { temp, city, condition, icon } = weatherData;
   const source = $('#current-weather-template').html();
@@ -25,12 +17,23 @@ function renderCurrentWeather(weatherData) {
   $('.current-weather').append(newHTML);
 }
 function renderForecast(forecastData) {
+  const days = {
+    0: 'Sunday',
+    1: 'Monday',
+    2: 'Tuesday',
+    3: 'Wednesday',
+    4: 'Thursday',
+    5: 'Friday',
+    6: 'Saturday',
+  };
+  // empty old forecast data to overwrite it with new data
   $('.forecast').empty();
   forecastData.forEach((elem) => {
     const { temp, unixDateTime, condition, icon } = elem;
+    // convert unix date/time code to day of the week string
     const dateObj = moment.unix(unixDateTime);
     const day = days[dateObj.day()];
-    console.log(dateObj.day());
+    // append
     const source = $('#forecast-template').html();
     const template = Handlebars.compile(source);
     const newHTML = template({
@@ -39,7 +42,10 @@ function renderForecast(forecastData) {
       condition,
       icon,
     });
+
     $('.forecast').append(newHTML);
+    // For styling purposes
+    $('.forecast :first-child').addClass('offset-md-1');
   });
 }
 const fetch = function (query) {
@@ -59,36 +65,35 @@ const fetch = function (query) {
 async function getFiveDayForecase({ lat, lon }) {
   const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
   const result = await fetch(url);
-  const increments = result.list || null;
-  const temps = [];
-  for (let i = 7; i < result.list.length; i += 8) {
-    temps.push({
-      temp: result.list[i].main.temp,
-      condition: result.list[i].weather[0].description,
-      unixDateTime: result.list[i].dt || null,
-      icon: result.list[i].weather[0].icon,
-    });
-  }
+  const hasListProperty = Object.prototype.hasOwnProperty.call(result, 'list');
 
-  renderForecast(temps);
+  if (hasListProperty) {
+    const temps = [];
+    for (let i = 7; i < result.list.length; i += 8) {
+      temps.push({
+        temp: result.list[i].main.temp || null,
+        condition: result.list[i].weather
+          ? result.list[i].weather[0].description
+          : null,
+        unixDateTime: result.list[i].dt || null,
+        icon: result.list[i].weather ? result.list[i].weather[0].icon : null,
+      });
+    }
+    renderForecast(temps);
+  }
 }
 
 async function getCurrentWeatherData({ lat, lon }) {
   const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&units=imperial`;
   const result = await fetch(url);
-  // console.log(result);
-  // we need:
-  // current temp
-  // condition
-  // icon
-  // name of city
-  const obj = {
+
+  const currentWeatherData = {
     temp: result.main.temp ? Math.round(result.main.temp) : null,
     condition: result.weather ? result.weather[0].main : null,
     icon: result.weather ? result.weather[0].icon : null,
     city: result.name || null,
   };
-  renderCurrentWeather(obj);
+  renderCurrentWeather(currentWeatherData);
 }
 
 async function getCoordinates() {
@@ -100,31 +105,10 @@ async function getCoordinates() {
     lat: result[0].lat || null,
     lon: result[0].lon || null,
   };
-  // console.table(coords);
-  getCurrentWeatherData(coords);
-  getFiveDayForecase(coords);
+  if (coords.lat && coords.lon) {
+    getCurrentWeatherData(coords);
+    getFiveDayForecase(coords);
+  }
 }
 
-//
 $('#search').click(getCoordinates);
-
-// =========================
-// TESTING CODE
-// =========================
-// const lat = 41.2695965;
-// const lon = -73.77527040089426;
-
-// fetch({
-//   latitude: lat,
-//   longitude: lon,
-// });
-
-// async function asyncCall() {
-//   console.log('calling');
-//   const result = await fetch(
-//     'http://api.openweathermap.org/geo/1.0/direct?q=yorktown heights&limit=1&appid=abf3979019dc7f6e55f328b41e30487e'
-//   );
-//   console.log(result);
-//   // expected output: "resolved"
-// }
-// asyncCall();
