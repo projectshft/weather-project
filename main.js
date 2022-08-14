cf9606ae0482246c9ad088656ac22704f75f471b
Ryan <ryancaimano@gmail.com>
2022-08-14 17:38:50 -0500

    fixed function to empty html contents for new city search. Renamed variables for readability. Fixed issue with pushing data to array by using the for-each method w/handlebars. Used a for-loop to push data for each day of 5-day forecast. Cleaned up messy sting concatenization by using string interpolation. Everything written is functional in console. Next up is converting dates to days of the week...fix html/css to get background back to the way it was.

--- main.js
+++ main.js
@@ -1,97 +1,32 @@
-var days = [];
+var apiKey = '9de0841aea702821eece6900aab8d8f1';
+var baseUrl = 'https://api.openweathermap.org/data/2.5';
 
 $('.search').on('click', function () {
-  var day = $('#search-query').val();
-  var forecast = $('#search-query').val();
-  $('#search-query').val('');
-
-  fetchDay(day);
-  fetchForecast(day);
+  let city = $('#search-query').val();
+  fetchAndRenderDay(city);
+  fetchAndRenderForecast(city);
 });
 
-var getDay = function (data) {
-  var icon = 'http://openweathermap.org/img/wn/';
-  days.push({
-    city: data.name,
-    temperature: Math.round(data.main.temp),
-    condition: data.weather[0].description,
-    icon: icon + data.weather[0].icon + '.png',
-  });
-
-  renderDay();
-};
-
-var fetchDay = function (day) {
-  var apiKey = '&appid=' + '9de0841aea702821eece6900aab8d8f1&units=imperial';
+var fetchAndRenderDay = function (city) {
   $.ajax({
     method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/weather?q=' + day + apiKey,
+    url: `${baseUrl}/weather?units=imperial&appid=${apiKey}&q=${city}`,
     dataType: 'json',
     success: function (data) {
-      getDay(data);
-    },
+      let day = {
+        city: data.name,
+        temperature: Math.round(data.main.temp),
+        condition: data.weather[0].description,
+        icon: `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`,
+      };
 
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.log(textStatus);
-    },
-  });
-};
+      console.log(day);
 
-var renderDay = function () {
-  $('.days').empty();
-  //next line is unnecessary but I was encouraged to leave it since nothing is broken
-  for (let i = 0; i < days.length; i++) {
-    const day = days[i];
-
-    var source = $('#day-template').html();
-    var template = Handlebars.compile(source);
-    var newHTML = template(day);
-
-    $('.days').append(newHTML);
-  }
-};
-
-renderDay();
-
-//the code below works for a single card (day) of the 5-day forecast
-var forecasts = [];
-
-var getForecast = function (fiveData) {
-  var icon = 'http://openweathermap.org/img/wn/';
-  forecasts.push({
-    day1Temp: fiveData.list[0 + 1].main.temp,
-    day1Condition: fiveData.list[0 + 8 + 1].weather[0].description,
-    day1icon: icon + fiveData.list[0].weather[0].icon + '.png',
-    dayOfWeek1: fiveData.list[0 + 8].dt_txt,
-    day2Temp: fiveData.list[1 + 8].main.temp,
-    day2Condition: fiveData.list[9 + 8].weather[0].description,
-    day2icon: icon + fiveData.list[1].weather[0].icon + '.png',
-    dayOfWeek2: fiveData.list[8 + 8].dt_txt,
-    day3Temp: fiveData.list[9 + 8].main.temp,
-    day3Condition: fiveData.list[17 + 8].weather[0].description,
-    day3icon: icon + fiveData.list[2].weather[0].icon + '.png',
-    dayOfWeek3: fiveData.list[16 + 8].dt_txt,
-    day4Temp: fiveData.list[17 + 8].main.temp,
-    day4Condition: fiveData.list[25 + 8].weather[0].description,
-    day4icon: icon + fiveData.list[3].weather[0].icon + '.png',
-    dayOfWeek4: fiveData.list[24 + 8].dt_txt,
-    day5Temp: fiveData.list[25 + 8].main.temp,
-    day5Condition: fiveData.list[33 + 5].weather[0].description,
-    day5icon: icon + fiveData.list[4].weather[0].icon + '.png',
-    dayOfWeek5: fiveData.list[32 + 5].dt_txt,
-  });
-
-  renderForecast();
-};
-
-var fetchForecast = function (day) {
-  var apiKey = '&appid=' + '9de0841aea702821eece6900aab8d8f1&units=imperial';
-  $.ajax({
-    method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + day + apiKey,
-    dataType: 'json',
-    success: function (fiveData) {
-      getForecast(fiveData);
+      $('.day').empty();
+      let source = $('#day-template').html();
+      let template = Handlebars.compile(source);
+      let html = template(day);
+      $('.day').append(html);
     },
     error: function (jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
@@ -99,32 +34,28 @@ var fetchForecast = function (day) {
   });
 };
 
-var renderForecast = function () {
-  $('.forecasts').empty();
-
-  for (let i = 0; i < forecasts.length; i++) {
-    const forecast = forecasts[i];
-
-    var source1 = $('#forecast-template').html();
-    var template1 = Handlebars.compile(source1);
-    var newHTML1 = template1(forecast);
-
-    $('.forecasts').append(newHTML1);
-  }
-};
-
-renderForecast();
-
-var fetchForecast = function (day) {
-  var apiKey = '&appid=' + '9de0841aea702821eece6900aab8d8f1&units=imperial';
+var fetchAndRenderForecast = function (city) {
   $.ajax({
     method: 'GET',
-    url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + day + apiKey,
+    url: `${baseUrl}/forecast?units=imperial&appid=${apiKey}&q=${city}`,
     dataType: 'json',
-    success: function (fiveData) {
-      getForecast(fiveData);
-    },
+    success: function (data) {
+      items = [];
+      for (let i = 7; i <= 40; i += 8) {
+        items.push({
+          day: data.list[i].dt_txt,
+          temperature: Math.round(data.list[i].main.temp),
+          condition: data.list[i].weather[0].description,
+          icon: `http://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png`,
+        });
+      }
 
+      $('.forecast').empty();
+      let source = $('#forecast-template').html();
+      let template = Handlebars.compile(source);
+      let html = template({ forecast: items });
+      $('.forecast').append(html);
+    },
     error: function (jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
     },
