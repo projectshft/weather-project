bacbb7c87c424a97fdfce4bc8a4fba8b8f234516
ryrob <ryanrobertson242@gmail.com>
2022-09-11 14:56:15 -0500

    Refactor getWeatherData function

--- main.js
+++ main.js
@@ -1,52 +1,7 @@
-
-let day = [
-    {
-      day: 'Monday',
-      high: 90,
-      low: 75,
-      icon: 'images/sun.png',
-      condition: 'sunny'
-    },
-    {
-      day: 'Tuesday',
-      high: 100,
-      low: 85,
-      icon: 'images/sun.png',
-      condition: 'sunny'
-    },
-    {
-      day: 'Wednesday',
-      high: 89,
-      low: 73,
-      icon: 'images/sun.png',
-      condition: 'rain'
-    },
-    {
-      day: 'Thursday',
-      high: 98,
-      low: 76,
-      icon: 'images/sun.png',
-      condition: 'sunny'
-    },
-    {
-      day: 'Friday',
-      high: 92,
-      low: 67,
-      icon: 'images/sun.png',
-      condition: 'cloudy'
-    }
-  ]
-
-let fiveDayArray = []
-
-
 $('document').ready(function() {
 
-  //ANCHOR Get current weather on form submit
-  $('#user-location-btn').on('click', function(e) {
-    e.preventDefault();
- 
-
+  //ANCHOR Function to call Weather API to get Current and Five Day weather
+  const getWeatherData = function(){
     const cityValue = $('#user-location').val();
     const stateValue = $('#user-state').val()
 
@@ -65,6 +20,7 @@ $('document').ready(function() {
           fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${keys.weatherKey}&units=imperial`)
         ])
 
+        //convert the responses to JSON
         const weatherData = await Promise.all(weatherResponse.map(weather => weather.json()))
 
         //function call to display weather on page
@@ -74,64 +30,65 @@ $('document').ready(function() {
         console.log(error)
       }
     }
-
-    //ANCHOR Populate page with current weather HTML
-    function showCurrentWeather(data) {
-      $('#current-weather-container').empty()
-      const current = data[0];
-
-      const currentWeatherHTML = $('#current-weather-template').html();
-      const currentWeatherFunction = Handlebars.compile(currentWeatherHTML)
-      const currentWeatherTemplate = currentWeatherFunction(
-        {
-          city: current.name || null,
-          conditionURL: `http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png` || null,
-          temperature: Math.round(current.main.temp) || null,
-          condition: current.weather[0].main
-        }
-      )
-      $(currentWeatherTemplate).appendTo('#current-weather-container')
-
-    }
-
-    //ANCHOR Show Five Day Weather function
-    function showFiveDayWeather(data) {
-      fiveDayArray.length = 0; //reset array each API call
-      $('#five-day-weather-container').empty()
-      const fiveDay = data[1].list
-
-      let filteredWeatherArr = fiveDay.filter((day, index) => {
-        if(index === 0 || index === 7 || index === 15 || index === 23 || index === 31) {
-          return day
-        }
-      })
-
-      let weatherArrData = filteredWeatherArr.map(day => {
-        return {
-          'day': convertUTX(day.dt),
-          'high': Math.round(day.main.temp),
-          'low':  Math.round(day.main.temp),
-          'iconURL': `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`,
-          'condition': day.weather[0].main
-        }
-      })
-
-      console.log(weatherArrData)
-
-
-      const fiveDayWeatherHTML = $('#five-day-weather-template').html();
-      const fiveDayWeatherFunction = Handlebars.compile(fiveDayWeatherHTML);
-      const fiveDayWeatherTemplate = fiveDayWeatherFunction({weatherArrData})
-
-      console.log(fiveDayWeatherTemplate)
-
-      $(fiveDayWeatherTemplate).appendTo('#five-day-weather-container')
-    }
-      
-
     getLocationWeather();
-    $('#search-location-form').trigger('reset');
+  }
+
+  //ANCHOR Get current weather on form submit
+  $('#user-location-btn').on('click', function(e) {
+    e.preventDefault();
+    $('#current-weather-container').empty()
+    $('#five-day-weather-container').empty()
+    getWeatherData(e)
+    // $('#search-location-form').trigger('reset');
   })
+
+  //ANCHOR Handlebars to display current weather on page
+  function showCurrentWeather(data) {
+    const current = data[0];
+
+    const currentWeatherHTML = $('#current-weather-template').html();
+    const currentWeatherFunction = Handlebars.compile(currentWeatherHTML)
+    const currentWeatherTemplate = currentWeatherFunction(
+      {
+        city: current.name || null,
+        conditionURL: `http://openweathermap.org/img/wn/${current.weather[0].icon}@2x.png` || null,
+        temperature: Math.round(current.main.temp) || null,
+        condition: current.weather[0].main
+      }
+    )
+    $(currentWeatherTemplate).appendTo('#current-weather-container')
+  }
+
+
+  //ANCHOR Handlebars to display five day forecast on page
+  function showFiveDayWeather(data) {
+    const fiveDay = data[1].list
+
+    /*  API returns data for every 3 hours, 40 times.  
+    Filtering out every 8 which equates to every 24 hours, showing as 5 days */
+    let filteredWeatherArr = fiveDay.filter((day, index) => {
+      if(index === 0 || index === 7 || index === 15 || index === 23 || index === 31) {
+        return day
+      }
+    })
+
+    //create array with relevant data for Handlebars
+    let weatherArrData = filteredWeatherArr.map(day => {
+      return {
+        'day': convertUTX(day.dt),
+        'temp': Math.round(day.main.temp),
+        'iconURL': `http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`,
+        'condition': day.weather[0].main
+      }
+    })
+
+    const fiveDayWeatherHTML = $('#five-day-weather-template').html();
+    const fiveDayWeatherFunction = Handlebars.compile(fiveDayWeatherHTML);
+    const fiveDayWeatherTemplate = fiveDayWeatherFunction({weatherArrData})
+
+    $(fiveDayWeatherTemplate).appendTo('#five-day-weather-container')
+  }
+
 
   //ANCHOR button to find users current location
   $('#use-current-location-btn').on('click', function(e) {
