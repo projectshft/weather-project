190172be4e20f8559cadc71ab034419a4c06f069
unknown <dgoodenough@proton.me>
2022-11-07 08:47:26 -0800

    Add error handling to fetch5Day() and fetchCurrent()

--- main.js
+++ main.js
@@ -1,55 +1,63 @@
-let apiKey = "504b76049c23a98f2e402766465dad0d"
+let apiKey = "504b76049c23a98f2e402766465dad0d";
 
 function renderCurrentWeather(data) {
-  $('.current').empty();
+  $(".current").empty();
 
-  let source = $('#current-template').html();
+  let source = $("#current-template").html();
   let template = Handlebars.compile(source);
-  let html = template (data);
+  let html = template(data);
 
-  $('.current').append(html);
+  $(".current").append(html);
 }
 
+function render5DayWeather(data) {
+  $(".five-day").empty();
 
-function render5DayWeather (data) {
-  $('.five-day').empty();
-
-  let source = $('#five-day-template').html();
+  let source = $("#five-day-template").html();
   let template = Handlebars.compile(source);
 
-  data.forEach(day => {
-    let html = template (day);
+  data.forEach((day) => {
+    let html = template(day);
 
-    $('.five-day').append(html);
+    $(".five-day").append(html);
   });
 }
 
 function renderError() {
-  $('.current').empty();
-  $('.five-day').empty();
+  $(".current").empty();
+  $(".five-day").empty();
 
-  $('.current').append('<p class="error">There was an error, please try another search query</p>')
+  $(".current").append(
+    '<p class="error">There was an error, please try another search query.</p>'
+  );
 }
 
-
-function changeIcon(weatherData) {//the '03d' and '03n' icons are blank white
-  if(weatherData.list) {
+function changeIcon(weatherData) {
+  //the '03d' and '03n' icons are blank white
+  if (weatherData.list) {
     weatherData.list.forEach((data) => {
-      if(data.weather[0].icon.includes('03')){
-        data.weather[0].icon = '04d'
+      if (data.weather[0].icon.includes("03")) {
+        data.weather[0].icon = "04d";
       }
-    })
+    });
   } else {
-    if(weatherData.weather[0].icon.includes('03')){
-      weatherData.weather[0].icon = '04d'
+    if (weatherData.weather[0].icon.includes("03")) {
+      weatherData.weather[0].icon = "04d";
     }
   }
   return weatherData;
 }
 
-
-function getDay (unixTime) {
-  let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+function getDay(unixTime) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   let weekDayNum = new Date(unixTime * 1000).getDay();
 
@@ -58,99 +66,121 @@ function getDay (unixTime) {
   return day;
 }
 
+function fetchCurrent(lat, lon) {
+  $.get(
+    "https://api.openweathermap.org/data/2.5/weather?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&APPID=" +
+      apiKey +
+      "&units=imperial",
+    function (data) {
+      // data = changeIcon(data);
+      let compressedData = {};
+      if (data.weather[0]) {
+        compressedData = {
+          condition: data.weather[0].main,
+          temp: Math.round(data.main.temp),
+          iconUrl:
+            "http://openweathermap.org/img/wn/" +
+            data.weather[0].icon +
+            "@2x.png",
+          location: data.name,
+        };
 
-function fetchCurrent (lat, lon) {
-  $.get("https://api.openweathermap.org/data/2.5/weather?lat=" + lat + 
-        "&lon=" + lon + 
-        "&APPID=" + 
-        apiKey + 
-        "&units=imperial", 
-        function(data) { // render error if data empty/false
-          data = changeIcon(data);
-          let compressedData = {condition: data.weather[0].main, temp: Math.round(data.main.temp), iconUrl: 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png', location: data.name}
-
-          renderCurrentWeather(compressedData);
-        })
+        renderCurrentWeather(compressedData);
+      } else {
+        renderError();
+      }
+    }
+  );
 }
 
-function fetch5Day (lat, lon) {
-  $.get("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + 
-        "&lon=" + lon + 
-        "&APPID=" + 
-        apiKey + 
-        "&units=imperial",
+function fetch5Day(lat, lon) {
+  $.get(
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+      lat +
+      "&lon=" +
+      lon +
+      "&APPID=" +
+      apiKey +
+      "&units=imperial",
 
-        function(data) {
-          data = changeIcon(data);
-          // render error if data list empty
-          let days = [];
-          for(let i = 0; i < data.list.length; i += 8){//should be 5 days as long as 40 items are returned
-            days.push(data.list[i])
-          }
-         
-          let compressedData = []
-          days.forEach((day) => {
-            let dayObj = {condition: day.weather[0].main, 
-                          tempMin: Math.round(day.main.temp_min), 
-                          tempMax: Math.round(day.main.temp_max), 
-                          iconUrl: 'http://openweathermap.org/img/wn/' + day.weather[0].icon + '@2x.png', 
-                          day: getDay(day.dt)
-                        }
-            compressedData.push(dayObj);
-          })
+    function (data) {
+      // data = changeIcon(data);
+      if (data.list) {
+        let days = [];
+        for (let i = 0; i < data.list.length; i += 8) {
+          //should be 5 days as long as 40 items are returned
+          days.push(data.list[i]);
+        }
 
-          render5DayWeather(compressedData)
+        let compressedData = [];
+        days.forEach((day) => {
+          let dayObj = {
+            condition: day.weather[0].main,
+            tempMin: Math.round(day.main.temp_min),
+            tempMax: Math.round(day.main.temp_max),
+            iconUrl:
+              "http://openweathermap.org/img/wn/" +
+              day.weather[0].icon +
+              "@2x.png",
+            day: getDay(day.dt),
+          };
+          compressedData.push(dayObj);
         });
+        render5DayWeather(compressedData);
+      } else {
+        renderError();
+      }
+    }
+  );
 }
-
 
 function fetchCoordinates(query) {
-  $.get("https://api.openweathermap.org/geo/1.0/direct?q=" + 
-  query + 
-  "&APPID=" + 
-  apiKey,
-  function(data) {
-    if(data.length === 0) {
-      renderError()
-    }
-    let lat = data[0].lat;
-    let lon = data[0].lon;
+  $.get(
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
+      query +
+      "&APPID=" +
+      apiKey,
+    function (data) {
+      if (data.length === 0) {
+        renderError();
+      }
+      let lat = data[0].lat;
+      let lon = data[0].lon;
 
-    fetchCurrent(lat, lon);
-    fetch5Day(lat, lon)
-    getImageData(0, lat, lon, 30);
-  })
+      fetchCurrent(lat, lon);
+      fetch5Day(lat, lon);
+      getImageData(0, lat, lon, 30);
+    }
+  );
 }
 
+$("#search-button").click(function () {
+  let query = $("#search-query").val();
 
-$('#search-button').click(function() {
-  
-  let query = $('#search-query').val();
-  
-  if(query) {
-    $('.five-day').removeClass('visually-hidden');
-    
+  if (query) {
+    $(".five-day").removeClass("visually-hidden");
+
     fetchCoordinates(query);
   }
-  
-})
+});
 
-
-$('button.current-location').click(function () {
-  $('.five-day').removeClass('visually-hidden')
+$("button.current-location").click(function () {
+  $(".five-day").removeClass("visually-hidden");
 
   let lon = null;
   let lat = null;
 
-  navigator.geolocation.getCurrentPosition(function(pos) {
+  navigator.geolocation.getCurrentPosition(function (pos) {
     let crds = pos.coords;
     lat = crds.latitude;
     lon = crds.longitude;
-    
+
     fetchCurrent(lat, lon);
     fetch5Day(lat, lon);
     getImageData(0, lat, lon, 30);
-  })
-})
-
-
+  });
+});
