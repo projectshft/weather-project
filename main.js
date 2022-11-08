1907436586fb9524909d3bf5389add7c4906c0fc
unknown <dgoodenough@proton.me>
2022-11-08 06:50:55 -0800

    Remove unnecessary declartion of lat and lon

--- main.js
+++ main.js
@@ -13,14 +13,11 @@ $("#search-button").click(function (e) {
 
 $(".current-location").click(function () {
   $(".five-day").removeClass("visually-hidden");
-
-  let lon = null;
-  let lat = null;
-
+ 
   navigator.geolocation.getCurrentPosition(function (pos) {
     let crds = pos.coords;
-    lat = crds.latitude;
-    lon = crds.longitude;
+    let lat = crds.latitude;
+    let lon = crds.longitude;
 
     fetchCurrent(lat, lon);
     fetch5Day(lat, lon);
