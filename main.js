432f71dce1c83995d8357b1c29368f125f932dd6
Connor Cook <97connorcook@gmail.com>
2022-12-07 16:44:58 -0700

    Needs styling but the 1 day forecast works, working on 5 day. currently seeing how to loop through and get an average

---
+++ main.js
@@ -0,0 +1,112 @@
+// Part 1: proof of concept
+// 1. create a script that will interact with a function
+// 2. first empty the div we will be putting info in, otherwise it'll continually pop up. create a function that finds the info we are looking for through a for loop 
+// 3. move the script into html, then compile using handlebars(this is now our template), using that template pass in the element to get what we need, finally append it.
+
+// 1. we need to put something in our array of cities = []. Fetch will help us with that but we need to get there first
+// 2. using the click function when the search button is clicked have the user input be passed into a new function called fetch
+// 3. fetch should have an ajax function set up and will have a 'GET' and a few other things along with the API and the the query you've made meaning the user input that was passed to this function. success should return a new function which will most likely be to add our new data into the cities array. make sure to pass in the data. call it addcity(data)
+// 4. addcity(data) will break down further what we need in the form of an object. we may need to employ some logic if needs. wrap what we'd like to correspond to our script in the 
+
+// 5 day forecast:
+// 1. Keep in mind that the API will give you an array of 40 items. this encompasses data for 5 days. Thus every 8 represents 1 day. So we will just need to get the average of each 8.
+// 2. create our script
+// 3. you can use the same functions, basically copy and paste for their respective areas.
+
+
+let cities = [];
+let fiveDay = [];
+
+$(".search").click(function () {
+  let userSearch = $("#search-query").val();
+
+  fetch(userSearch);
+});
+
+let fetch = function (query) {
+  $.ajax({
+  
+    // Our url to make request 
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=651b4326c31add8e66f753623aae609d",
+
+    // Type of Request
+    type: "GET",
+
+    // Function to call when
+    success: function (data) {
+        addCity(data);
+    },
+
+    // Error handling 
+    error: function (error) {
+        console.log(`Error ${error}`);
+    }
+  });
+
+  $.ajax({
+  
+    // Our url to make request 
+    url: "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=651b4326c31add8e66f753623aae609d",
+
+    // Type of Request
+    type: "GET",
+
+    // Function to call when
+    success: function (data2) {
+        addCity(data2);
+    },
+
+    // Error handling 
+    error: function (error) {
+        console.log(`Error ${error}`);
+    }
+  });
+
+};
+
+
+let addCity = function (data, data2) {
+  let city = { 
+    temperature: `${Math.round(((data.main.temp - 273.15) * (9/5) + 32))}\u00B0 F` || null,
+    cityName: data.name || null,
+    clouds: data.weather[0].main,
+    imageURL: "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
+  }
+
+  let fiveDay = {
+    temperature: data2.list
+
+  }
+
+  cities.push(city);
+
+  renderWeather();
+}
+
+
+let renderWeather = function () {
+  $(".weather-display").empty();
+  $(".weather-display2").empty();
+
+  for (let i = 0; i < cities.length; i++) {
+    const element = cities[i];
+    
+    let source = $("#weather-template").html();
+    let template = Handlebars.compile(source);
+    let newHTML = template(element);
+
+    $(".weather-display").append(newHTML);
+  }
+
+  for (let j = 0; j < fiveDay.length; j++) {
+    const forecastElement = fiveDay[j];
+    
+    let sourceTwo = $("#weather-template2").html();
+    let templateTwo = Handlebars.compile(sourceTwo);
+    let newHTMLTwo = templateTwo(forecastElement);
+
+    $(".weather-display2").append(newHTMLTwo);
+  }
+}
+
+renderWeather()
