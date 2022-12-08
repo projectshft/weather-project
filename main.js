19b9fcf717d506a220e36e1675cce9742c783545
Johnny Wright <johnnywrightiv@gmail.com>
2022-12-08 15:57:50 -0600

    Mockup UI

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-alert('howdy, world!')
+var city;
+
+$('#search-city').click(function() {
+  $('html').addClass('wait')
+  city = $('#city').val()
+  $('#city').val('')
+
+  setInterval(() => {
+    // Use to remove cursor spinwheel once API fetch is complete
+    $('html').removeClass('wait')
+  }, 2000);
+});
+
