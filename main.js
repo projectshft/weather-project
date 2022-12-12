19b43cf9309db32655e3b8dfc9637c1299105a8c
Johnny Wright <johnnywrightiv@gmail.com>
2022-12-12 14:44:40 -0600

    Submit Eval

--- main.js
+++ main.js
@@ -1,56 +1,67 @@
+// var key = 'YOUR_API_KEY_HERE';
+var key = config.MY_API_KEY;
 var $city;
 var currentWeather = {};
-var UTCTime;
-var locationTime;
+var forecast5Day = [];
 
-// Get user input and pass to API fetch call
+// Get user input, pass to API, and reset input field
 $('#search-city').click(function() {
-  $('html').addClass('wait')
-  $city = $('#city').val()
-
-  fetch($city);
+  $('html').addClass('wait');
+  $city = $('#city').val();
+  fetch('weather');
+  fetch('forecast');
   $('#city').val('')
 });
 
-// Call fetch (get) on API with jQuery Ajax function
-var fetch = function() {
+// Get current weather and 5-day forecast data from API with jQuery Ajax function 
+var fetch = function(fetchWhat) {
   $.ajax({
     method: "GET",
-    url: "https://api.openweathermap.org/data/2.5/weather?q=" + $city + "&units=imperial" + "&appid=303f064fa97c38222657765c89499f12",
+    url: "https://api.openweathermap.org/data/2.5/" + fetchWhat + "?q=" + $city + "&units=imperial" + "&appid=" + key,
     dataType: "json",
     success: function (data) {
-      console.log(`API response for "${$city}": `, data);
-      addWeather(data);
+      console.log(`API ${fetchWhat} response for "${$city}": `, data);
+      if (fetchWhat === 'weather') {
+        addWeather(data);
+      };
+      if (fetchWhat === 'forecast') {
+        addForecast(data);
+      };
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
     }
   });
-  $('html').removeClass('wait')
 };
 
-// Pass API data to currentWeather obj (for variable storage/reference) and invoke renderCurrentWeather (to compile and append to page)
-var addWeather = function (data) {
-  currentWeather = {};
-  // Logic to fix casing on weather description
-  var weatherConditions = data.weather[0].description
-  if (weatherConditions.includes(" ")) {
-    weatherConditions = weatherConditions.toLowerCase()
+// fixCasing for weather descriptions
+var fixCasing = function(condition) {
+  if (condition.includes(" ")) {
+    condition = condition.toLowerCase()
     .split(' ')
     .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
     .join(' ');
   } else {
-    weatherConditions = weatherConditions[0].toUpperCase() + weatherConditions.substr(1).toLowerCase()
+    condition = condition[0].toUpperCase() + condition.substr(1).toLowerCase()
   };
+  return condition
+};
+
+// Pass current weather data to currentWeather obj and invoke renderCurrentWeather
+var addWeather = function (data) {
+  $('html').removeClass('wait');
+  currentWeather = {};
+
+  // Fix casing on weather description
+  var descriptionLower = data.weather[0].description;
+  var descriptionProper = fixCasing(descriptionLower);
   
   // Add props to object for templating
   currentWeather = {
     cityName: data.name,
-    description: weatherConditions,
+    description: descriptionProper,
     temp: Math.floor(data.main.temp),
-    // timeStamp: taken when data is calculated, measured against UTC
     timeStamp: data.dt,
-    // timezone: Offset in seconds from Location to UTC
     timezone: data.timezone,
     sunUp: data.sys.sunrise,
     sunDown: data.sys.sunset,
@@ -60,92 +71,179 @@ var addWeather = function (data) {
   renderCurrentWeather(currentWeather);
 };
 
+// Pass 5-day forecast data to nextDay obj, push to forecast5Day arr, and invoke renderForecast
+var addForecast = function (data) {
+  forecast5Day = [];
+  var firstDayIndex;
+  
+  var weekday=new Array(7);
+  weekday[0]="Sunday";
+  weekday[1]="Monday";
+  weekday[2]="Tuesday";
+  weekday[3]="Wednesday";
+  weekday[4]="Thursday";
+  weekday[5]="Friday";
+  weekday[6]="Saturday";
+
+  
+  // Loop through next 5 days, get weather snapshots, push to array
+  for (let i = 1; i < 40; i += 8) {
+    var forecastSnapshot = data.list[i];
+
+    // Fix casing on weather description
+    var forecastDescriptionLower = forecastSnapshot.weather[0].description
+    var forecastDescriptionProper = fixCasing(forecastDescriptionLower);
+
+    // Get tomorrow's day index, each day after increment by one
+    if (!firstDayIndex) {
+      firstDayIndex = new Date ((forecastSnapshot.dt)*1000).getDay();
+    } else {
+      firstDayIndex ++;
+    };
+
+    var nextDay = {
+      description: forecastDescriptionProper,
+      temp: Math.floor(forecastSnapshot.main.temp),
+      icon: "http://openweathermap.org/img/wn/" + forecastSnapshot.weather[0].icon + "@2x.png",
+      day: weekday[firstDayIndex]
+    };
+    forecast5Day.push(nextDay);
+  };
+  renderForecast();
+};
+
 // Render weather data to page
 var renderCurrentWeather = function () {  
-  // Create new Date (object?/var) to store current time... struggled to get timezone conversions working
-  UTCTime = new Date ((currentWeather.timeStamp - currentWeather.timezone)*1000);
-  locationTime = new Date (currentWeather.timeStamp*1000);
-  var month = locationTime.getMonth()+1;
-  var date = locationTime.getDate();
-  var day = locationTime.getDay();
   
   // Call Handlebars Template
   var $source = $('#weather-template-current').html()
   var template = Handlebars.compile($source);
   var newHTML = template(currentWeather);
-
   $('#display-now').html(newHTML);
-  
-  render5Day();
-  
-  // Save default location via local storage
-  $('#set-default').click(function() {
-    localStorage.setItem('default-location', $city)
-    this.remove()
-  });
 
-  // Remove "Save as Default Location"
+  // Render 5-Day Display Box
+  $('#display-5-day').html('<div class="forecast-5-box row d-flex justify-content-around text-center mt-4 pb-4"><h3 class="my-3">5-Day Forecast</h3></div>')
+
+  renderBackgroundVid();
+  
+  // Remove "Save as Default Location" if already saved in local storage
   if (localStorage.getItem('default-location')) {
-    $('#set-default').remove()
+    $('#set-default').remove();
   };
 };
 
-// Extension - when currentWeather is rendered invoke render5Day
-// may need to pass currentWeather as an arg...
-var render5Day = function (current) {
-  // -- Code here --
-  $('#display-5-day').html(
-    // -- Code here --
-  );
+// Render 5-day forecast data to page
+var renderForecast = function () {
+  for (let i = 0; i < forecast5Day.length; i++) {
+    var renderDay = forecast5Day[i]
+
+    // Call Handlebars Template
+    var $source = $('#weather-template-5-day').html()
+    var template = Handlebars.compile($source);
+    var newHTML = template(renderDay);
+    
+    $('.forecast-5-box').append(newHTML);
+  };
 };
 
-
-// Repurposing and Frankenstein-ing code from Stack Overflow to create functioning clock
+// Frankenstein-ing code from Stack Overflow to create a functioning clock
 var createClock = function () {
   var twelveHourTime;
   var now = new Date();
   var startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
   var startOfDayTimestamp = startOfDay/1000;
 
+  // Pushes user's current time (based on their current location) to page
   var format = function (t) {
     var hours = Math.floor(t/3600),
         minutes = Math.floor(t/60%60),
         seconds = Math.floor(t%60),
         arr = [];
+    if (hours === 0) {
+      arr.push('12:');
+      twelveHourTime = ' AM';
+    };
     if (hours > 0) {
       arr.push(hours == 1 ? '1:' : hours + ':');
       twelveHourTime = ' AM';
-    }
+    };
     if (hours > 12) {
       hours = hours - 12;
       arr.shift();
       arr.push(hours == 1 ? '1:' : hours + ':');
       twelveHourTime = ' PM';
-    }
+    };
     if (minutes > 0 || hours > 0) {
       arr.push(minutes > 9 ? minutes + ':' : '0' + minutes + ':');
-    }
+    };
     if (seconds > 0 || minutes > 0 || hours > 0) {
       arr.push(seconds > 9 ? seconds : '0' + seconds);
-      arr.push(twelveHourTime)
-    }
-    $('#local-clock').html(arr.join(''));
+      arr.push(twelveHourTime);
+    };
+    $('#local-clock').html('Your Local Time: ' + arr.join(''));
   };
   
-  // Pushes user's current time (based on their current location) to page
   setInterval(function () {
     format(new Date().getTime()/1000-startOfDayTimestamp);
   }, 1000);
 };
 
+// Dynamic video backgrounds
+var renderBackgroundVid = function() {
+  $('#background-video').html(
+    '<source src="" type="video/mp4">')
+  var backgroundType = $('#current-conditions').text()
+  if (backgroundType.includes('Clear' || 'Sun')) {
+    $('#background-video').attr('src', "videos/clear.mp4")
+  };
+  if (backgroundType.includes('Cloud')) {
+    $('#background-video').attr('src', "videos/clouds.mp4")
+  };
+  if (backgroundType.includes('Drizzle' || 'Mist')) {
+    $('#background-video').attr('src', "videos/drizzle.mp4")
+  };
+  if (backgroundType.includes('Rain')) {
+    $('#background-video').attr('src', "videos/rain.mp4")
+  };
+  if (backgroundType.includes('Snow')) {
+    $('#background-video').attr('src', "videos/snow.mp4")
+  };
+  if (backgroundType.includes('Storm')) {
+    $('#background-video').attr('src', "videos/storm.mp4")
+  };
+  if (backgroundType.includes('Mist')) {
+    $('#background-video').attr('src', "videos/mist1.mp4")
+  };
+  if (!backgroundType) {
+    $('#background-video').attr('src', "videos/default-fallback.mp4")
+  };
+};
+
 window.onload = (event) => {
-  // For Fun - when currentWeather is rendered invoked create clock
+  renderBackgroundVid();
   createClock();
-  // If default location saved in local storage, load it and change display elements
+  // If default location is saved in local storage, load it and change display elements
   if (localStorage.getItem('default-location')) {
     $city = localStorage.getItem('default-location');
-    fetch($city);
+    fetch('weather');
+    fetch('forecast');
     $('#set-default').remove()
-    $('.page-header').append('<br><button id="remove-default" class="btn btn-secondary">Remove Default Location</button>')
-  }
+    $('.remove-btn-area').append('<button id="remove-default" class="btn btn-secondary mb-3">Remove Default Location</button>')
+  };
 };
+
+// On click, clear local storage and switch buttons
+$('.remove-btn-area').on('click', 'button', function () {
+  localStorage.clear();
+  $('#remove-default').remove();
+  $('.now-info').append('<button id="set-default" class="btn btn-secondary">Save as Default Location</button>')
+});
+
+// On click, save default location to local storage and switch buttons
+$('#display-now').on('click', 'button', function() {
+  if (!localStorage.getItem('default-location')) {
+    localStorage.setItem('default-location', $city);
+    $('#set-default').remove()
+    $('.remove-btn-area').append('<button id="remove-default" class="btn btn-secondary">Remove Default Location</button>');
+  };
+});
