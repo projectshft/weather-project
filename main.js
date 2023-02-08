f25ab82e617aabfd1c472452e4bc1707a419bdef
josephl37 <josephliow2000@gmail.com>
2023-02-08 17:39:03 -0600

    added weather API

--- main.js
+++ main.js
@@ -3,24 +3,38 @@ let current = [
     temp: 50,
     name: 'London',
     main: 'Coulds',
-    description: 'light clouds'
+    iconURL: 'http://openweathermap.org/img/wn/10d@2x.png'
   }
 ];
 
 $('.search').on('click', function () {
   let query = $('#search-query').val();
 
-  //currentWeather(query);
+  currentWeather(query);
+
   // clears input
   $('#search-query').val('');
 })
+
+const currentWeather = (query) => {
+  $.ajax({
+    method: "GET",
+    url: `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=4df43207cfa7a1f67f6f7fbd99044f1c&units=imperial`,
+    dataType: "json",
+    success: function(data) {
+    // reformat data from JSON to objects
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+}
 
 const renderCurrentWeather = () => {
   $('.current').empty();
 
   for (let i=0; i < current.length; i++) {
     let source = $('#current-template').html();
-    console.log(source);
     let resultTemplate = Handlebars.compile(source);
 
     let newWeather = resultTemplate(current[i]);
