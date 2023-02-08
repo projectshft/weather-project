68e3b5e8c95fd1c98deea7a2469ec09a6c2b371d
josephl37 <josephliow2000@gmail.com>
2023-02-08 16:58:08 -0600

    added current weather render function

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-let current = [];
+let current = [
+  {
+    temp: 50,
+    name: 'London',
+    main: 'Coulds',
+    description: 'light clouds'
+  }
+];
 
 $('.search').on('click', function () {
   let query = $('#search-query').val();
 
+  //currentWeather(query);
   // clears input
   $('#search-query').val('');
 })
 
+const renderCurrentWeather = () => {
+  $('.current').empty();
+
+  for (let i=0; i < current.length; i++) {
+    let source = $('#result-template').html();
+    console.log(source);
+    let resultTemplate = Handlebars.compile(source);
+
+    let newWeather = resultTemplate(current[i]);
+    $('.current').append(newWeather);
+  }
+};
+
+renderCurrentWeather();
