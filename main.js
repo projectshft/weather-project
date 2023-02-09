2591ec35622d11674b9dbba62ae0f6130f26eefb
josephl37 <josephliow2000@gmail.com>
2023-02-09 11:10:08 -0600

    set up forecast API and render

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
 let current = [];
+let forecast = [
+  {
+    temp: 62,
+    day: 'Friday',
+    main: 'Clouds',
+    iconURL: `http://openweathermap.org/img/wn/01d@2x.png`
+  },
+  {
+    temp: 62,
+    day: 'Saturday',
+    main: 'Clouds',
+    iconURL: `http://openweathermap.org/img/wn/01d@2x.png`
+  },
+  {
+    temp: 62,
+    day: 'Sunday',
+    main: 'Clouds',
+    iconURL: `http://openweathermap.org/img/wn/01d@2x.png`
+  },
+  {
+    temp: 62,
+    day: 'Monday',
+    main: 'Clouds',
+    iconURL: `http://openweathermap.org/img/wn/01d@2x.png`
+  },
+  {
+    temp: 62,
+    day: 'Tuesday',
+    main: 'Clouds',
+    iconURL: `http://openweathermap.org/img/wn/01d@2x.png`
+  }
+];
 
 $('.search').on('click', function () {
   let query = $('#search-query').val();
@@ -10,12 +42,26 @@ $('.search').on('click', function () {
 })
 
 const currentWeather = (query) => {
+  // current weather
   $.ajax({
     method: "GET",
     url: `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=4df43207cfa7a1f67f6f7fbd99044f1c&units=imperial`,
     dataType: "json",
     success: function(data) {
       addCurrentWeather(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      console.log(textStatus);
+    }
+  });
+
+  // forecast weather
+  $.ajax({
+    method: "GET",
+    url: `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=4df43207cfa7a1f67f6f7fbd99044f1c&units=imperial`,
+    dataType: "json",
+    success: function(data) {
+      console.log(data);
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
@@ -51,3 +97,21 @@ const renderCurrentWeather = () => {
   // empty current weather array
   current = [];
 };
+
+const renderForecastWeather = () => {
+  // empty forecast weather div
+  $('.forecast').empty();
+
+  for (let i=0; i < forecast.length; i++) {
+    let source = $('#forecast-template').html();
+    let resultTemplate = Handlebars.compile(source);
+
+    let newWeather = resultTemplate(forecast[i]);
+    $('.forecast').append(newWeather);
+  }
+
+  // empty current weather array
+  forecast = [];
+};
+
+renderForecastWeather();
