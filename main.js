2e4d2eb49e74c20be7cd4bdb47eddb529f9aff6d
Carly Burkhart <21320169+TimeIsOnMySide@users.noreply.github.com>
2023-06-11 09:59:45 -0700

    Working on 5-day forecast now...

--- main.js
+++ main.js
@@ -12,17 +12,24 @@ const fetchWeather = function(query) {
     .then(data => addWeather(data));
 }
 
+const fetchForecast = function(query) {
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q=' + query + '&appid=0eb7282867c9f6a908734d94070fdac1&units=imperial';
+  // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+  // API Key: 0eb7282867c9f6a908734d94070fdac1
+  fetch(url, {
+    method: 'GET',
+    dataType: 'json'
+  })
+    .then(data => data.json())
+    .then(data => addWeather(data));
+}
+
 document.querySelector('.search').addEventListener('click', function () {
-  const loader = `
-  <div class="spinner-border" role="status">
-    <span class="sr-only">Loading...</span>
-  </div>
-  `;
-  document.querySelector('.weather').innerHTML = loader;
 
   const search = document.querySelector('#search-query').value;
 
   fetchWeather(search);
+  fetchForecast(search);
 
   document.querySelector('#search-query').value = '';
 });
@@ -50,12 +57,15 @@ var renderWeather = function () {
   document.querySelector('.weather').replaceChildren();
 
   const template = `
-  <div class="col-md-6">
-    <h3>${ weather[0].currentTemp }</h3>
-    <h2>${ weather[0].location }</h2>
-    <h3>${ weather[0].currentWeather }</h3>
+  <div class="col-6">
+    <h3 class="text-center">${ Math.round(weather[0].currentTemp) }&deg;</h3>
+    <h2 class="text-center">${ weather[0].location }</h2>
+    <h3 class="text-center">${ weather[0].currentWeather }</h3>
+  </div>
+  <div class="col-6 d-flex justify-content-center">
     <img src="https://openweathermap.org/img/wn/${weather[0].currentIcon}@2x.png" alt="">
-  </div>`;
+  </div>
+  `;
 
   document.querySelector('.weather').innerHTML = template;
 };
