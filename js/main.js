9056d0256949a49be6951bacaa52fd6565e18d81
Jordan Cox <jordancox747@outlook.com>
2023-09-05 16:57:29 -0600

    setDefault working

--- js/main.js
+++ js/main.js
@@ -2,8 +2,10 @@
 import fetchCityData from "./current_weather.js";
 import getGeolocationWeather from "./geolocation.js";
 
-if (!localStorage.getItem("defaultStart")) {
+if (localStorage.getItem("defaultStart") === null || localStorage.getItem("defaultStart") === "null") {
   getGeolocationWeather();
+} else {
+  fetchCityData(localStorage.getItem("defaultStart"));
 }
 
 // Get user input
@@ -39,3 +41,32 @@ toggleBtn.addEventListener('click', () => {
   const input = localStorage.getItem("input");
   fetchCityData(input);
 });
+
+// Set default landing page
+const setDefault = document.querySelector("#set-default");
+
+setDefault.addEventListener("click", () => {
+  const input = localStorage.getItem("input");
+  localStorage.setItem("defaultStart", input);
+
+  updateOptionsDropdown();
+
+  const rmBtn = document.querySelector("#rm-default");
+
+  rmBtn.removeEventListener("click", removeDefault);
+  rmBtn.addEventListener("click", removeDefault);
+});
+
+const removeDefault = () => {
+  localStorage.removeItem("defaultStart");
+};
+
+const updateOptionsDropdown = () => {
+  document.querySelector(".dropdown-menu").replaceChildren();
+
+  const template = `
+  <li><a class="dropdown-item" href="#" id="set-default">Set Default</a></li>
+  <li><a class="dropdown-item" href="#" id="rm-default">Remove Default</a></li>`;
+
+  document.querySelector(".dropdown-menu").insertAdjacentHTML("beforeend", template);
+};
