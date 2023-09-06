cb27c0bd613286e16ce7373789ce620f7cc4471d
Jordan Cox <jordancox747@outlook.com>
2023-09-06 07:13:45 -0600

    map embed styling

---
+++ js/google_maps.js
@@ -0,0 +1,6 @@
+const MAP_API_KEY = 'AIzaSyBCNbEnV7RZIRMyO9vgfOD-evXXlu6C2yU';
+
+const renderMap = () => {
+  const template = `
+  <iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJrTDwkEVKaYcRgkCrtChCsak&key=AIzaSyBCNbEnV7RZIRMyO9vgfOD-evXXlu6C2yU"></iframe>`;
+};
