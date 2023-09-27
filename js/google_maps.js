d8274b0039df1257c98f35c58a0fbdb133bf4819
Jordan Cox <jordancox747@outlook.com>
2023-09-27 17:28:03 -0600

    env variables

--- js/google_maps.js
+++ js/google_maps.js
@@ -3,7 +3,7 @@
 // dotenv.config();
 
 const renderMap = (lat, lon) => {
-  const MAP_API_KEY = GOOGLE_MAPS_API_KEY;
+  const MAP_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 
   document.querySelector(".google-maps").replaceChildren();
 
