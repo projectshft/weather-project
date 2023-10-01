1a264462474e642cdcef4b408271d79a94c218dd
Jordan Cox <jordancox747@outlook.com>
2023-10-01 09:10:16 -0600

    revert changes

--- js/current_weather.js
+++ js/current_weather.js
@@ -2,9 +2,7 @@ import fetchForecastData from "./forecast.js";
 import { getLocalTime, getSunriseTime, getSunsetTime, compareTime } from "./time.js";
 import renderMap from "./google_maps.js";
 
-const process = require('process');
-const API_KEY = process.env.WEATHER_API_KEY;
-// const API_KEY = "078ae2ec7600b1d6a28bd166f6aad9e8";
+const API_KEY = "078ae2ec7600b1d6a28bd166f6aad9e8";
 
 /** Collects city data from API
 * @param {string} input - City name (and, optionally, state, country ISO code) received from user input on UI
