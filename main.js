da1da5d6c25f919274d826cefedfba9c7ed3170b
AdamC7313 <AdamC7313@gmail.com>
2023-12-04 21:01:58 -0500

    final commit before testing

--- main.js
+++ main.js
@@ -1,44 +1,72 @@
-//API Key
 const key = '5dbc25bc8e1707e2fd6040f71c4df0dd';
-//Check if there is a default city in local storage and set city equal to it
+
 let city = localStorage.getItem('defaultCity') ? localStorage.getItem('defaultCity') : 'No default city';
 
-//Capitalize the first letter of a string to use on city
-const capitalizeFirstLetter = (string) => {
-  return string[0].toUpperCase() + string.slice(1)
-}
+/** 
+ * Sets global city variable to city from geolocation
+ */
 
-//Set the city equal to data passed from geolocation
-handleGeolocation = (data) => {
-  city = data
-}
+const handleGeolocation = (data) => {
+  city = data;
+};
 
-//use google API to get city from lat/lon provided by geolocation API
+/**
+ * Uses google API to get the city based on users current coordinates
+ * @param {number} lat latitude
+ * @param {number} lon longitude
+ */
+
 const getGeolocation = (lat, lon) => {
-  let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyB-4vMY0awy9BejmkOB8Az55XNs2-GJEiI`
+  let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyB-4vMY0awy9BejmkOB8Az55XNs2-GJEiI`;
   fetch(url, {
     method: "GET",
     dataType: "json"
   })
     .then(data => data.json())
     .then(data => handleGeolocation(data.results[0].address_components[2].long_name)
-    )
-}
+    .catch(error => console.error('Error fetching data:', error));
+    );
+};
 
-//Add DOM content loaded listener for accessing DOM after load
 document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.default-city').innerHTML = `Default city: ${city}`
+  document.querySelector('.default-city').innerHTML = `Default city: ${city}`;
   document.querySelector('.geolocation').addEventListener('click', () => {
-    navigator.geolocation.getCurrentPosition((pos) => {
-      let lat = pos.coords.latitude;
-      let lon = pos.coords.longitude;
-      getWeather(lat, lon)
-      getGeolocation(lat, lon)
-    })
-  })
-})
+    navigator.geolocation.getCurrentPosition((pos) => handleClickGeo(pos));
+  });
+});
 
-//Return a day based on the current JavaScript Date object
+/**
+ * Distributes lat and lon to getWeather and getFiveDay functions, and sets city variable through getGeolocation
+ * @param {object} pos position object from navigator.geolocation API
+ */
+
+const handleClickGeo = (pos) => {
+
+  const loader = '<div class="loader-container"><div class="loader"></div></div>';
+  document.querySelector('body').insertAdjacentHTML('beforebegin', loader);
+
+  let lat = pos.coords.latitude;
+  let lon = pos.coords.longitude;
+
+  getGeolocation(lat, lon);
+
+  let data = [{
+    "lat": pos.coords.latitude,
+    "lon": pos.coords.longitude,
+    "name": city
+  }];
+  
+  getWeather(data);
+  getFiveDay(lat, lon);
+};
+
+
+/**
+ * Takes in a number and returns a day of the week string
+ * @param {number} day number based on day of the week
+ * @returns {string} day of the week
+ */
+
 const getDay = (day) => {
   const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
   const d = new Date(day);
@@ -46,12 +74,15 @@ const getDay = (day) => {
   return dayOfWeek;
 };
 
-//Display the five day forecast with a template
+/**
+ * Inserts 5 day forecast with template based on OpenWeatherAPI
+ * @param {object} data array of objects containing data for weather every three hours
+ */
 const showFiveDay = (data) => {
-  document.querySelector('.five-day-container').replaceChildren()
+  document.querySelector('.five-day-container').replaceChildren();
 
   //daily forecast is paid subscription, loop through 5 days of 3 hour data and get one from each day 
-  for(let i = 0; i < 40; i += 8) {
+  for(let i = 0; i < data.list.length; i += 8) {
     const day = data.list[i];
     const template = `
     <div
@@ -70,9 +101,16 @@ const showFiveDay = (data) => {
     `;
     document.querySelector('.five-day-container').insertAdjacentHTML('beforeend', template);
   }
+  if(document.querySelector('.loader-container')){
+  document.querySelector('.loader-container').remove();
+  };
 };
 
-//Fetch five day forecase from OpenWeatherAPI
+/**
+ * Gets the 5 day forecast and feeds it to display function
+ * @param {number} lat latitude
+ * @param {number} lon longitude
+ */
 const getFiveDay = (lat, lon) => {
   const url = `https://api.openweathermap.org/data/2.5/forecast/?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
   
@@ -81,17 +119,22 @@ const getFiveDay = (lat, lon) => {
     dataType: "json"
   })
     .then(data => data.json())
-    .then(data => showFiveDay(data));
+    .then(data => showFiveDay(data))
+    .catch(error => console.error('Error fetching data:', error));
 };
 
-//Display the current weather with a template
+/**
+ * Displays current weather data from OpenWeatherAPI with a template
+ * @param {object} data object with current weather data
+ */
+
 const showWeather = (data) => {
-  desc = data.weather[0].main;
-  //city = data.name;
+  document.querySelector('.weather-now').replaceChildren();
+  const desc = data.weather[0].main;
   const template = `
 
-  <div class="weather-container d-flex p-2">
-    <div class="weather-words text-center">
+  <div class="weather-container d-flex rounded-3 m-1 p-3">
+    <div class="weather-words text-center ">
       <h3 class="temp">${Math.floor(data.main.temp)}&deg;</h3>
       <h2 class="city">${city}</h2>
       <p class="weather-desc">${desc}</p>
@@ -99,35 +142,62 @@ const showWeather = (data) => {
     <div class="icon">
       <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="" />
     </div>
+    <button class="btn btn-primary default-button">
+    Add City as Default
+    </button>
   </div>
   `;
   
   document.querySelector('.weather-now').insertAdjacentHTML('beforeend', template);
 
-  //Make the add defualt button show up when a city is chosen
-  const defaultBtn = document.querySelector('.default-button')
-  defaultBtn.classList.remove('d-none')
+  const defaultBtn = document.querySelector('.default-button');
+
+  if (localStorage.getItem('defaultCity') !== city) {
+    defaultBtn.classList.remove('disabled');
+    defaultBtn.innerHTML = `Set ${city} as Default`;
+  } else {
+    defaultBtn.classList.add('disabled');
+    defaultBtn.innerHTML = `${city} is your default city`;
+  };
   
   //Add default city to local storage and give confirmation
   defaultBtn.addEventListener('click', () => {
-    localStorage.setItem('defaultCity', city)
+    localStorage.setItem('defaultCity', city);
     
-    const conf = document.querySelector('.default-conf')
-    conf.classList.add('conf')
+    const conf = document.querySelector('.default-conf');
+    conf.classList.add('conf');
 
-    setTimeout(() => {conf.classList.remove('conf')}, "2000")
+    setTimeout(() => {conf.classList.remove('conf')}, "2000");
+
     if(localStorage.getItem('defaultCity') === city) {
-      defaultBtn.classList.add('disabled')
-      defaultBtn.innerHTML = `${city} is your default`
-    } 
+      defaultBtn.classList.add('disabled');
+      defaultBtn.innerHTML = `${city} is your default`;
+    };
 
-    document.querySelector('.default-city').innerHTML = `Default City: ${city}`
-  })
+    document.querySelector('.default-city').innerHTML = `Default City: ${city}`;
+  });
 
 };
 
-//Fetch the current weather from OpenWeatherAPI
-const getWeather = (lat, lon) => {
+/**
+ * Fetches data from OpenWeatherAPI and feeds current weather data to showWeather()
+ * @param {object} data object with current location specifics
+ */
+
+const getWeather = (data) => {
+
+  console.log(data);
+
+  if(!data[0]) {
+    document.querySelector('.loader-container').remove();
+    alert('please enter a valid city');
+    return;
+  }
+
+  let lat = data[0].lat;
+  let lon = data[0].lon;
+  city = data[0].name;
+
   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${key}`;
 
   getFiveDay(lat, lon);
@@ -137,10 +207,15 @@ const getWeather = (lat, lon) => {
     dataType: "json"
   })
     .then(data => data.json())
-    .then(data => showWeather(data));
+    .then(data => showWeather(data))
+    .catch(error => console.error('Error fetching data:', error));
 };
 
-//Fetch the lat/lon based on user input of city
+/**
+ * Gets location specifics based on city string passed in
+ * @param {string} city 
+ */
+
 const getLatLong = (city) => {
   const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&units=imperial&appid=${key}`;
   fetch(url, {
@@ -148,28 +223,24 @@ const getLatLong = (city) => {
     dataType: "json"
   })
     .then(data => data.json())
-    .then(data => getWeather(data[0].lat, data[0].lon));
+    .then(data => getWeather(data))
+    .catch(error => console.error('Error fetching data:', error));
 };
 
-//If there is a default city in local storage, get the weather info based on that city on page load
+/* If there is a default city in local storage, get the weather info based on that city on page load */
 if(localStorage.getItem('defaultCity')) {
-  getLatLong(city)
-}
+  getLatLong(city);
+};
 
-//Listen for a click to submit the city, then set that city equal to the global city variable and get the lat/lon
+/* Listen for a click to submit the city, then get the lat/lon */
 document.querySelector('.search').addEventListener('click', () => {
-
-  document.querySelector('.weather-now').replaceChildren();
+  const loader = '<div class="loader-container"><div class="loader"></div></div>';
+  document.querySelector('body').insertAdjacentHTML('beforebegin', loader);
 
   let q = document.querySelector('#city').value;
-  city = capitalizeFirstLetter(q);
 
   getLatLong(q);
-  
+
   document.querySelector('#city');
-  const defaultBtn = document.querySelector('.default-button')
-  if(localStorage.getItem('defaultCity') !== city) {
-    defaultBtn.classList.remove('disabled')
-    defaultBtn.innerHTML = `Set ${city} as Default`
-  }
+
 });
